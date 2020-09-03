097214bcf5c50c1bdc72e6bf7ed04de356528662
Cameron <cclarkjh@gmail.com>
2020-09-03 14:00:56 +0100

    feat(component): show error if required on login
    Show error on login if: server not found, Incorrect username/password, or bad request

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,26 +1,31 @@
 <template>
-  <v-form @submit.prevent="userLogin">
-    <v-text-field
-      v-model="serverUrl"
-      outlined
-      label="Server Address"
-      type="url"
-    ></v-text-field>
-    <v-text-field
-      v-model="login.username"
-      outlined
-      label="Username"
-    ></v-text-field>
-    <v-text-field
-      v-model="login.pw"
-      outlined
-      label="Password"
-      :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
-      :type="showPassword ? 'text' : 'password'"
-      @click:append="() => (showPassword = !showPassword)"
-    ></v-text-field>
-    <v-btn block large color="primary" type="submit">submit</v-btn>
-  </v-form>
+  <div>
+    <v-form @submit.prevent="userLogin">
+      <v-text-field
+        v-model="serverUrl"
+        outlined
+        label="Server Address"
+        type="url"
+      ></v-text-field>
+      <v-text-field
+        v-model="login.username"
+        outlined
+        label="Username"
+      ></v-text-field>
+      <v-text-field
+        v-model="login.pw"
+        outlined
+        label="Password"
+        :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="showPassword ? 'text' : 'password'"
+        @click:append="() => (showPassword = !showPassword)"
+      ></v-text-field>
+      <v-btn block large color="primary" type="submit">submit</v-btn>
+    </v-form>
+    <v-alert v-if="errorMessage" class="mt-5" outlined type="error"
+      >{{ errorMessage }}
+    </v-alert>
+  </div>
 </template>
 
 <script lang="ts">
@@ -32,28 +37,41 @@ export default {
         username: '',
         password: ''
       },
-      showPassword: false
+      showPassword: false,
+      errorMessage: ''
     };
   },
   methods: {
     async userLogin() {
       try {
         this.$axios.setBaseURL(this.serverUrl);
+
         const response = await this.$auth.loginWith('local', {
           data: this.login
         });
+
         this.$auth.setUserToken(
           // TODO: Generate the token properly
           `MediaBrowser Client="Jellyfin Web", Device="Firefox", DeviceId="TW96aWxsYS81LjAgKFgxMTsgTGludXggeDg2XzY0OyBydjo3Ny4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94Lzc3LjB8MTU5NTQ1MTYzMzE4OQ11", Version="10.7.0", Token="${response.data.AccessToken}"`
         );
+
         this.$axios.setToken(
           // TODO: Generate the token properly
           `MediaBrowser Client="Jellyfin Web", Device="Firefox", DeviceId="TW96aWxsYS81LjAgKFgxMTsgTGludXggeDg2XzY0OyBydjo3Ny4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94Lzc3LjB8MTU5NTQ1MTYzMzE4OQ11", Version="10.7.0", Token="${response.data.AccessToken}"`,
           'X-Emby-Authorization'
         );
+
         this.$auth.setUser(response.data.User);
-      } catch (err) {
-        console.error('Failed to login:', err);
+      } catch (error) {
+        console.error('Failed to login:', error);
+
+        if (!error.response) {
+          this.errorMessage = 'Server Not Found';
+        } else if (error.response.status === 500) {
+          this.errorMessage = 'Incorrect Password';
+        } else if (error.response.status === 400) {
+          this.errorMessage = 'Bad Request. Try Again';
+        }
       }
     }
   }
