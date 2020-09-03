4696916a857e9412ad00d33941a0f16c1288fda7
MrTimscampi <julien.machiels@protonmail.com>
2020-09-03 07:57:42 +0200

    build(nuxt): switch to server target

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ const config: NuxtConfig = {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'static',
+  target: 'server',
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
