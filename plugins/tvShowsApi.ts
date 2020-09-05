732e578aedc2a786cf350e897824fd7e521240ad
MrTimscampi <julien.machiels@protonmail.com>
2020-09-05 20:10:43 +0200

    fix(tvshowsapi): fix type error on axios instance

--- plugins/tvShowsApi.ts
+++ plugins/tvShowsApi.ts
@@ -1,4 +1,5 @@
 import { Context } from '@nuxt/types';
+import { AxiosInstance } from 'axios';
 import { TvShowsApi } from '~/api/api';
 import { Configuration } from '~/api/configuration';
 import { PluginInjection } from '~/types/utils';
@@ -22,6 +23,10 @@ declare module 'vue/types/vue' {
 export default (context: Context, inject: PluginInjection): void => {
   const config = new Configuration();
 
-  const tvShowsApi = new TvShowsApi(config, '', context.$axios);
+  const tvShowsApi = new TvShowsApi(
+    config,
+    '',
+    context.$axios as AxiosInstance
+  );
   inject('tvShowsApi', tvShowsApi);
 };
