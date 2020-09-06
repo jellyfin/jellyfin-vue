20189cd665e8a95eda8f135680701ef05b42544d
MrTimscampi <julien.machiels@protonmail.com>
2020-09-06 13:56:07 +0200

    fix(vueperslides): ignore missing types while waiting for a Typings package

--- plugins/components/vueperSlides.ts
+++ plugins/components/vueperSlides.ts
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment -- Need to build a Type package for the module
+// @ts-ignore
 import { VueperSlides, VueperSlide } from 'vueperslides';
 import 'vueperslides/dist/vueperslides.css';
 
