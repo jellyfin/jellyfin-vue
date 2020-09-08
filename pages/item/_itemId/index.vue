509d17cd6378d9f2aa4a3ea3cc1f1a805dcf468d
Cameron <cclarkjh@gmail.com>
2020-09-08 11:31:33 +0100

    feat(item details): add length of show to item details page
    adds methods to convert from ticks to X hrs X min and renders the item's length in the item details
    screen

--- pages/item/_itemId/index.vue
+++ pages/item/_itemId/index.vue
@@ -6,6 +6,7 @@
       </v-col>
       <v-col cols="8">
         <h1>{{ item.Name }}</h1>
+        <p>{{ ticksToTime(item.RunTimeTicks) }}</p>
         <p>{{ item.Overview }}</p>
         <v-btn color="primary">{{ $t('play') }}</v-btn>
         <v-btn>{{ $t('more') }}</v-btn>
@@ -39,6 +40,16 @@ export default Vue.extend({
     ).data.Items as BaseItemDto[];
 
     this.item = Item[0];
+  },
+  methods: {
+    ticksToTime(ticks: number) {
+      const ms = ticks / 600000000;
+      if (Math.floor(ms / 60)) {
+        return `${Math.floor(ms / 60)} hrs ${Math.floor(ms % 60)} min`;
+      } else {
+        return `${Math.floor(ms % 60)} min`;
+      }
+    }
   }
 });
 </script>
