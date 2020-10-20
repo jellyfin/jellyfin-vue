0760419e4dbfd87de92c4ccedc812cd21c7bfb4f
MrTimscampi <julien.machiels@protonmail.com>
2020-10-20 18:54:02 +0200

    style: fix style issues due to eslint rules update

--- plugins/playbackProfile.ts
+++ plugins/playbackProfile.ts
@@ -39,6 +39,7 @@ declare module 'vue/types/vue' {
 }
 
 declare module 'vuex/types/index' {
+  // eslint-disable-next-line -- Current TypeScript rules flag S as unused, but Nuxt requires identical types
   interface Store<S> {
     $playbackProfile: DeviceProfile;
   }
