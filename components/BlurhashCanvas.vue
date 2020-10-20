b95f6076c2c4867c86e0c9fe2dcc0ea74d619624
ferferga <ferferga.fer@gmail.com>
2020-10-20 20:40:51 +0200

    feat(blurhash): Make blurhash functional and cache decoded results

--- components/BlurhashCanvas.vue
+++ components/BlurhashCanvas.vue
@@ -4,8 +4,10 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import Worker from 'worker-loader!./blurhash.worker';
+import Worker from 'worker-loader!~/plugins/workers/blurhash.worker';
 const worker = new Worker();
+let pending_canvas: { [id: string]: HTMLCanvasElement } = {};
+let computed_hashes: { [id: string]: Uint8ClampedArray } = {};
 
 export default Vue.extend({
   props: {
@@ -26,33 +28,36 @@ export default Vue.extend({
       default: 1
     }
   },
-  watch: {
-    hash() {
-      this.$nextTick(() => {
-        this.draw();
-      });
-    }
-  },
   mounted() {
-    this.draw();
-  },
-  methods: {
-    draw() {
-      const bhash_data = {
+    if (!(this.hash in pending_canvas) && !(this.hash in computed_hashes)) {
+      pending_canvas[this.hash] = this.$refs.canvas as HTMLCanvasElement;
+      worker.postMessage({
         hash: this.hash,
         width: this.width,
         height: this.height
-      };
-      worker.postMessage(bhash_data);
-      const ctx = (this.$refs.canvas as HTMLCanvasElement).getContext('2d');
-      const imageData = ctx?.createImageData(this.width, this.height);
+      });
       worker.onmessage = ({ data }) => {
-        const pixels = data;
-        if (pixels && imageData) {
+        this.draw(pending_canvas[data.hash], data.pixels);
+        computed_hashes[data.hash] = data.pixels;
+        delete pending_canvas[data.hash];
+      };
+    } else {
+      this.draw(
+        this.$refs.canvas as HTMLCanvasElement,
+        computed_hashes[this.hash]
+      );
+    }
+  },
+  methods: {
+    draw(canvas: HTMLCanvasElement, pixels: Uint8ClampedArray) {
+      if (pixels) {
+        const ctx = canvas.getContext('2d');
+        const imageData = ctx?.createImageData(this.width, this.height);
+        if (imageData) {
           imageData.data.set(pixels);
           ctx?.putImageData(imageData, 0, 0);
         }
-      };
+      }
     }
   }
 });
