88389acd6d4636b6473b9a65dca6f01b78ccfbd1
Julien Machiels <julien.machiels@protonmail.com>
2020-11-10 18:47:27 +0100

    fix(app-init): remove console output
    Co-authored-by: Cameron <cclarkjh@gmail.com>

--- plugins/appInitPlugin.ts
+++ plugins/appInitPlugin.ts
@@ -1,7 +1,6 @@
 import { Plugin } from '@nuxt/types';
 
 const appInitPlugin: Plugin = (context) => {
-  console.error('setting base url');
   if (context.store.state.user.serverUrl) {
     context.$axios.setBaseURL(context.store.state.user.serverUrl);
   }
