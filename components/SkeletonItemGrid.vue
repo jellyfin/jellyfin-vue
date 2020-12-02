3f5ab206988bc93068b5db53fed73765578b4df9
cameron clark <cameron.clark@hey.com>
2020-12-02 10:12:07 +0000

    refactor(skeleton cards): move skeleton card grid to is own componnet

---
+++ components/SkeletonItemGrid.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row>
+    <v-col cols="12" class="card-grid-container">
+      <skeleton-card v-for="n in 24" :key="n" :card-shape="skeletonCardShape" />
+    </v-col>
+  </v-row>
+</template>
+
+<style lang="scss" scoped>
+@import '~vuetify/src/styles/styles.sass';
+.card-grid-container {
+  display: grid;
+}
+
+@media #{map-get($display-breakpoints, 'sm-and-down')} {
+  .card-grid-container {
+    grid-template-columns: repeat(3, minmax(calc(100% / 3), 1fr));
+  }
+}
+
+@media #{map-get($display-breakpoints, 'sm-and-up')} {
+  .card-grid-container {
+    grid-template-columns: repeat(4, minmax(calc(100% / 4), 1fr));
+  }
+}
+
+@media #{map-get($display-breakpoints, 'lg-and-up')} {
+  .card-grid-container {
+    grid-template-columns: repeat(6, minmax(calc(100% / 6), 1fr));
+  }
+}
+
+@media #{map-get($display-breakpoints, 'xl-only')} {
+  .card-grid-container {
+    grid-template-columns: repeat(8, minmax(calc(100% / 8), 1fr));
+  }
+}
+</style>
