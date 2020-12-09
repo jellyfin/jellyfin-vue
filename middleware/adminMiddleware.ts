781d9f20d6eca1fda3f9c1c71dea4c6cc60183bc
Sarab Singh <sarab@sarabsingh.com>
2020-12-10 01:53:29 +0530

    feat(activity logs and admin middleware): add admin middleware to check for admin privilege

---
+++ middleware/adminMiddleware.ts
@@ -0,0 +1,14 @@
+import { Context } from '@nuxt/types';
+
+/**
+ * Middleware providing a redirect to the home page in case the
+ * User is not Admin and tries to access the admin page.
+ *
+ * @param {Context} context Nuxt application context
+ * @returns {void}
+ */
+export default function (context: Context): void {
+  if (!context.$auth.user.Policy.IsAdministrator) {
+    return context.redirect('/');
+  }
+}
--- pages/settings/activity.vue
+++ pages/settings/activity.vue
@@ -2,16 +2,37 @@
   <v-container>
     <v-row>
       <v-col cols="12" :offset-md="1" md="5" class="pt-0 pb-4">
-        <!-- Administrator settings -->
-        <v-list
-          v-if="$auth.user.Policy.IsAdministrator"
-          two-line
-          class="mb-4"
-          disabled
-        >
+        <v-list two-line class="mb-2">
+          <v-list-item-group>
+            <v-list-item
+              v-for="file in logFiles"
+              :key="file.Name"
+              :href="getLogFileLink(file.Name)"
+              target="_blank"
+            >
+              <v-list-item-avatar>
+                <v-icon>mdi-file</v-icon>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title v-text="file.Name" />
+                <v-list-item-subtitle
+                  v-text="getFormattedLogDate(file.DateModified)"
+                />
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-icon>mdi-chevron-right</v-icon>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-col>
+      <v-col cols="12" md="5" class="pt-0 pb-4">
+        <v-list two-line class="mb-2" disabled>
           <v-list-item-group>
             <v-list-item v-for="activity in activityList" :key="activity.Id">
-              <v-list-item-avatar>
+              <v-list-item-avatar
+                :color="getColorFromSeverity(activity.Severity)"
+              >
                 <v-icon>mdi-shield-account-outline</v-icon>
               </v-list-item-avatar>
               <v-list-item-content>
@@ -19,27 +40,10 @@
                 <v-list-item-subtitle v-text="activity.ShortOverview" />
               </v-list-item-content>
               <v-list-item-action>
-                <v-list-item-subtitle v-text="activity.Date" />
-              </v-list-item-action>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-        <!-- Mobile links -->
-        <v-list v-if="$vuetify.breakpoint.mobile">
-          <v-list-item-group>
-            <v-list-item
-              v-for="linkItem in linkItems"
-              :key="linkItem.name"
-              :href="linkItem.link"
-            >
-              <v-list-item-avatar>
-                <v-icon v-text="linkItem.icon" />
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title v-text="linkItem.name" />
-              </v-list-item-content>
-              <v-list-item-action>
-                <v-icon>mdi-chevron-right</v-icon>
+                <v-list-item-subtitle
+                  class="text-capitalize-first-letter"
+                  v-text="getFormattedActivityDate(activity.Date)"
+                />
               </v-list-item-action>
             </v-list-item>
           </v-list-item-group>
@@ -50,32 +54,102 @@
 </template>
 
 <script lang="ts">
-import { isEmpty } from 'lodash';
 import Vue from 'vue';
 import { mapActions } from 'vuex';
-import { SystemInfo } from '~/api';
+import colors from 'vuetify/lib/util/colors';
+import { ActivityLogEntry, LogFile, LogLevel } from '~/api';
 import htmlHelper from '~/mixins/htmlHelper';
 
 export default Vue.extend({
   mixins: [htmlHelper],
+  middleware: 'adminMiddleware',
   data() {
     return {
-      activityList: {} as SystemInfo
+      activityList: [] as ActivityLogEntry[],
+      logFiles: [] as LogFile[]
+    };
+  },
+  head() {
+    return {
+      title: this.$store.state.page.title
     };
   },
   async beforeMount() {
-    this.setAppBarOpacity({ opaqueAppBar: true });
-    this.setPageTitle({ title: this.$t('settings') });
-
-    if (this.$auth.user.Policy.IsAdministrator) {
-      this.activityList = (await this.$api.system.getActivityLogs()).data.Items;
-    }
+    this.setPageTitle({ title: this.$t('activityLogs') });
+    const minDate = new Date();
+    minDate.setTime(minDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+    this.activityList =
+      (await this.$api.activityLog.getLogEntries({ minDate })).data.Items || [];
+    this.logFiles = (await this.$api.system.getServerLogs()).data;
   },
   methods: {
-    ...mapActions('page', ['setPageTitle', 'setAppBarOpacity']),
-    isEmpty(object: any): boolean {
-      return isEmpty(object);
+    ...mapActions('page', ['setPageTitle']),
+    getColorFromSeverity(severity: LogLevel): string {
+      switch (severity) {
+        case LogLevel.Trace:
+          return (
+            this.$vuetify.theme.currentTheme.success?.toString() ||
+            colors.green.base
+          );
+        case LogLevel.Debug:
+          return (
+            this.$vuetify.theme.currentTheme.accent?.toString() ||
+            colors.blue.accent1
+          );
+        case LogLevel.Information:
+          return (
+            this.$vuetify.theme.currentTheme.info?.toString() ||
+            colors.blue.base
+          );
+        case LogLevel.Warning:
+          return (
+            this.$vuetify.theme.currentTheme.warning?.toString() ||
+            colors.amber.base
+          );
+        case LogLevel.Error:
+          return (
+            this.$vuetify.theme.currentTheme.error?.toString() ||
+            colors.red.accent2
+          );
+        case LogLevel.Critical:
+          return (
+            this.$vuetify.theme.currentTheme.secondary?.toString() ||
+            colors.grey.darken3
+          );
+        default:
+          return (
+            this.$vuetify.theme.currentTheme.primary?.toString() ||
+            colors.blue.darken2
+          );
+      }
+    },
+    getFormattedActivityDate(date: Date): string {
+      const today = this.$dateFns.endOfToday();
+      const day = this.$dateFns.formatRelative(
+        this.$dateFns.parseJSON(date),
+        today
+      );
+      return day;
+    },
+    getFormattedLogDate(date: Date): string {
+      const day = this.$dateFns.format(date, 'Ppp');
+      return day;
+    },
+    getLogFileLink(name: string): string {
+      return (
+        this.$axios.defaults.baseURL +
+        '/System/Logs/Log?name=' +
+        name +
+        '&api_key=' +
+        this.$store.state.user.accessToken
+      );
     }
   }
 });
 </script>
+
+<style>
+.text-capitalize-first-letter::first-letter {
+  text-transform: uppercase;
+}
+</style>
