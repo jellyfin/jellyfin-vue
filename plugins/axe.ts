4fca2881a803434378ca63f2ab37eb91355d43e4
MrTimscampi <julien.machiels@protonmail.com>
2021-01-09 02:37:59 +0100

    refactor(axe): prevent axe from running automatically
    axe running by itself caused some error messages to get lost. For ease of development, run it
    manually with $nuxt.$axe.run() in your browser's console.

--- plugins/axe.ts
+++ plugins/axe.ts
@@ -3,6 +3,6 @@ import Vue from 'vue';
 if (process.env.NODE_ENV === 'development') {
   // eslint-disable-next-line
   import('vue-axe').then(({ default: VueAxe }) => {
-    Vue.use(VueAxe);
+    Vue.use(VueAxe, { auto: false, clearConsoleOnUpdate: false });
   });
 }
