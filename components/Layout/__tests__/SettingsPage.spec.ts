f3242a76e1ffa41bdec458b99893b55ec5a93c12
Cameron Clark <cameron.clark@hey.com>
2021-01-13 00:01:06 +0000

    test(settings page): add tests for settings page

---
+++ components/Layout/__tests__/SettingsPage.spec.ts
@@ -0,0 +1,39 @@
+import { mount } from '@vue/test-utils';
+import SettingsPage from '~/components/Layout/SettingsPage.vue';
+
+const $t = (str: string): string => str;
+
+const wrapper = mount(SettingsPage, {
+  propsData: {
+    pageTitle: 'test-page-title'
+  },
+  mocks: {
+    $t
+  },
+  slots: {
+    actions: '<p>This is a demo action</p>',
+    content: '<p>This is the demo content</p>'
+  }
+});
+
+describe('Settings Page', () => {
+  test('Title and both slots are displayed', () => {
+    expect(wrapper.text()).toContain('test-page-title');
+    expect(wrapper.text()).toContain('This is a demo action');
+    expect(wrapper.text()).toContain('This is the demo content');
+  });
+
+  test('Only content slot is shown when title is undefined.', async () => {
+    await wrapper.setProps({ pageTitle: undefined });
+
+    expect(wrapper.text()).toEqual(
+      expect.not.stringContaining('test-page-title')
+    );
+
+    expect(wrapper.text()).toEqual(
+      expect.not.stringContaining('This is a demo action')
+    );
+
+    expect(wrapper.text()).toContain('This is the demo content');
+  });
+});
