6c6526ba17d2a048c0e7f71022b5bc65ca338c1c
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2021-01-17 16:32:42 +0100

    feat(localeswitcher): add automatic locale picking

--- components/System/LocaleSwitcher.vue
+++ components/System/LocaleSwitcher.vue
@@ -19,9 +19,11 @@
     </template>
     <v-list class="overflow-y-auto">
       <v-list-item
-        v-for="(item, index) in $i18n.locales"
-        :key="index"
-        :input-value="item.code === $i18n.locale"
+        v-for="item in locales"
+        :key="item.code"
+        :input-value="
+          item.code === $store.state.displayPreferences.CustomPrefs.locale
+        "
         @click="editCustomPref({ key: 'locale', value: item.code })"
       >
         <v-list-item-title>{{ item.name }}</v-list-item-title>
@@ -30,8 +32,14 @@
   </v-menu>
 </template>
 <script lang="ts">
+import { NuxtVueI18n } from 'nuxt-i18n/types/nuxt-i18n';
 import Vue from 'vue';
 import { mapActions } from 'vuex';
+
+interface LocalesSelect {
+  code: string;
+  name: string;
+}
 
 export default Vue.extend({
   props: {
@@ -40,8 +48,34 @@ export default Vue.extend({
       required: true
     }
   },
+  computed: {
+    locales: {
+      get(): LocalesSelect[] {
+        if (!this.$i18n.locales) return [];
+        const res: LocalesSelect[] = (this.$i18n
+          .locales as NuxtVueI18n.Options.LocaleObject[]).map((el) => {
+          return { code: el.code, name: el.name };
+        });
+        res.unshift({
+          code: 'auto',
+          name: `${this.$t('auto')} (${this.getLocaleCodeName(
+            this.$i18n.getBrowserLocale()
+          )})`
+        });
+        return res;
+      }
+    }
+  },
   methods: {
-    ...mapActions('displayPreferences', ['editCustomPref'])
+    ...mapActions('displayPreferences', ['editCustomPref']),
+    getLocaleCodeName(code: string | undefined): string {
+      if (!this.$i18n.locales || !code) return '';
+      const res = (this.$i18n
+        .locales as NuxtVueI18n.Options.LocaleObject[]).find(
+        (el) => el.code === code
+      );
+      return res ? res.name : '';
+    }
   }
 });
 </script>
