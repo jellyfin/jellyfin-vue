9ffd0abb1902a96e14f3c2ae59a8b911311fb538
Cameron Clark <cameron.clark@hey.com>
2021-02-03 21:45:53 +0000

    test(itemhelper): add tests for itemHelper

---
+++ mixins/__tests__/itemHelper.spec.ts
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import itemHelper from '~/mixins/itemHelper.ts';
+
+const TestComponent = new Vue({
+  mixins: [itemHelper]
+});
+
+describe('itemHelper', () => {
+  test('Correctly returns true if the item can be resumed', () => {
+    expect(TestComponent.canPlay({ Type: 'MusicGenre' })).toEqual(true);
+    expect(TestComponent.canPlay({ Type: 'Season' })).toEqual(true);
+    expect(TestComponent.canPlay({ Type: 'Series' })).toEqual(true);
+    expect(TestComponent.canPlay({ Type: 'BoxSet' })).toEqual(true);
+    expect(TestComponent.canPlay({ Type: 'MusicAlbum' })).toEqual(true);
+    expect(TestComponent.canPlay({ Type: 'MusicArtist' })).toEqual(true);
+    expect(TestComponent.canPlay({ Type: 'Playlist' })).toEqual(true);
+
+    expect(TestComponent.canPlay({ MediaType: 'Video' })).toEqual(true);
+    expect(TestComponent.canPlay({ MediaType: 'Audio' })).toEqual(true);
+
+    expect(TestComponent.canPlay({})).toEqual(false);
+  });
+
+  test('Correctly returns true if the item can be resumed', () => {
+    expect(
+      TestComponent.canResume({ UserData: { PlaybackPositionTicks: 1 } })
+    ).toEqual(true);
+
+    expect(
+      TestComponent.canResume({ UserData: { PlaybackPositionTicks: 0 } })
+    ).toEqual(false);
+
+    expect(TestComponent.canResume({ UserData: {} })).toEqual(false);
+  });
+});
