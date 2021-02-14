1cb2e0b790a8a2a6e4cc94bb231450c052cd8781
Cameron Clark <cameron.clark@hey.com>
2021-02-14 10:59:34 +0000

    fix(skeleton home section): fix missing margin on skeleton home sections heading

--- components/Skeletons/SkeletonHomeSection.vue
+++ components/Skeletons/SkeletonHomeSection.vue
@@ -1,6 +1,6 @@
 <template>
   <v-col>
-    <v-skeleton-loader type="heading" max-width="25em" />
+    <v-skeleton-loader type="heading" max-width="25em" class="ml-2" />
     <v-row class="space-around ma-0">
       <skeleton-card
         v-for="i in cardNumber"
