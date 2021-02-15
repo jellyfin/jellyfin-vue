5c914ba866e48f612af4cde966fc8d1a6a9e1861
Cameron Clark <cameron.clark@hey.com>
2021-02-15 20:40:52 +0000

    test(admin middleware): add tests for admin middleware

---
+++ middleware/__tests__/adminMiddleware.spec.ts
@@ -0,0 +1,46 @@
+import { Context } from '@nuxt/types';
+import adminMiddleware from '../adminMiddleware';
+
+const mockRedirect = jest.fn();
+
+const BASE_INPUT = ({
+  $auth: {
+    user: {
+      Policy: {
+        IsAdministrator: false
+      }
+    }
+  },
+  redirect: mockRedirect
+} as unknown) as Context;
+
+const INPUT_NOT_ADMIN = {
+  ...BASE_INPUT
+};
+
+const INPUT_ADMIN = ({
+  ...BASE_INPUT,
+  $auth: {
+    user: {
+      Policy: {
+        IsAdministrator: true
+      }
+    }
+  }
+} as unknown) as Context;
+
+afterEach(() => mockRedirect.mockReset());
+
+describe('adminMiddleware', () => {
+  it('redirect to "/" when user is not an administrator', (): void => {
+    adminMiddleware(INPUT_NOT_ADMIN);
+
+    expect(mockRedirect.mock.calls[0][0]).toBe('/');
+  });
+
+  it('does not redirect when user is not an administrator', (): void => {
+    adminMiddleware(INPUT_ADMIN);
+
+    expect(mockRedirect.mock.calls).toHaveLength(0);
+  });
+});
