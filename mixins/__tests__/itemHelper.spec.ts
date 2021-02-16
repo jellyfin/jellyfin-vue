78887eda16b6062aa0d7d378c9c88317de378403
Cameron Clark <cameron.clark@hey.com>
2021-02-16 09:22:28 +0000

    test(item helper): add more tests to itemHelper

--- mixins/__tests__/itemHelper.spec.ts
+++ mixins/__tests__/itemHelper.spec.ts
@@ -5,9 +5,8 @@ const TestComponent = new Vue({
   mixins: [itemHelper]
 });
 
-describe('mixin: itemHelper', () => {
+describe('itemHelper', () => {
   it('returns true if the item can be played', () => {
-    // TODO: Replace with BaseItemKind enum
     expect(TestComponent.canPlay({ Type: 'AggregateFolder' })).toEqual(false);
     expect(TestComponent.canPlay({ Type: 'Audio' })).toEqual(true);
     expect(TestComponent.canPlay({ Type: 'AudioBook' })).toEqual(true);
@@ -61,5 +60,78 @@ describe('mixin: itemHelper', () => {
     ).toEqual(false);
 
     expect(TestComponent.canResume({ UserData: {} })).toEqual(false);
+  });
+
+  it('returns the correct item icon', () => {
+    expect(TestComponent.getItemIcon({ Type: 'AggregateFolder' })).toEqual(
+      'mdi-folder'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'Audio' })).toEqual(
+      'mdi-music-note'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'AudioBook' })).toEqual(
+      'mdi-music-note'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'BasePluginFolder' })).toEqual(
+      'mdi-folder'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'Book' })).toEqual(
+      'mdi-book-open-page-variant'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'BoxSet' })).toEqual(
+      'mdi-folder-multiple'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'Channel' })).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'ChannelFolderItem' })).toEqual(
+      ''
+    );
+    expect(TestComponent.getItemIcon({ Type: 'CollectionFolder' })).toEqual(
+      'mdi-folder'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'Episode' })).toEqual(
+      'mdi-television-classic'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'Folder' })).toEqual('mdi-folder');
+    expect(TestComponent.getItemIcon({ Type: 'Genre' })).toEqual('');
+    expect(
+      TestComponent.getItemIcon({ Type: 'ManualPlaylistsFolder' })
+    ).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'Movie' })).toEqual(
+      'mdi-filmstrip'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'MusicAlbum' })).toEqual(
+      'mdi-album'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'MusicArtist' })).toEqual(
+      'mdi-account'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'MusicGenre' })).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'MusicVideo' })).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'Person' })).toEqual(
+      'mdi-account'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'Photo' })).toEqual('mdi-image');
+    expect(TestComponent.getItemIcon({ Type: 'PhotoAlbum' })).toEqual(
+      'mdi-image-multiple'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'Playlist' })).toEqual(
+      'mdi-playlist-play'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'Program' })).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'Recording' })).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'Season' })).toEqual(
+      'mdi-television-classic'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'Series' })).toEqual(
+      'mdi-television-classic'
+    );
+    expect(TestComponent.getItemIcon({ Type: 'Studio' })).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'Trailer' })).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'TvChannel' })).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'TvProgram' })).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'UserRootFolder' })).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'UserView' })).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'Video' })).toEqual('');
+    expect(TestComponent.getItemIcon({ Type: 'Year' })).toEqual('');
   });
 });
--- mixins/itemHelper.ts
+++ mixins/itemHelper.ts
@@ -136,13 +136,16 @@ const itemHelper = Vue.extend({
     getItemIcon(item: BaseItemDto): string {
       switch (item.Type) {
         case 'Audio':
+        case 'AudioBook':
           return 'mdi-music-note';
         case 'Book':
           return 'mdi-book-open-page-variant';
         case 'BoxSet':
           return 'mdi-folder-multiple';
-        case 'Folder':
+        case 'AggregateFolder':
+        case 'BasePluginFolder':
         case 'CollectionFolder':
+        case 'Folder':
           return 'mdi-folder';
         case 'Movie':
           return 'mdi-filmstrip';
@@ -151,11 +154,14 @@ const itemHelper = Vue.extend({
         case 'MusicArtist':
         case 'Person':
           return 'mdi-account';
+        case 'Photo':
+          return 'mdi-image';
         case 'PhotoAlbum':
           return 'mdi-image-multiple';
         case 'Playlist':
           return 'mdi-playlist-play';
         case 'Series':
+        case 'Season':
         case 'Episode':
           return 'mdi-television-classic';
         default:
