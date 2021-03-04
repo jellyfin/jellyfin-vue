8573ed82eded47dc7a9e5bf5fdba4679cfe68fa1
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2021-03-04 22:45:15 +0100

    feat(artist item): use item store

--- client/pages/artist/_itemId/index.vue
+++ client/pages/artist/_itemId/index.vue
@@ -112,7 +112,7 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 import { BaseItemDto, ImageType } from '@jellyfin/client-axios';
 import { Context } from '@nuxt/types';
 import htmlHelper from '~/mixins/htmlHelper';
@@ -126,32 +126,21 @@ export default Vue.extend({
   validate(ctx: Context) {
     return isValidMD5(ctx.route.params.itemId);
   },
-  async asyncData({ params, $api, $auth }) {
-    const item = (
-      await $api.userLibrary.getItem({
-        userId: $auth.user?.Id,
-        itemId: params.itemId
-      })
-    ).data;
-
-    const appearances = (
-      await $api.items.getItems({
-        userId: $auth.user?.Id,
-        albumArtistIds: [params.itemId],
-        sortBy: 'PremiereDate,ProductionYear,SortName',
-        sortOrder: 'Descending',
-        recursive: true,
-        includeItemTypes: ['MusicAlbum']
-      })
-    ).data.Items;
-
-    return { item, appearances };
+  async asyncData({ params, $libraries }) {
+    await $libraries.fetchItem(params.itemId);
+    const appearanceIds = await $libraries.fetchItems({
+      albumArtistIds: [params.itemId],
+      sortBy: 'PremiereDate,ProductionYear,SortName',
+      sortOrder: 'Descending',
+      recursive: true,
+      includeItemTypes: ['MusicAlbum']
+    });
+    return { appearanceIds };
   },
   data() {
     return {
       activeTab: 0,
-      item: {} as BaseItemDto,
-      appearances: [] as BaseItemDto[]
+      appearanceIds: [] as string[]
     };
   },
   head() {
@@ -160,9 +149,16 @@ export default Vue.extend({
     };
   },
   computed: {
+    ...mapGetters('items', ['getItem', 'getItems']),
+    appearances(): BaseItemDto[] {
+      return this.getItems(this.appearanceIds);
+    },
+    item(): BaseItemDto {
+      return this.getItem(this.$route.params.itemId);
+    },
     overview(): string {
-      if (this.$data.item.Overview) {
-        return this.sanitizeHtml(this.$data.item.Overview);
+      if (this.item.Overview) {
+        return this.sanitizeHtml(this.item.Overview);
       } else {
         return '';
       }
