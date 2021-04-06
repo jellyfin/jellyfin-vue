43563505307166c0aff30b913bcb663aa442225d
Fernando Fernández <ferferga@hotmail.com>
2021-04-06 21:01:00 +0200

    feat(websocket): handle UserDataChanged messages to update the item store

--- client/store/plugins/websocket.ts
+++ client/store/plugins/websocket.ts
@@ -28,40 +28,64 @@ export const websocketPlugin: Plugin<AppState> = (store) => {
     store.state.socket.instance?.send(JSON.stringify(msg));
   }
 
-  store.subscribe(async (mutation, state) => {
-    // Messages
-    // Keep the websocket alive
-    if (
-      mutation.type === 'SOCKET_ONMESSAGE' &&
-      state.socket.message.MessageType === 'ForceKeepAlive'
-    ) {
-      sendWebsocketMessage('KeepAlive');
-      keepAliveInterval = window.setInterval(() => {
-        sendWebsocketMessage('KeepAlive');
-      }, state.socket.message.Data * 1000 * 0.5);
+  /**
+   * Updates the items in the store after the websocket informs of it
+   *
+   * @param {string[]} itemIds - Ids of the items to update
+   */
+  async function updateStoreItems(itemIds: string[]): Promise<void> {
+    if (itemIds.length) {
+      const updatedItems: BaseItemDto[] =
+        (
+          await store.$api.items.getItems({
+            userId: store.$auth.user?.Id,
+            ids: itemIds
+          })
+        ).data.Items || [];
+
+      store.dispatch('items/addItems', { items: updatedItems });
     }
-    // Update items when changed
-    else if (
-      mutation?.type === 'SOCKET_ONMESSAGE' &&
-      state.socket.message.MessageType === 'LibraryChanged'
-    ) {
-      // @ts-expect-error -- The Data property doesn't describe its content
-      const itemsToUpdate: string[] = state.socket.message.Data.ItemsUpdated.filter(
-        (itemId: string) => {
-          return store.state.items.allIds.includes(itemId);
-        }
-      );
+  }
 
-      if (itemsToUpdate) {
-        const updatedItems: BaseItemDto[] =
-          (
-            await store.$api.items.getItems({
-              userId: store.$auth.user?.Id,
-              ids: itemsToUpdate
-            })
-          ).data.Items || [];
+  store.subscribe((mutation, state) => {
+    const message = state.socket.message;
+    const storeIds = store.state.items.allIds;
+    let itemsToUpdate: string[];
 
-        store.dispatch('items/addItems', { items: updatedItems });
+    if (mutation.type === 'SOCKET_ONMESSAGE') {
+      switch (message.MessageType) {
+        case 'ForceKeepAlive': // Keep the websocket alive
+          sendWebsocketMessage('KeepAlive');
+          keepAliveInterval = window.setInterval(() => {
+            sendWebsocketMessage('KeepAlive');
+          }, message.Data * 1000 * 0.5);
+          break;
+        case 'LibraryChanged': // Update items when metadata changes
+          // @ts-expect-error -- The Data property doesn't describe its content
+          itemsToUpdate = message.Data.ItemsUpdated.filter((itemId: string) => {
+            return storeIds.includes(itemId);
+          });
+
+          updateStoreItems(itemsToUpdate);
+          break;
+        case 'UserDataChanged': // Update items when their userdata is changed (like, mark as watched, etc)
+          // @ts-expect-error -- The Data property doesn't describe its content
+          itemsToUpdate = message.Data.UserDataList.filter(
+            (updatedData: never) => {
+              // @ts-expect-error -- There are no typings for websocket returned data.
+              const itemId = updatedData.ItemId as string;
+
+              return storeIds.includes(itemId);
+            }
+          ).map((updatedData: never) => {
+            // @ts-expect-error -- There are no typings for websocket returned data.
+            return updatedData.ItemId as string;
+          });
+
+          updateStoreItems(itemsToUpdate);
+          break;
+        default:
+          break;
       }
     }
     // Socket disconnection
