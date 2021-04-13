818223bfa76551125a9c63de9dfddcf0d189519e
Alf Sebastian Houge <git@alfhouge.no>
2021-04-13 15:30:29 +0200

    feat(up-next): remove unnecessary if check

--- client/store/playbackManager.ts
+++ client/store/playbackManager.ts
@@ -177,11 +177,7 @@ export const getters: GetterTree<PlaybackManagerState, RootState> = {
     return null;
   },
   getCurrentTime: (state) => {
-    if (state.currentTime !== null) {
-      return state.currentTime;
-    } else {
-      return null;
-    }
+    return state.currentTime;
   }
 };
 
