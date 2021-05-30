1adaa7b85c96ebd3df2332a65e3ef79b07f77fdd
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2021-05-30 17:14:02 +0200

    feat(header): added episode link

--- client/components/Layout/HomeHeader/HomeHeaderEpisodeTitle.vue
+++ client/components/Layout/HomeHeader/HomeHeaderEpisodeTitle.vue
@@ -11,20 +11,25 @@
       :alt="item.Name"
       :src="logo"
     />
-    <h1
+    <nuxt-link
       v-else
       data-swiper-parallax="-300"
-      class="text-h4 text-sm-h2 text-truncate mt-n2 mb-n1"
+      class="link d-block text-h4 text-sm-h2 text-truncate mt-n2 mb-n1"
+      :to="getItemDetailsLink({ Id: item.SeriesId }, 'Series')"
     >
       {{ item.SeriesName }}
-    </h1>
+    </nuxt-link>
     <p data-swiper-parallax="-200" class="mb-n1 text-truncate text-subtitle-2">
       {{ item.SeasonName }}
       {{ $t('episodeNumber', { episodeNumber: item.IndexNumber }) }}
     </p>
-    <h2 data-swiper-parallax="-200" class="text-h5 text-sm-h4 text-truncate">
+    <nuxt-link
+      data-swiper-parallax="-200"
+      class="link d-block text-h5 text-sm-h4 text-truncate"
+      :to="getItemDetailsLink(item)"
+    >
       {{ item.Name }}
-    </h2>
+    </nuxt-link>
   </div>
 </template>
 
@@ -32,9 +37,10 @@
 import Vue from 'vue';
 import { BaseItemDto, ImageType } from '@jellyfin/client-axios';
 import imageHelper from '~/mixins/imageHelper';
+import itemHelper from '~/mixins/itemHelper';
 
 export default Vue.extend({
-  mixins: [imageHelper],
+  mixins: [imageHelper, itemHelper],
   props: {
     item: {
       type: Object as () => BaseItemDto,
