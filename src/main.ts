221a862cbeac89d2f44a9e3c2c20736072a00a0f
MrTimscampi <julien.machiels@protonmail.com>
2021-06-23 10:55:15 +0200

    feat(app): add router support

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Vuetify from './plugins/vuetify';
 import App from './App.vue';
 import store from './store';
+import routes from './routes';
 
 import 'vuetify/dist/vuetify.min.css';
 
 const app = createApp(App);
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+});
 
 app.use(Vuetify);
 app.use(store);
+app.use(router);
 
 app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import ViteComponents from 'vite-plugin-components';
+import Layouts from 'vite-plugin-vue-layouts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ViteComponents({})]
+  plugins: [vue(), ViteComponents({}), Layouts()]
 });
