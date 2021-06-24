954c9af1329f456c326785abf3082c1bd11015f4
MrTimscampi <julien.machiels@protonmail.com>
2021-06-24 08:31:21 +0200

    feat(app): add icon support for Vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,4 +1,4 @@
-// import '@mdi/font/css/materialdesignicons.css';
+import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/lib/styles/main.sass';
 import { createVuetify } from '@vuetify/nightly';
 import * as components from '@vuetify/nightly/lib/components';
