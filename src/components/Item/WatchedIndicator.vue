d637b3e7d2d19f37f72aa0e8fc2a2cf9a0910569
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2021-08-26 17:33:31 +0200

    refactor(watchedindicator): factorized out the watched indicator

---
+++ src/components/Item/WatchedIndicator.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-chip color="green" class="card-chip" small>
+    <v-icon>mdi-check</v-icon>
+  </v-chip>
+</template>
+
+<style lang="scss" scoped>
+.card-chip {
+  position: absolute;
+  top: 1em;
+  right: 1em;
+}
+</style>
