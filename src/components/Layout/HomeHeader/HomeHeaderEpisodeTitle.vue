018fc0f8f4f46711516dd2a4e6cd2191dfd0851b
MrTimscampi <julien.machiels@protonmail.com>
2021-08-26 06:30:52 +0200

    refactor(home-header): use functional components for item titles

--- src/components/Layout/HomeHeader/HomeHeaderEpisodeTitle.vue
+++ src/components/Layout/HomeHeader/HomeHeaderEpisodeTitle.vue
@@ -1,71 +1,66 @@
-<template>
+<template functional>
   <div>
-    <nuxt-link
-      v-if="imageTag"
-      :to="
-        item.SeriesId
-          ? getItemDetailsLink({ Id: item.SeriesId }, 'Series')
-          : null
-      "
-    >
+    <nuxt-link v-if="props.logo.tag" :to="props.parentLink">
       <v-img
         class="mb-2"
-        :max-width="$vuetify.breakpoint.mdAndUp ? '50%' : '40%'"
-        :max-height="$vuetify.breakpoint.smAndUp ? '7.5em' : '4em'"
+        :max-width="parent.$vuetify.breakpoint.mdAndUp ? '50%' : '40%'"
+        :max-height="parent.$vuetify.breakpoint.smAndUp ? '7.5em' : '4em'"
         contain
         position="left center"
         data-swiper-parallax="-300"
-        :alt="item.Name"
-        :src="logo"
+        :alt="props.item.Name"
+        :src="props.logo.url"
       />
     </nuxt-link>
     <nuxt-link
       v-else
       data-swiper-parallax="-300"
       class="link d-block text-h4 text-sm-h2 text-truncate mt-n2 mb-n1"
-      :to="
-        item.SeriesId
-          ? getItemDetailsLink({ Id: item.SeriesId }, 'Series')
-          : null
-      "
+      :to="props.parentLink"
     >
-      {{ item.SeriesName }}
+      {{ props.item.SeriesName }}
     </nuxt-link>
     <p data-swiper-parallax="-200" class="mb-n1 text-truncate text-subtitle-2">
-      {{ item.SeasonName }}
-      {{ $t('episodeNumber', { episodeNumber: item.IndexNumber }) }}
+      {{ props.item.SeasonName }}
+      {{
+        parent.$t('episodeNumber', { episodeNumber: props.item.IndexNumber })
+      }}
     </p>
     <nuxt-link
       data-swiper-parallax="-200"
       class="link d-block text-h5 text-sm-h4 text-truncate"
-      :to="getItemDetailsLink(item)"
+      :to="props.link"
     >
-      {{ item.Name }}
+      {{ props.item.Name }}
     </nuxt-link>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { BaseItemDto, ImageType } from '@jellyfin/client-axios';
-import imageHelper from '~/mixins/imageHelper';
-import itemHelper from '~/mixins/itemHelper';
+import { VImg } from 'vuetify/lib';
+import { BaseItemDto } from '@jellyfin/client-axios';
+import { ImageUrlInfo } from '~/mixins/imageHelper';
+
+Vue.component('VImg', VImg);
 
 export default Vue.extend({
-  mixins: [imageHelper, itemHelper],
   props: {
     item: {
       type: Object as () => BaseItemDto,
       required: true
     },
-    logo: {
+    parentLink: {
       type: String,
-      default: ''
-    }
-  },
-  computed: {
-    imageTag(): string | undefined {
-      return this.getImageTag(this.item, ImageType.Logo);
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    logo: {
+      type: Object as () => ImageUrlInfo,
+      default: undefined
     }
   }
 });
