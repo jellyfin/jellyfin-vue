897094ed64a825e608679a3ae5b562c41d1fa765
MrTimscampi <julien.machiels@protonmail.com>
2021-08-28 08:19:01 +0200

    fix(watched-indicator): fix vuetify component treeshaking

--- src/components/Item/WatchedIndicator.vue
+++ src/components/Item/WatchedIndicator.vue
@@ -4,6 +4,18 @@
   </v-chip>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+import { VChip, VIcon } from 'vuetify/lib';
+
+Vue.component('VChip', VChip);
+Vue.component('VIcon', VIcon);
+
+export default Vue.extend({
+  name: 'WatchedIndicator'
+});
+</script>
+
 <style lang="scss" scoped>
 .card-chip {
   position: absolute;
