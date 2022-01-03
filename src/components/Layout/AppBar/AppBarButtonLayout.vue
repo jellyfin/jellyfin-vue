93c965b9ce7c42638df4c14e715f9d91f3174b95
Fernando Fernández <ferferga@hotmail.com>
2022-01-03 23:47:21 +0100

    feat: create app-bar-button-layout component

---
+++ src/components/Layout/AppBar/AppBarButtonLayout.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="ma-1">
+    <v-tooltip bottom>
+      <template #activator="{ on, attrs }">
+        <v-btn
+          class="align-self-center active-button"
+          :icon="!fab"
+          :fab="fab"
+          :small="fab"
+          :class="{ 'ma-n1': !fab }"
+          :color="color"
+          v-bind="attrs"
+          v-on="$slots.tooltip ? { ...on, ...customListener } : customListener"
+        >
+          <slot name="icon" />
+        </v-btn>
+      </template>
+      <slot name="tooltip" />
+    </v-tooltip>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapState } from 'vuex';
+
+export default Vue.extend({
+  props: {
+    color: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    customListener: {
+      type: Object as () => EventTarget,
+      required: false,
+      default: undefined
+    }
+  },
+  computed: {
+    ...mapState('page', ['opaqueAppBar', 'isScrolled']),
+    fab(): boolean {
+      return (
+        !(this.opaqueAppBar || this.$vuetify.breakpoint.xsOnly) &&
+        !this.isScrolled
+      );
+    }
+  }
+});
+</script>
