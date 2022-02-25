bf8be690fafc7af7cc65dea84986d9f11e331c10
Fernando Fernández <ferferga@hotmail.com>
2022-02-25 18:08:14 +0100

    feat(pinia): migrate socket store

--- frontend/store/socket.ts
+++ frontend/store/socket.ts
@@ -1,5 +1,5 @@
 import destr from 'destr';
-import { ActionTree, MutationTree } from 'vuex';
+import { defineStore } from 'pinia';
 
 let intervalId: number | null = null;
 
@@ -7,24 +7,6 @@ export interface WebSocketMessage {
   MessageType: string;
   Data?: Record<string, never>;
 }
-
-export interface SocketState {
-  instance: WebSocket | null;
-  isConnected: boolean;
-  messageType: string;
-  messageData?: Record<string, never> | number;
-  isConnecting: boolean;
-}
-
-export const defaultState = (): SocketState => ({
-  instance: null,
-  isConnected: false,
-  messageType: '',
-  messageData: undefined,
-  isConnecting: false
-});
-
-export const state = defaultState;
 
 /**
  * Clears the WebSocket reconnection interval
@@ -36,98 +18,108 @@ function clearInterval(): void {
   }
 }
 
-export const mutations: MutationTree<SocketState> = {
-  ONOPEN(state: SocketState, { instance }: { instance: WebSocket }) {
-    state.instance = instance;
-    state.isConnecting = false;
-    state.isConnected = true;
+type WebSocketMessageData = Record<string, never> | number | undefined;
+
+export interface SocketState {
+  instance: WebSocket | null;
+  isConnected: boolean;
+  messageType: string;
+  messageData?: WebSocketMessageData;
+  isConnecting: boolean;
+}
+
+export const socketStore = defineStore('socket', {
+  state: () => {
+    return {
+      instance: null,
+      isConnected: false,
+      messageType: '',
+      messageData: undefined,
+      isConnecting: false
+    } as SocketState;
   },
-  ONCLOSE(state: SocketState) {
-    if (state.instance) {
-      state.instance.close();
-      state.instance = null;
-    }
+  actions: {
+    /**
+     * Connect the WebSocket to the Jellyfin server
+     *
+     * @param url
+     * @param reconnect
+     */
+    connect(url: string, reconnect?: boolean): void {
+      const isDifferentWebsocket = this.instance && url !== this.instance.url;
 
-    state.isConnected = false;
-  },
-  ONMESSAGE(state: SocketState, { message }: { message: WebSocketMessage }) {
-    state.messageType = message.MessageType;
-    state.messageData = message.Data;
-  },
-  CONNECTING(state: SocketState) {
-    state.isConnecting = true;
-  }
-};
+      if (!this.instance || isDifferentWebsocket) {
+        if (isDifferentWebsocket) {
+          this.closeSocket();
+        }
 
-export const actions: ActionTree<SocketState, SocketState> = {
-  connectSocket(
-    { commit, state, dispatch },
-    { url, reconnect }: { url: string; reconnect?: boolean }
-  ) {
-    const isDifferentWebsocket = state.instance && url !== state.instance.url;
+        if (reconnect === undefined) {
+          reconnect = true;
+        }
 
-    if (!state.instance || isDifferentWebsocket) {
-      if (isDifferentWebsocket) {
-        dispatch('closeSocket');
-      }
+        console.info(`[WebSocket] Connecting to ${url}...`);
+        this.isConnecting = true;
 
-      if (reconnect === undefined) {
-        reconnect = true;
-      }
+        const instance = new WebSocket(url);
 
-      console.info(`[WebSocket] Connecting to ${url}...`);
-      commit('CONNECTING');
+        this.instance = instance;
+        this.isConnecting = false;
+        this.isConnected = true;
 
-      const instance = new WebSocket(url);
-
-      commit('ONOPEN', {
-        instance
-      });
-
-      instance.onopen = (_event: Event): void => {
-        const url = instance.url;
-
-        console.info(`[WebSocket] Connection established to ${url}!`);
-        clearInterval();
-
-        instance.onerror = (_event: Event): void => {
+        instance.onopen = (_event: Event): void => {
           const url = instance.url;
 
-          console.error(`[WebSocket] Error in the connection with ${url}`);
-          dispatch('closeSocket');
+          console.info(`[WebSocket] Connection established to ${url}!`);
+          clearInterval();
 
-          if (reconnect) {
-            console.log('[WebSocket] Reconnecting the socket in 3 seconds...');
-            intervalId = window.setInterval(() => {
-              dispatch('connectSocket', { url, reconnect: true });
-            }, 3000);
+          instance.onerror = (_event: Event): void => {
+            const url = instance.url;
+
+            console.error(`[WebSocket] Error in the connection with ${url}`);
+            this.closeSocket();
+
+            if (reconnect) {
+              console.log(
+                '[WebSocket] Reconnecting the socket in 3 seconds...'
+              );
+              intervalId = window.setInterval(() => {
+                this.connect(url, true);
+              }, 3000);
+            }
+          };
+        };
+
+        instance.onmessage = (event: MessageEvent): void => {
+          if (event.data) {
+            const message = destr(event.data) as WebSocketMessage;
+
+            if (message.MessageType !== undefined) {
+              this.setMessage(message.MessageType, message.Data);
+            }
           }
         };
-      };
+      }
+    },
+    setMessage(messageType: string, messageData: WebSocketMessageData) {
+      this.messageType = messageType;
+      this.messageData = messageData;
+    },
+    sendToSocket(message: string): void {
+      if (this.instance) {
+        this.instance.send(JSON.stringify(message));
+      }
+    },
+    closeSocket(): void {
+      if (this.instance) {
+        const url = this.instance?.url;
 
-      instance.onmessage = (event: MessageEvent): void => {
-        if (event.data) {
-          const message = destr(event.data) as WebSocketMessage;
-
-          if (message.MessageType !== undefined) {
-            commit('ONMESSAGE', { message });
-          }
-        }
-      };
-    }
-  },
-  sendToSocket({ state }, { message }: { message: WebSocketMessage }) {
-    if (state.instance) {
-      state.instance.send(JSON.stringify(message));
-    }
-  },
-  closeSocket({ commit, state }) {
-    if (state.instance) {
-      const url = state.instance?.url;
-
-      console.log(`[WebSocket] Closing connection to ${url}...`);
-      clearInterval();
-      commit('ONCLOSE');
+        console.log(`[WebSocket] Closing connection to ${url}...`);
+        clearInterval();
+        this.instance.close();
+        this.instance = null;
+        this.isConnected = false;
+        this.$reset();
+      }
     }
   }
-};
+});
