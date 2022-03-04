54583f3b24cf74aad2f4d14778683d35e72808dc
Fernando Fernández <ferferga@hotmail.com>
2022-03-04 11:55:44 +0100

    feat(pinia): migrate websocket store plugin

--- frontend/plugins/store/watchers.ts
+++ frontend/plugins/store/watchers.ts
@@ -1,8 +1,10 @@
 import { Plugin, Context } from '@nuxt/types';
 import watchAuth from './watchers/auth';
+import watchSocket from './watchers/socket';
 
 const piniaWatchers: Plugin = (ctx: Context) => {
   watchAuth(ctx);
+  watchSocket(ctx);
 };
 
 export default piniaWatchers;
---
+++ frontend/plugins/store/watchers/socket.ts
@@ -0,0 +1,75 @@
+import { ItemFields } from '@jellyfin/client-axios';
+import { Context } from '@nuxt/types';
+import { itemsStore, socketStore } from '~/store';
+
+/**
+ * Handle socket messages that are relevant to items inside the items store.
+ *
+ */
+export default function watchSocket(ctx: Context) {
+  const socket = socketStore();
+  const items = itemsStore();
+
+  /**
+   * Updates the items in the store after the websocket informs of it. Just a request is enough, as the Axios
+   * interceptors already handle updating the item in the store
+   *
+   * @param {string[]} itemIds - Ids of the items to update
+   */
+  async function updateStoreItems(itemIds: string[]): Promise<void> {
+    if (itemIds.length) {
+      await ctx.$api.items.getItems({
+        userId: ctx.$auth.user?.Id,
+        ids: itemIds,
+        fields: Object.keys(ItemFields) as ItemFields[]
+      });
+    }
+  }
+
+  /**
+   * Watch just the setMessage action in the socket store
+   */
+  socket.$onAction(({ name, after, store }) => {
+    after(() => {
+      if (name === 'setMessage') {
+        const messageType = store.messageType;
+        const messageData = store.messageData;
+        let itemsToUpdate: string[];
+
+        switch (messageType) {
+          case 'ForceKeepAlive': // Keep the websocket alive
+            socket.sendToSocket('KeepAlive');
+            break;
+          case 'LibraryChanged': // Update items when metadata changes
+            // @ts-expect-error -- The Data property doesn't describe its content
+            itemsToUpdate = messageData.ItemsUpdated.filter(
+              (itemId: string) => {
+                return Object.keys(items.byId).includes(itemId);
+              }
+            );
+
+            updateStoreItems(itemsToUpdate);
+            break;
+          case 'UserDataChanged': // Update items when their userdata is changed (like, mark as watched, etc)
+            // @ts-expect-error -- The Data property doesn't describe its content
+            itemsToUpdate = messageData.UserDataList.filter(
+              (updatedData: never) => {
+                // @ts-expect-error -- There are no typings for websocket returned data.
+                const itemId = updatedData.ItemId as string;
+
+                return Object.keys(items.byId).includes(itemId);
+              }
+            ).map((updatedData: never) => {
+              // @ts-expect-error -- There are no typings for websocket returned data.
+              return updatedData.ItemId as string;
+            });
+
+            updateStoreItems(itemsToUpdate);
+            break;
+          default:
+            break;
+        }
+      }
+    });
+  });
+}
