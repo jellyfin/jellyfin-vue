c52298b0ff45aa791900089150534400d02fc020
Fernando Fernández <ferferga@hotmail.com>
2022-04-07 14:58:18 +0200

    feat(pinia): migrate preference syncing logic

--- frontend/plugins/store/index.ts
+++ frontend/plugins/store/index.ts
@@ -1,11 +1,13 @@
 import { Plugin, Context } from '@nuxt/types';
 import persistence from './plugins/persistence';
+import preferencesSync from './plugins/preferencesSync';
 import watchAuth from './watchers/auth';
 import watchPlaybackReporting from './watchers/playbackReporting';
 import watchSocket from './watchers/socket';
 
 const piniaPlugins: Plugin = (ctx: Context) => {
   ctx.$pinia.use(persistence);
+  ctx.$pinia.use(preferencesSync);
   watchAuth(ctx);
   watchSocket(ctx);
   watchPlaybackReporting(ctx);
