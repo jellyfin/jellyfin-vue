f282d4e1c359d8ad3d0c40ade89142b7667dde06
Fernando Fernández <ferferga@hotmail.com>
2022-05-23 19:23:04 +0200

    fix: missing last character in commitlink
    Open commit link in new tab

--- frontend/components/Layout/CommitLink.vue
+++ frontend/components/Layout/CommitLink.vue
@@ -2,12 +2,14 @@
   <v-list-item
     v-if="commit"
     :href="'https://github.com/jellyfin/jellyfin-vue/commit/' + commit"
+    target="_blank"
+    rel="noopener noreferrer"
   >
     <v-list-item-action>
       <v-icon>mdi-github</v-icon>
     </v-list-item-action>
     <v-list-item-content>
-      <v-list-item-title v-text="'#' + commit.substring(0, 6)" />
+      <v-list-item-title v-text="'#' + commit.substring(0, 7)" />
     </v-list-item-content>
   </v-list-item>
 </template>
