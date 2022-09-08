99d43e3587c137fed8214ab44e0a95f131b8c379
Fernando Fernández <ferferga@hotmail.com>
2022-09-08 13:33:57 +0200

    chore(vite): scaffold project
    * Remove Jest, ESLint, Stylelint and Prettier to prepare for a further cleanup of all our testing and linting suites
    * Testing will be introduced later with vitest and/or Cypress

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  collectCoverage: true,
-  coverageReporters: ['text', 'cobertura', 'html', 'lcov'],
-  projects: ['<rootDir>/frontend/jest.config.js'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$'
-};
