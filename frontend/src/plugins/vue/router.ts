b435fab9844d7f130e01f10a0af58762d72e5f31
Fernando Fernández <ferferga@hotmail.com>
2022-09-08 22:33:29 +0200

    chore(vite): fix double Vue instances

--- frontend/src/plugins/vue/router.ts
+++ frontend/src/plugins/vue/router.ts
@@ -1,9 +1,6 @@
-import Vue from 'vue';
 import VueRouter from 'vue-router';
 import { setupLayouts } from 'virtual:generated-layouts';
 import generatedRoutes from '~pages';
-
-Vue.use(VueRouter);
 
 const router = new VueRouter({
   fallback: true,
