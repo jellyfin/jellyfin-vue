3d724b7082df8f0baf8eba38dbecdf25156f2ce8
Fernando Fernández <ferferga@hotmail.com>
2022-09-08 18:09:52 +0200

    chore(vite): cleanup main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,16 +3,27 @@ import App from '@/App.vue';
 import Vue from 'vue';
 import '@/plugins/vue/components';
 import '@/plugins/vue/directives';
-import store from '@/store';
+import { createPinia, PiniaVuePlugin } from 'pinia';
 import router from '@/plugins/vue/router';
 import i18n from '@/plugins/vue/i18n';
 
+/**
+ * Pinia
+ */
+const pinia = createPinia();
+Vue.use(PiniaVuePlugin);
+
+/**
+ * Vue global config
+ */
 Vue.config.performance = true;
 
+/**
+ * App initialization
+ */
 const app = new Vue({
-  auth,
   router,
-  store,
+  pinia,
   vuetify,
   i18n,
   render: (h) => h(App)
