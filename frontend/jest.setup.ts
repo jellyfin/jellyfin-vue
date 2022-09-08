cf05f5792dca1befcf8d1b70a142076ab2417907
Fernando Fernández <ferferga@hotmail.com>
2022-09-08 13:33:57 +0200

    chore(vite): scaffold project
    * Remove Jest, ESLint, Stylelint and Prettier to prepare for a further cleanup of all our testing and linting suites
    * Testing will be introduced later with vitest and/or Cypress

--- frontend/jest.setup.ts
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-
-Vue.use(Vuetify);
---
+++ frontend/vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite';
+import { createVuePlugin } from 'vite-plugin-vue2';
+import path from 'path';
+import Pages from 'vite-plugin-pages';
+import Layouts from 'vite-plugin-vue-layouts';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import Components from 'unplugin-vue-components/vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000
+  },
+  plugins: [
+    createVuePlugin(),
+    Pages({
+      routeStyle: 'nuxt'
+    }),
+    Layouts(),
+    // This plugin allows to autoimport vue components
+    Components({
+      /**
+       * The icons resolver finds icons components from 'unplugin-icons' using this convenction:
+       * {prefix}-{collection}-{icon} e.g. <i-mdi-thumb-up />
+       */
+      resolvers: [IconsResolver()]
+    }),
+    /**
+     * This plugin allows to use all icons from Iconify as vue components
+     * See: https://github.com/antfu/unplugin-icons
+     */
+    Icons({
+      compiler: 'vue2'
+    }),
+    VitePWA()
+  ],
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, './src')}/`
+    }
+  }
+});
