91aa5efb128347cc4c835e0a76330cb3b4bdcca1
Fernando Fernández <ferferga@hotmail.com>
2022-09-09 01:21:25 +0200

    chore(vite): remove functional components

--- frontend/src/components/Item/WatchedIndicator.vue
+++ frontend/src/components/Item/WatchedIndicator.vue
@@ -1,20 +1,8 @@
-<template functional>
+<template>
   <v-chip color="green" class="card-chip" small>
     <v-icon>mdi-check</v-icon>
   </v-chip>
 </template>
-
-<script lang="ts">
-import Vue from 'vue';
-import { VChip, VIcon } from 'vuetify/lib';
-
-Vue.component('VChip', VChip);
-Vue.component('VIcon', VIcon);
-
-export default Vue.extend({
-  name: 'WatchedIndicator'
-});
-</script>
 
 <style lang="scss" scoped>
 .card-chip {
