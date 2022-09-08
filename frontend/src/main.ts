63ddc2e357fdfedd6d5155e896e0a2da63602b40
Fernando Fernández <ferferga@hotmail.com>
2022-09-08 19:07:06 +0200

    chore(vite): plug vuetify and add ~ alias for backwards compat

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,27 @@
-import '@/assets/scss/app.scss';
-import App from '@/App.vue';
+import { createPinia, PiniaVuePlugin } from 'pinia';
 import Vue from 'vue';
+import App from '@/App.vue';
 import '@/plugins/vue/components';
 import '@/plugins/vue/directives';
-import { createPinia, PiniaVuePlugin } from 'pinia';
 import router from '@/plugins/vue/router';
 import i18n from '@/plugins/vue/i18n';
+import vuetify from '@/plugins/vue/vuetify';
+import piniaPlugins from '@/plugins/store';
+/**
+ * CSS Imports
+ *
+ * Order of imports is relevant. Vuetify needs to be loaded first, so it's variables are available to the rest of SCSS files.
+ */
+import 'vuetify/src/styles/styles.sass';
+//import '@/assets/global.scss';
+import '@/assets/transitions.scss';
 
 /**
  * Pinia
  */
 const pinia = createPinia();
 Vue.use(PiniaVuePlugin);
+pinia.use(piniaPlugins);
 
 /**
  * Vue global config
---
+++ frontend/src/plugins/vue/vuetify.ts
@@ -0,0 +1,51 @@
+/** Vuetify Plugin */
+import type { UserVuetifyPreset } from 'vuetify';
+import variables from '@/assets/variables.scss';
+import Vuetify from 'vuetify';
+import Vue from 'vue';
+
+export default createVuetify({
+  customVariables: [variables],
+  treeShake: true,
+  defaultAssets: false,
+  theme: {
+    dark: true,
+    default: 'dark',
+    disable: false,
+    themes: {
+      dark: {
+        primary: '#9d37c2',
+        secondary: '#2f3951',
+        accent: '#FF4081',
+        info: '#0099CC',
+        warning: '#FB8C00',
+        error: '#FF5252',
+        success: '#4CAF50',
+        background: '#14141F',
+        card: '#1c2331',
+        thumb: '#252e41'
+      },
+      light: {
+        primary: '#9d37c2',
+        secondary: '#424242',
+        accent: '#FF4081',
+        info: '#33b5e5',
+        warning: '#FB8C00',
+        error: '#FF5252',
+        success: '#4CAF50',
+        background: '#f2f2f2',
+        card: '#FFFFFF',
+        thumb: '#000000'
+      }
+    },
+    options: {
+      customProperties: true
+    }
+  }
+});
+
+/** Create Vuetify */
+export function createVuetify(options: UserVuetifyPreset): Vuetify {
+  Vue.use(Vuetify);
+  return new Vuetify(options);
+}
