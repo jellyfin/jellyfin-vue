e4be42d94ec88f6f93b566bb8d7d6e9bf855be6a
Fernando Fernández <ferferga@hotmail.com>
2022-09-08 20:30:35 +0200

    chore(vite): fix i18n and VueRouter imports

--- frontend/src/plugins/vue/router.ts
+++ frontend/src/plugins/vue/router.ts
@@ -1,6 +1,9 @@
+import Vue from 'vue';
 import VueRouter from 'vue-router';
 import { setupLayouts } from 'virtual:generated-layouts';
 import generatedRoutes from '~pages';
+
+Vue.use(VueRouter);
 
 const router = new VueRouter({
   fallback: true,
