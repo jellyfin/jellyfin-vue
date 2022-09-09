3f82327b50e58279ffd37f36542ebd5338513e1f
Fernando Fernández <ferferga@hotmail.com>
2022-09-09 20:28:38 +0200

    chore(vite): add and configure rollup-plugin-visualizer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import Pages from 'vite-plugin-pages';
@@ -11,58 +11,74 @@ import {
   VueUseComponentsResolver
 } from 'unplugin-vue-components/resolvers';
 import { VitePWA } from 'vite-plugin-pwa';
+import visualizer from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000
-  },
-  define: {
-    __COMMIT_HASH__: JSON.stringify(
-      process.env.COMMIT_HASH || process.env.CF_PAGES_COMMIT_SHA || ''
-    )
-  },
-  plugins: [
-    vue(),
-    Pages({
-      routeStyle: 'nuxt',
-      importMode: 'sync'
-    }),
-    Layouts({
-      importMode: () => 'sync'
-    }),
-    // This plugin allows to autoimport vue components
-    Components({
+export default defineConfig(async ({ mode }): Promise<UserConfig> => {
+  const config: UserConfig = {
+    server: {
+      port: 3000
+    },
+    define: {
+      __COMMIT_HASH__: JSON.stringify(
+        process.env.COMMIT_HASH || process.env.CF_PAGES_COMMIT_SHA || ''
+      )
+    },
+    plugins: [
+      vue(),
+      Pages({
+        routeStyle: 'nuxt',
+        importMode: 'sync'
+      }),
+      Layouts({
+        importMode: () => 'sync'
+      }),
+      // This plugin allows to autoimport vue components
+      Components({
+        /**
+         * The icons resolver finds icons components from 'unplugin-icons' using this convenction:
+         * {prefix}-{collection}-{icon} e.g. <i-mdi-thumb-up />
+         */
+        resolvers: [
+          IconsResolver(),
+          Vuetify3Resolver(),
+          VueUseComponentsResolver()
+        ]
+      }),
       /**
-       * The icons resolver finds icons components from 'unplugin-icons' using this convenction:
-       * {prefix}-{collection}-{icon} e.g. <i-mdi-thumb-up />
+       * This plugin allows to use all icons from Iconify as vue components
+       * See: https://github.com/antfu/unplugin-icons
        */
-      resolvers: [
-        IconsResolver(),
-        Vuetify3Resolver(),
-        VueUseComponentsResolver()
-      ]
-    }),
-    /**
-     * This plugin allows to use all icons from Iconify as vue components
-     * See: https://github.com/antfu/unplugin-icons
-     */
-    Icons({
-      compiler: 'vue3'
-    }),
-    VitePWA()
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
+      Icons({
+        compiler: 'vue3'
+      }),
+      VitePWA()
+    ],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+          plugins: [
+            mode === 'analyze'
+              ? // rollup-plugin-visualizer
+                // https://github.com/btd/rollup-plugin-visualizer
+                visualizer({
+                  open: true,
+                  filename: 'dist/stats.html',
+                  gzipSize: true,
+                  brotliSize: true
+                })
+              : undefined
+          ]
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, './src')}/`,
+        '~/': `${path.resolve(__dirname, './src')}/`
       }
     }
-  },
-  resolve: {
-    alias: {
-      '@/': `${path.resolve(__dirname, './src')}/`,
-      '~/': `${path.resolve(__dirname, './src')}/`
-    }
-  }
+  };
+  return config;
 });
