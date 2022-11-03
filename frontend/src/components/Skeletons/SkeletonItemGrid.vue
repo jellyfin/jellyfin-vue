994ac2c4e95bf6eed081c2017d517c96655178db
Fernando Fernández <ferferga@hotmail.com>
2022-11-03 23:32:39 +0100

    chore(vite): tweak sass config and comment more variable blocks
    * This is needed in order to finally have a workable build

--- frontend/src/components/Skeletons/SkeletonItemGrid.vue
+++ frontend/src/components/Skeletons/SkeletonItemGrid.vue
@@ -47,27 +47,27 @@ export default defineComponent({
   display: grid;
 }
 
-@media #{map-get($display-breakpoints, 'sm-and-down')} {
-  .card-grid-container {
-    grid-template-columns: repeat(3, minmax(calc(100% / 3), 1fr));
-  }
-}
+// @media #{map-get(settings.$display-breakpoints, 'sm-and-down')} {
+//   .card-grid-container {
+//     grid-template-columns: repeat(3, minmax(calc(100% / 3), 1fr));
+//   }
+// }
 
-@media #{map-get($display-breakpoints, 'sm-and-up')} {
-  .card-grid-container {
-    grid-template-columns: repeat(4, minmax(calc(100% / 4), 1fr));
-  }
-}
+// @media #{map-get(settings.$display-breakpoints, 'sm-and-up')} {
+//   .card-grid-container {
+//     grid-template-columns: repeat(4, minmax(calc(100% / 4), 1fr));
+//   }
+// }
 
-@media #{map-get($display-breakpoints, 'lg-and-up')} {
-  .card-grid-container {
-    grid-template-columns: repeat(6, minmax(calc(100% / 6), 1fr));
-  }
-}
+// @media #{map-get(settings.$display-breakpoints, 'lg-and-up')} {
+//   .card-grid-container {
+//     grid-template-columns: repeat(6, minmax(calc(100% / 6), 1fr));
+//   }
+// }
 
-@media #{map-get($display-breakpoints, 'xl-only')} {
-  .card-grid-container {
-    grid-template-columns: repeat(8, minmax(calc(100% / 8), 1fr));
-  }
-}
+// @media #{map-get(settings.$display-breakpoints, 'xl-only')} {
+//   .card-grid-container {
+//     grid-template-columns: repeat(8, minmax(calc(100% / 8), 1fr));
+//   }
+// }
 </style>
