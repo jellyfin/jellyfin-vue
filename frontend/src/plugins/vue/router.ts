3a79b0e06cc67ecce564eeb839997e6446cd69f2
Fernando Fernández <ferferga@hotmail.com>
2022-11-04 15:56:55 +0100

    chore(vue3): handle web history based on env var

--- frontend/src/plugins/vue/router.ts
+++ frontend/src/plugins/vue/router.ts
@@ -1,9 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory
+} from 'vue-router';
 import { setupLayouts } from 'virtual:generated-layouts';
 import generatedRoutes from 'virtual:generated-pages';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: __HISTORY_ROUTER_MODE__
+    ? createWebHistory()
+    : createWebHashHistory(),
   routes: setupLayouts(generatedRoutes)
 });
 
