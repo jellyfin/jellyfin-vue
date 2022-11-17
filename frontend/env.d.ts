6fa5c1aaf4d2ed672655727b0e8efcf7e5f6cefc
Fernando Fernández <ferferga@hotmail.com>
2022-11-17 19:45:35 +0100

    chore(vue3): target ES2022 and add eslint globals
    * Needed for top-level await at main.ts

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -4,8 +4,8 @@
 
 declare module '*.vue' {
   import { DefineComponent } from 'vue';
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>;
+
+  const component: DefineComponent<never, never, never>;
   export default component;
 }
 
