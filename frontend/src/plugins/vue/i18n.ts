12925741ae3c0caddcc33ef2db50dabe04a1a695
Fernando Fernández <ferferga@hotmail.com>
2022-11-22 10:29:10 +0100

    chore(vue3): draft remote plugin and minor QoL improvements
    * Add unicorn eslint plugin for stricter code style
    * Replace @jellyfin/client-axios with @jellyfin/sdk
    * Static bundle Vue-i18n
    * Avoid extraneous dependencies in code (eslint)
    * Draft router middlewares
    * Socket plugin is still pending to be completed
    * Remove Axe config (the plugin seems really outdated and problematic and it can always be reintroduced later)
    * Rename env.d.ts to environment.d.ts to comply with unicorn/prevent-abbreviations
    * Add eslint-plugin-you-dont-need-lodash-underscore to optimize lodash usage and remove unneeded cases
    * Create data-manipulation.ts in utils to store functions related to the manipulation of JavaScript data structures

--- frontend/src/plugins/vue/i18n.ts
+++ frontend/src/plugins/vue/i18n.ts
@@ -1,37 +1,18 @@
 import { createI18n } from 'vue-i18n';
+import messages from '@intlify/unplugin-vue-i18n/messages';
+import { useNavigatorLanguage } from '@vueuse/core';
+import { computed } from 'vue';
 
-const DEFAULT_LANGUAGE = 'en';
-const BROWSER_LANGUAGE = navigator?.language?.split('-')[0];
+const DEFAULT_LANGUAGE = 'en_US';
+const BROWSER_LANGUAGE = computed<string>(
+  () => useNavigatorLanguage().language.value || ''
+);
 
-/**
- *
- */
-function getMessages() {
-  const messages: any = {};
-  // See: https://vitejs.dev/guide/features.html#glob-import
-  const localeFiles = import.meta.glob('../../locales/*.json');
-
-  for (const path in localeFiles) {
-    // E.g: ../../locales/de.json
-    const pathParts = path.split('/');
-    // E.g: de.json -> de
-    const locale = pathParts[pathParts.length - 1].slice(0, -5);
-
-    // E.g: "de" => { "Hello": "Hallo" }
-    // @ts-expect-error - No types for this as strings are loaded dynamically at runtime
-    messages[locale] = localeFiles[path].default;
-  }
-
-  return messages;
-}
-
-const messages = getMessages();
 const i18n = createI18n({
-  locale: Object.keys(messages).includes(BROWSER_LANGUAGE)
-    ? BROWSER_LANGUAGE
+  locale: Object.keys(messages).includes(BROWSER_LANGUAGE.value)
+    ? BROWSER_LANGUAGE.value
     : DEFAULT_LANGUAGE,
   fallbackLocale: DEFAULT_LANGUAGE,
-  legacy: false, // Enables $t(), $tc(), etc in templates
   messages,
   globalInjection: true
 });
