1d8356dfb1da21611f27651f5d11229411aab3de
Fernando Fernández <ferferga@hotmail.com>
2022-11-22 10:29:10 +0100

    chore(vue3): draft remote plugin and minor QoL improvements
    * Add unicorn eslint plugin for stricter code style
    * Replace @jellyfin/client-axios with @jellyfin/sdk
    * Static bundle Vue-i18n
    * Avoid extraneous dependencies in code (eslint)
    * Draft router middlewares
    * Socket plugin is still pending to be completed
    * Remove Axe config (the plugin seems really outdated and problematic and it can always be reintroduced later)
    * Rename env.d.ts to environment.d.ts to comply with unicorn/prevent-abbreviations
    * Add eslint-plugin-you-dont-need-lodash-underscore to optimize lodash usage and remove unneeded cases
    * Create data-manipulation.ts in utils to store functions related to the manipulation of JavaScript data structures

---
+++ frontend/src/plugins/vue/remote/sdk/sdk-utils.ts
@@ -0,0 +1,76 @@
+import { Api, Jellyfin } from '@jellyfin/sdk';
+import { RemovableRef, useStorage } from '@vueuse/core';
+import { v4 as uuidv4 } from 'uuid';
+import { DeviceState } from './types';
+import { version } from '@/../package.json';
+import { BrowserDetector } from '@/utils/browserDetection';
+import { mergeExcludingUnknown } from '@/utils/data-manipulation';
+
+const state: RemovableRef<DeviceState> = useStorage(
+  'deviceProfile',
+  {
+    deviceId: uuidv4()
+  },
+  localStorage,
+  {
+    mergeDefaults: (storageValue, defaults) =>
+      mergeExcludingUnknown(storageValue, defaults)
+  }
+);
+
+const SDK = new Jellyfin({
+  clientInfo: {
+    name: 'Jellyfin Web (Vue)',
+    version: version
+  },
+  deviceInfo: {
+    name: getDeviceName(),
+    id: state.value.deviceId
+  }
+});
+
+/**
+ * Gets the device's name
+ *
+ * @returns Device name
+ */
+function getDeviceName(): string {
+  let deviceName = 'Unknown';
+  const $browser = new BrowserDetector();
+
+  // TODO: Replace with pattern matching once TC39 adopts the proposal
+  // See: https://github.com/tc39/proposal-pattern-matching
+  if ($browser.isChrome()) {
+    deviceName = 'Chrome';
+  } else if ($browser.isEdge() && !$browser.isChromiumBased()) {
+    deviceName = 'Edge (EdgeHTML)';
+  } else if ($browser.isEdge()) {
+    deviceName = 'Edge (Chromium)';
+  } else if ($browser.isFirefox()) {
+    deviceName = 'Firefox';
+  } else if ($browser.isApple() && !$browser.isMobile()) {
+    deviceName = 'Safari';
+  } else if ($browser.isWebOS()) {
+    deviceName = 'LG Smart TV';
+  } else if ($browser.isTizen()) {
+    deviceName = 'Samsung Smart TV';
+  } else if ($browser.isApple() && $browser.isMobile()) {
+    deviceName = 'iPhone';
+  } else if ($browser.isAndroid()) {
+    deviceName = 'Android';
+  }
+
+  return deviceName;
+}
+
+/**
+ * Connects to the given server with the given credentials without
+ * altering the app's API/SDK or axios instance.
+ */
+export function useOneTimeAPI(
+  ...arguments_: Parameters<typeof SDK.createApi>
+): Api {
+  return SDK.createApi(...arguments_);
+}
+
+export default SDK;
