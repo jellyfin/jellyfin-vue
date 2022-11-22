52ae0646ec3e0acec96d8c2f4f114f8c9094684c
Fernando Fernández <ferferga@hotmail.com>
2022-11-22 11:15:40 +0100

    chore(vue3): create ExternalJSONConfig plugin
    * Deprecate appInit nuxt plugin

---
+++ frontend/src/plugins/vue/config/index.ts
@@ -0,0 +1,29 @@
+import isNil from 'lodash/isNil';
+import { ExternalJSONConfig } from './types';
+import { useRemote } from '@/composables';
+
+const response = await fetch('./config.json');
+const data = (await response.json()) as ExternalJSONConfig;
+
+/**
+ * Parses external configuration file (config.json in public folder)
+ */
+export function createJSONConfig(): { install: () => Promise<void> } {
+  return {
+    install: async (): Promise<void> => {
+      const auth = useRemote().auth;
+      const defaultServers = data.defaultServerURLs;
+      const missingServers = defaultServers.filter((serverUrl) => {
+        const server = auth.servers.value.find(
+          (lsServer) => lsServer.PublicAddress === serverUrl
+        );
+
+        return isNil(server);
+      });
+
+      for (const serverUrl of missingServers) {
+        await auth.connectServer(serverUrl, true);
+      }
+    }
+  };
+}
