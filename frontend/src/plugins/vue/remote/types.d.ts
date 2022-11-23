57aa54b37afa34b9853eca91ed6a05e1ab20bb60
Fernando Fernández <ferferga@hotmail.com>
2022-11-23 14:13:55 +0100

    feat(vue3): create socket plugin inside remote plugin
    * Update VueUse to 9.6.0 to allow refs as socket URL

--- frontend/src/plugins/vue/remote/types.d.ts
+++ frontend/src/plugins/vue/remote/types.d.ts
@@ -1,9 +1,11 @@
 import RemotePluginAuthInstance from './auth';
 import RemotePluginAxiosInstance from './axios';
 import RemotePluginSDKInstance from './sdk';
+import RemotePluginSocketInstance from './socket';
 
 export interface RemotePlugin {
   axios: typeof RemotePluginAxiosInstance;
   sdk: typeof RemotePluginSDKInstance;
   auth: typeof RemotePluginAuthInstance;
+  socket: typeof RemotePluginSocketInstance;
 }
