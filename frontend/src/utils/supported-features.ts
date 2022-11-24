cf51e9ae6aa98c83fa750b5944605d57146d6f10
Fernando Fernández <ferferga@hotmail.com>
2022-11-24 14:45:52 +0100

    chore(vue3): reorganize last remnants of nuxt plugins

---
+++ frontend/src/utils/supported-features.ts
@@ -0,0 +1,79 @@
+import { BrowserDetector } from './browser-detection';
+
+export interface SupportedFeatures {
+  pictureInPicture: boolean;
+  airPlay: boolean;
+  googleCast: boolean;
+  playbackRate: boolean;
+  fullScreen: boolean;
+}
+
+const supportedFeatures: SupportedFeatures = {
+  pictureInPicture: false,
+  airPlay: false,
+  googleCast: false,
+  playbackRate: false,
+  fullScreen: false
+};
+
+const $browser = new BrowserDetector();
+
+/**
+ * Detects if the current platform supports showing fullscreen videos
+ *
+ * @returns - Whether fullscreen is supported or not
+ */
+function supportsFullscreen(): boolean {
+  // TVs don't support fullscreen.
+  if ($browser.isTv()) {
+    return false;
+  }
+
+  const element = document.documentElement;
+
+  return !!(
+    element.requestFullscreen ||
+    // @ts-expect-error -- Non-standard property
+    element.mozRequestFullScreen ||
+    // @ts-expect-error -- Non-standard property
+    element.webkitRequestFullscreen ||
+    // @ts-expect-error -- Non-standard property
+    element.msRequestFullscreen ||
+    // @ts-expect-error -- Non-standard property
+    document.createElement('video').webkitEnterFullscreen
+  );
+}
+
+const video = document.createElement('video');
+
+if (
+  // Check non-standard Safari PiP support
+  // @ts-expect-error - Non-standard functions doesn't have typings
+  (typeof video.webkitSupportsPresentationMode === 'function' &&
+    // @ts-expect-error - Non-standard functions doesn't have typings
+    video.webkitSupportsPresentationMode('picture-in-picture') &&
+    // @ts-expect-error - Non-standard functions doesn't have typings
+    typeof video.webkitSetPresentationMode === 'function') ||
+  // Check standard PiP support
+  document.pictureInPictureEnabled
+) {
+  supportedFeatures.pictureInPicture = true;
+}
+
+if (typeof video.playbackRate === 'number') {
+  supportedFeatures.playbackRate = true;
+}
+
+if (supportsFullscreen()) {
+  supportedFeatures.fullScreen = true;
+}
+
+if ($browser.isApple()) {
+  supportedFeatures.airPlay = true;
+}
+
+if ($browser.isChrome() || ($browser.isEdge() && $browser.isChromiumBased())) {
+  supportedFeatures.googleCast = true;
+}
+
+export default supportedFeatures;
