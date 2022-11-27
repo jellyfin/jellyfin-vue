1d96b739b1cd832f226718ddc78ea51047f23a2c
Fernando Fernández <ferferga@hotmail.com>
2022-11-28 00:51:29 +0100

    feat(vue3): login flow
    * Fix transitions not playing
    * Remove vee-validate and use Vuetify validation
    * Create icon component while waiting for https://github.com/vuetifyjs/vuetify/pull/16168
    * Fix Vuetify themes

--- frontend/src/composables/index.ts
+++ frontend/src/composables/index.ts
@@ -1,3 +1,4 @@
 export { usei18n } from './use-i18n';
-export { useSnackbar } from './use-snackbar';
 export { useRemote } from './use-remote';
+export { useSnackbar } from './use-snackbar';
+export { useVuetify } from './use-vuetify';
--- frontend/src/layouts/fullpage.vue
+++ frontend/src/layouts/fullpage.vue
@@ -3,8 +3,8 @@
     <v-main>
       <router-view-transition />
     </v-main>
-    <v-footer app color="rgba(0, 0, 0, 0)">
-      <locale-switcher large top />
+    <v-footer app color="background">
+      <locale-switcher top />
     </v-footer>
   </v-app>
 </template>
