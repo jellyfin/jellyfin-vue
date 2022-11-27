d69354aeda4c05f74e36ae9fcf5a9419928f075a
Fernando Fernández <ferferga@hotmail.com>
2022-11-28 00:51:29 +0100

    feat(vue3): login flow
    * Fix transitions not playing
    * Remove vee-validate and use Vuetify validation
    * Create icon component while waiting for https://github.com/vuetifyjs/vuetify/pull/16168
    * Fix Vuetify themes

--- frontend/src/components/Wizard/WizardAdminAccount.vue
+++ frontend/src/components/Wizard/WizardAdminAccount.vue
@@ -1,93 +1,98 @@
 <template>
-  <validation-observer v-slot="{ invalid }">
-    <v-form v-model="validInputs">
-      <validation-provider v-slot="{ errors }" name="username" rules="required">
-        <v-text-field
-          v-model="admin.Name"
-          variant="outlined"
-          :label="$t('username')"
-          type="username"
-          :error-messages="errors"
-          required />
-      </validation-provider>
-      <validation-provider
-        v-slot="{ errors }"
-        rules="bothPasswordsSame:@confirm">
-        <v-text-field
-          v-model="admin.Password"
-          variant="outlined"
-          :label="$t('password')"
-          :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
-          :type="showPassword ? 'text' : 'password'"
-          :error-messages="errors"
-          @click:append="() => (showPassword = !showPassword)" />
-      </validation-provider>
+  <v-form v-model="valid">
+    <v-text-field
+      v-model="admin.Name"
+      variant="outlined"
+      :label="$t('username')"
+      type="username"
+      :rules="RequiredRule"
+      required />
+    <v-text-field
+      v-model="admin.Password"
+      variant="outlined"
+      :label="$t('password')"
+      :append-icon="showPassword ? IconEyeOff : IconEye"
+      :type="showPassword ? 'text' : 'password'"
+      @click:append="() => (showPassword = !showPassword)" />
 
-      <validation-provider v-slot="{ errors }" name="confirm" rules="">
-        <v-text-field
-          v-model="passwordCheck"
-          variant="outlined"
-          :label="$t('wizard.confirmPassword')"
-          :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
-          :type="showPassword ? 'text' : 'password'"
-          :error-messages="errors"
-          @click:append="() => (showPassword = !showPassword)" />
-      </validation-provider>
+    <v-text-field
+      v-model="passwordCheck"
+      variant="outlined"
+      :label="$t('wizard.confirmPassword')"
+      :append-icon="showPassword ? IconEyeOff : IconEye"
+      :type="showPassword ? 'text' : 'password'"
+      :rules="SamePasswordRules"
+      @click:append="() => (showPassword = !showPassword)" />
 
-      <v-btn color="secondary" @click="$emit('previous-step', { step: 2 })">
-        {{ $t('previous') }}
-      </v-btn>
-      <v-btn
-        color="primary"
-        :disabled="invalid"
-        :loading="loading"
-        @click="createAdminAccount">
-        {{ $t('next') }}
-      </v-btn>
-    </v-form>
-  </validation-observer>
+    <v-btn color="secondary" @click="$emit('previous-step', { step: 2 })">
+      {{ $t('previous') }}
+    </v-btn>
+    <v-btn
+      color="primary"
+      :disabled="!valid"
+      :loading="loading"
+      @click="createAdminAccount">
+      {{ $t('next') }}
+    </v-btn>
+  </v-form>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { useSnackbar } from '@/composables';
+<script setup lang="ts">
+import { getStartupApi } from '@jellyfin/sdk/lib/utils/api/startup-api';
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { useRemote, useSnackbar } from '@/composables';
+import IconEyeOff from '~icons/mdi/eye-off';
+import IconEye from '~icons/mdi/eye';
 
-export default defineComponent({
-  setup() {
-    return {
-      useSnackbar
-    };
-  },
-  data() {
-    return {
-      admin: {
-        Name: '',
-        Password: ''
-      },
-      passwordCheck: '',
-      showPassword: false,
-      loading: false
-    };
-  },
-  methods: {
-    async createAdminAccount(): Promise<void> {
-      this.loading = true;
+interface StepEvent {
+  step: number;
+}
 
-      try {
-        this.auth.setAxiosHeader();
+const { t } = useI18n();
+const api = useRemote().sdk.api;
 
-        await this.$api.startup.updateStartupUser({
-          startupUserDto: this.admin
-        });
+const emit = defineEmits<{
+  (e: 'previous-step', value: StepEvent): void;
+  (e: 'step-complete', value: StepEvent): void;
+}>();
 
-        this.$emit('step-complete', { step: 2 });
-      } catch (error) {
-        console.error(error);
-        this.useSnackbar(this.$t('wizard.setAdminError'), 'error');
-      }
+const valid = ref(false);
+const admin = ref({
+  Name: '',
+  Password: ''
+});
+const passwordCheck = ref('');
+const showPassword = ref(false);
+const loading = ref(false);
 
-      this.loading = false;
+const SamePasswordRules = [
+  (v: string): boolean | string =>
+    v === admin.value.Password || t('validation.bothPasswordsSame')
+];
+const RequiredRule = [
+  (v: string): boolean | string => !!v.trim() || t('validation.required')
+];
+
+/**
+ * Creates the admin account during the wizard process
+ */
+async function createAdminAccount(): Promise<void> {
+  loading.value = true;
+
+  if (api) {
+    try {
+      await getStartupApi(api).updateStartupUser({
+        startupUserDto: admin.value
+      });
+
+      emit('step-complete', { step: 2 });
+    } catch (error) {
+      console.error(error);
+      useSnackbar(t('wizard.setAdminError'), 'error');
     }
   }
-});
+
+  loading.value = false;
+}
 </script>
