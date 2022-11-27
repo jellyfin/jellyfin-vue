39dccf7352b3367713b88ff001cf0a1665a9ca23
Fernando Fernández <ferferga@hotmail.com>
2022-11-28 00:51:29 +0100

    feat(vue3): login flow
    * Fix transitions not playing
    * Remove vee-validate and use Vuetify validation
    * Create icon component while waiting for https://github.com/vuetifyjs/vuetify/pull/16168
    * Fix Vuetify themes

--- frontend/src/components/Forms/LoginForm.vue
+++ frontend/src/components/Forms/LoginForm.vue
@@ -1,119 +1,102 @@
 <template>
   <div>
-    <validation-observer v-slot="{ invalid }">
-      <v-form :disabled="loading" @submit.prevent="userLogin">
-        <validation-provider
-          v-slot="{ errors }"
-          name="username"
-          :rules="rules.username">
-          <v-text-field
-            v-if="isEmpty(user)"
-            v-model="login.username"
-            variant="outlined"
-            hide-details
-            autofocus
-            autocomplete="username"
-            :label="$t('username')"
-            :error-messages="errors" />
-        </validation-provider>
-        <v-text-field
-          v-model="login.password"
-          variant="outlined"
-          hide-details
-          class="mt-4"
-          autocomplete="current-password"
-          :label="$t('password')"
-          :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
-          :type="showPassword ? 'text' : 'password'"
-          @click:append="() => (showPassword = !showPassword)" />
-        <v-checkbox
-          v-model="login.rememberMe"
-          hide-details
-          class="mt-6 mb-6"
-          :label="$t('login.rememberMe')" />
-        <v-row align="center" no-gutters>
-          <v-col class="mr-2">
-            <v-btn
-              v-if="isEmpty(user)"
-              to="/server/select"
-              nuxt
-              block
-              size="large">
-              {{ $t('login.changeServer') }}
-            </v-btn>
-            <v-btn v-else block size="large" @click="$emit('change')">
-              {{ $t('login.changeUser') }}
-            </v-btn>
-          </v-col>
-          <v-col class="mr-2">
-            <v-btn
-              :disabled="invalid"
-              :loading="loading"
-              block
-              size="large"
-              color="primary"
-              type="submit">
-              {{ $t('signIn') }}
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-form>
-    </validation-observer>
+    <v-form v-model="valid" :disabled="loading" @submit.prevent="userLogin">
+      <v-text-field
+        v-if="isEmpty(user)"
+        v-model="login.username"
+        variant="outlined"
+        hide-details
+        autofocus
+        autocomplete="username"
+        :label="$t('username')"
+        :rules="rules" />
+      <v-text-field
+        v-model="login.password"
+        variant="outlined"
+        hide-details
+        class="mt-4"
+        autocomplete="current-password"
+        :label="$t('password')"
+        :append-icon="showPassword ? IconEyeOff : IconEye"
+        :type="showPassword ? 'text' : 'password'"
+        @click:append="() => (showPassword = !showPassword)" />
+      <v-checkbox
+        v-model="login.rememberMe"
+        hide-details
+        class="mt-6 mb-6"
+        color="primary"
+        :label="$t('login.rememberMe')" />
+      <v-row align="center" no-gutters>
+        <v-col class="mr-2">
+          <v-btn v-if="isEmpty(user)" to="/server/select" block size="large">
+            {{ $t('login.changeServer') }}
+          </v-btn>
+          <v-btn v-else block size="large" @click="$emit('change')">
+            {{ $t('login.changeUser') }}
+          </v-btn>
+        </v-col>
+        <v-col class="mr-2">
+          <v-btn
+            :disabled="!valid"
+            :loading="loading"
+            block
+            size="large"
+            color="primary"
+            type="submit">
+            {{ $t('signIn') }}
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
 import isEmpty from 'lodash/isEmpty';
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
 import { UserDto } from '@jellyfin/sdk/lib/generated-client';
+import { useRemote } from '@/composables';
+import IconEyeOff from '~icons/mdi/eye-off';
+import IconEye from '~icons/mdi/eye';
 
-export default defineComponent({
-  props: {
-    user: {
-      type: Object as () => UserDto,
-      default(): UserDto {
-        return {};
-      }
-    }
-  },
-  data() {
-    return {
-      login: {
-        username: '',
-        password: '',
-        rememberMe: true
-      },
-      showPassword: false,
-      loading: false,
-      rules: {
-        username: {
-          required: true
-        }
-      }
-    };
-  },
-  methods: {
-    async userLogin(): Promise<void> {
-      if (!isEmpty(this.user)) {
-        // If we have a user from the public user selector, set it as login
-        this.login.username = this.user.Name || '';
-      }
+const remote = useRemote();
+const { t } = useI18n();
 
-      this.loading = true;
-
-      try {
-        await this.$remote.auth.loginUser(
-          this.login.username,
-          this.login.password,
-          this.login.rememberMe
-        );
-      } finally {
-        this.loading = false;
-      }
-    },
-    isEmpty(value: Record<never, never>): boolean {
-      return isEmpty(value);
-    }
+const props = defineProps({
+  user: {
+    required: true,
+    type: Object as () => UserDto
   }
 });
+
+const valid = ref(false);
+const login = ref({ username: '', password: '', rememberMe: true });
+const showPassword = ref(false);
+const loading = ref(false);
+const rules = [
+  (v: string): boolean | string => !!v.trim() || t('validation.required')
+];
+
+/**
+ * Login the user into the client
+ */
+async function userLogin(): Promise<void> {
+  if (!isEmpty(props.user)) {
+    // If we have a user from the public user selector, set it as login
+    login.value.username = props.user.Name || '';
+  }
+
+  loading.value = true;
+
+  try {
+    await remote.auth.loginUser(
+      login.value.username,
+      login.value.password,
+      login.value.rememberMe
+    );
+  } finally {
+    loading.value = false;
+  }
+}
 </script>
