dc78957ab7ffe85f43de95a21f992d89d039160b
Fernando Fernández <ferferga@hotmail.com>
2022-11-29 19:36:00 +0100

    chore(vite): use lodash-es instead of lodash
    * In Nuxt, lodash-es rendered a bigger bundle size than lodash, so we kept it. With full complete of our building tools, we can switch back to lodash-es and remove the eslint plugin

--- frontend/src/utils/data-manipulation.ts
+++ frontend/src/utils/data-manipulation.ts
@@ -1,4 +1,4 @@
-import defaultsDeep from 'lodash/defaultsDeep';
+import { defaultsDeep } from 'lodash-es';
 
 /**
  * Merge 2 objects, excluding the keys from the destination that are not present in source
