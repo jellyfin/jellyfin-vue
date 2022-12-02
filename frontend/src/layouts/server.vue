9a18678ed745720c56e0b706bca10ba3ec81d71e
Fernando Fernández <ferferga@hotmail.com>
2022-12-02 14:36:12 +0100

    chore(vue3): first reimplementation of music playback's swiper

---
+++ frontend/src/layouts/server.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view-transition />
+    </v-main>
+    <v-footer app color="background">
+      <locale-switcher top />
+    </v-footer>
+  </v-app>
+</template>
