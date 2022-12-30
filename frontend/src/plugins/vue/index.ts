e3fc8aba01151a51f104a5e81f5ef0d2ac524c6e
Fernando Fernández <ferferga@hotmail.com>
2022-12-30 16:01:38 +0100

    chore(vue3): move router history config to JSON

--- frontend/src/plugins/vue/index.ts
+++ frontend/src/plugins/vue/index.ts
@@ -3,4 +3,3 @@ export { default as pinia } from './pinia';
 export { default as vuetify } from './vuetify';
 export { default as router } from './router';
 export { default as createRemote } from './remote';
-export { default as createJSONConfig } from './config';
