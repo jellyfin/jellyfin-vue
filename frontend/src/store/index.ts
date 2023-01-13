a8414036da30937955c94df0e65e293fd0bfa45d
Fernando Fernández <ferferga@hotmail.com>
2023-01-13 10:15:43 +0100

    chore(vue3): deprecate pinia
    Pinia was great in Vue 2. However, since in Vue 3 there's no longer a global instance,
    doing reactivity and watching outside the app context is much easier and simplifies tremendously
    our app initialization context (for things like playback watchers) and persistence.
    
    Also, Pinia didn't allow us to be explicit with what we wanted to expose and, by default,
    all the methods, state properties and getters were accessible by everyone. With TypeScript classes,
    we have full control of the external visibility of every property and
    how we want to expose everything (readonly or not readonly?)
    
    We lose the ability to have DevTools support, but it's not like we used it that much anyway

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,6 +1,44 @@
-export * from './clientSettings';
-export * from './userViews';
-export * from './homeSection';
-export * from './items';
+import clientSettings from './clientSettings';
+import items from './items';
+import taskManager from './taskManager';
+import userLibraries from './userLibraries';
+import playbackManager from './playbackManager';
+
+/**
+ * Get global instance of clientSettings store
+ */
+export function clientSettingsStore(): typeof clientSettings {
+  return clientSettings;
+}
+
+/**
+ * Get global instance of items store
+ */
+export function itemsStore(): typeof items {
+  return items;
+}
+
+/**
+ * Get global instance of taskManager store
+ */
+export function taskManagerStore(): typeof taskManager {
+  return taskManager;
+}
+
+/**
+ * Get global instance of userLibraries store
+ */
+export function userLibrariesStore(): typeof userLibraries {
+  return userLibraries;
+}
+
+/**
+ * Get global instance of playbackManager store
+ */
+export function playbackManagerStore(): typeof playbackManager {
+  return playbackManager;
+}
+
+export type { HomeSection } from './userLibraries';
+export { TaskType } from './taskManager';
 export * from './playbackManager';
-export * from './taskManager';
