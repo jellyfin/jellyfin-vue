bbc143cd1b6629d7e03d587246e0bf8b023b9989
Fernando Fernández <ferferga@hotmail.com>
2023-01-13 10:18:50 +0100

    chore(vue3): update plugin imports
    With the pinia deprecation, there's no need to distinguish between vue and store plugins

--- frontend/src/composables/use-remote.ts
+++ frontend/src/composables/use-remote.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-imports */
-import { remoteInstance } from '@/plugins/vue/remote';
+import { remoteInstance } from '@/plugins/remote';
 /* eslint-enable no-restricted-imports */
 
 /**
