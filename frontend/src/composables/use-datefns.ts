17609c5b20436c0fa69a5ca21c97ed5953cdacdc
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2023-01-21 17:30:47 +0100

    fix(usedatefns): argument merging logic and better locale matching between vuei18n and datefns

--- frontend/src/composables/use-datefns.ts
+++ frontend/src/composables/use-datefns.ts
@@ -2,12 +2,28 @@ import { computed, ComputedRef } from 'vue';
 import * as datefnslocales from 'date-fns/locale';
 import { usei18n } from './use-i18n';
 
+type keyofDateFnsLocale = keyof typeof datefnslocales;
+
 /**
- * Get date-fns locale based on our locale key
+ * Get dateFns locale
+ *
+ * We need this due to the differences between the vue i18n and date-fns locales.
+ *  1st case: when there is a date-fns locale equals to the i18n, it's all good
+ *  2nd case: vue i18n got locales such as fr-FR but date-fns got them as frFR. That's to match those
+ *  3rd case: if date-fns doesn't have the frFR locale, it's to check if fr exists
+ * Without it, we strongly limit those matching
  */
 function getDateFnsLocale(locale: string): Locale | undefined {
-  //@ts-expect-error - Some of our locales are not present in datefns.
-  return datefnslocales[locale];
+  if (datefnslocales[locale as keyofDateFnsLocale]) {
+    // If the i18n locale exactly matches the date-fns one
+    return datefnslocales[locale as keyofDateFnsLocale];
+  } else if (datefnslocales[locale.replace('-', '') as keyofDateFnsLocale]) {
+    // Removes the potential dash to match for instance "en-US" from i18n to "enUS" for date-fns
+    return datefnslocales[locale.replace('-', '') as keyofDateFnsLocale];
+  } else if (datefnslocales[locale.split('-')[0] as keyofDateFnsLocale]) {
+    // Takes the part before the potential dash to try, for instance "fr-FR" in i18n to "fr"
+    return datefnslocales[locale.split('-')[0] as keyofDateFnsLocale];
+  }
 }
 
 /**
