0d5f4698f791e6e5637168e76973b6de25b8bc2f
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2023-01-27 22:02:42 +0100

    feat: drop shaka player and natively play direct streaming audio

--- frontend/src/components/Playback/PlayerElement.vue
+++ frontend/src/components/Playback/PlayerElement.vue
@@ -14,28 +14,16 @@
 </template>
 
 <script setup lang="ts">
-/**
- * Shaka Player needs to be unmounted for it to be properly disposed. However,
- * since this component will always be mounted in the app (as it's inside the top-level App component),
- * it doesn't make sense to add code for handling the unmount process.
- */
-// @ts-expect-error - This module doesn't have typings
-import muxjs from 'mux.js';
-// @ts-expect-error - This module doesn't have typings
-import shakaPlayer from 'shaka-player/dist/shaka-player.compiled';
 import { computed, watch } from 'vue';
 import { isNil } from 'lodash-es';
+import { useI18n } from 'vue-i18n';
 import { playbackManagerStore, RepeatMode } from '@/store';
 import { mediaElementRef } from '@/store/playbackManager';
 import { getImageInfo } from '@/utils/images';
+import { useSnackbar } from '@/composables';
 
 const playbackManager = playbackManagerStore();
-let isShakaAttached = false;
-const shaka = new shakaPlayer.Player();
-
-window.muxjs = muxjs;
-window.player = shaka;
-shakaPlayer.polyfill.installAll();
+const { t } = useI18n();
 
 const mediaElement = computed<'audio' | 'video' | undefined>(() => {
   if (playbackManager.currentlyPlayingMediaType === 'Audio') {
@@ -46,51 +34,37 @@ const mediaElement = computed<'audio' | 'video' | undefined>(() => {
 });
 
 /**
- * If the player is a video element and we're
+ * If the player is a video element and we're in the PiP player or fullscreen video playback, we need to ensure the DOM elements are mounted before the teleport target is ready
  */
-const teleportTarget = computed<'body' | '.video-container'>(() => {
-  return mediaElement.value === 'audio' ? 'body' : '.video-container';
-});
+const teleportTarget = computed<'body' | '.video-container'>(() =>
+  mediaElement.value === 'audio' ? 'body' : '.video-container'
+);
 
-const posterUrl = computed<string>(() => {
-  return !isNil(playbackManager.currentItem) && mediaElement.value === 'video'
+const posterUrl = computed<string>(() =>
+  !isNil(playbackManager.currentItem) && mediaElement.value === 'video'
     ? getImageInfo(playbackManager.currentItem, {
         preferBackdrop: true
       }).url || ''
-    : '';
-});
+    : ''
+);
 
 const isLoopingOnce = computed(
   () => playbackManager.repeatMode === RepeatMode.RepeatOne
 );
 
-watch(mediaElement, async () => {
-  await shaka.detach();
-  isShakaAttached = false;
-
-  if (mediaElementRef.value) {
-    await shaka.attach(mediaElementRef.value);
-
-    if (playbackManager.currentSourceUrl) {
-      await shaka.load(playbackManager.currentSourceUrl);
-    }
-
-    isShakaAttached = true;
-  }
-});
-
-watch(
-  () => playbackManager.currentItemIndex,
-  async () => {
-    await shaka.unload();
-  }
-);
-
 watch(
   () => playbackManager.currentSourceUrl,
-  async () => {
-    if (isShakaAttached && playbackManager.currentSourceUrl) {
-      await shaka.load(playbackManager.currentSourceUrl);
+  () => {
+    if (
+      mediaElementRef.value &&
+      playbackManager.currentSourceUrl &&
+      playbackManager.currentMediaSource?.SupportsDirectPlay &&
+      playbackManager.currentlyPlayingMediaType === 'Audio'
+    ) {
+      mediaElementRef.value.src = playbackManager.currentSourceUrl;
+    } else {
+      useSnackbar(t('errors.cantPlayItem'), 'error');
+      playbackManager.stop();
     }
   }
 );
