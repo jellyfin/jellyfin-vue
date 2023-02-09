8376bd728aa2166e65f2466d7466e6fdd096619f
aweebs <8809837+aweebs@users.noreply.github.com>
2023-02-09 00:24:15 -0800

    chore: fix up metadata index page

--- frontend/src/pages/metadata/index.vue
+++ frontend/src/pages/metadata/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row class="pa-4 metadata">
     <v-col cols="3" class="tree-view-container">
-      <!-- TODO: Wait for Vuetify 3.1 -->
+      <!-- TODO: Wait for tree view to be implemented in Vuetify 3 (https://github.com/vuetifyjs/vuetify/issues/13518) -->
       <!-- <v-treeview
         :items="items"
         :load-children="fetchItems"
@@ -19,54 +19,69 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { BaseItemDto } from '@jellyfin/sdk/lib/generated-client';
-import { getUserLibraryApi } from '@jellyfin/sdk/lib/utils/api/user-library-api';
 import { getLibraryApi } from '@jellyfin/sdk/lib/utils/api/library-api';
+import { getItemsApi } from '@jellyfin/sdk/lib/utils/api/items-api';
 
 type ITreeNode = {
-  id: string | number | undefined;
+  id: string;
   name: string | null | undefined;
   children?: ITreeNode[];
 };
 
+interface Data {
+  items: ITreeNode[];
+  itemId: string | undefined;
+}
+
 export default defineComponent({
-  data() {
+  data(): Data {
     return {
-      items: [] as ITreeNode[],
-      itemId: ''
+      items: [],
+      itemId: undefined
     };
   },
   async created() {
     const folders = (
       await this.$remote.sdk.newUserApi(getLibraryApi).getMediaFolders()
-    ).data.Items as BaseItemDto[];
+    ).data.Items;
 
-    this.items = folders.map((dir, index) => {
+    if (!folders) {
+      return;
+    }
+
+    this.items = folders.map((dir) => {
+      if (!dir.Id) {
+        throw new Error('received item without id');
+      }
+
       return {
-        id: dir.Id || index,
+        id: dir.Id,
         name: dir.Name,
-        children: [] as ITreeNode[]
+        children: []
       };
     });
   },
   methods: {
     async fetchItems(node: ITreeNode): Promise<void> {
-      const libraryItems = (
-        (
-          await this.$remote.sdk.newUserApi(getUserLibraryApi).getItem(
-            { userId: this.$remote.auth.currentUserId || '', itemId: '' },
-            {
-              query: {
-                ParentId: node.id,
-                SortBy: 'SortName'
-              }
-            }
-          )
-        ).data as unknown as { Items: BaseItemDto[] }
-      ).Items;
+      if (!node.children) {
+        throw new Error('expanding a node without children');
+      }
 
-      (node.children as ITreeNode[]).push(
+      const libraryItems =
+        (
+          await this.$remote.sdk.newUserApi(getItemsApi).getItemsByUserId({
+            userId: this.$remote.auth.currentUserId || '',
+            parentId: node.id,
+            sortBy: ['SortName']
+          })
+        ).data.Items ?? [];
+
+      node.children.push(
         ...libraryItems.map((item) => {
+          if (!item.Id) {
+            throw new Error('received item without it');
+          }
+
           const baseObject = { id: item.Id, name: item.Name };
 
           return item.IsFolder
@@ -78,6 +93,7 @@ export default defineComponent({
         })
       );
     },
+
     handleAction(ids: string[]): void {
       this.$data.itemId = ids[0];
     }
