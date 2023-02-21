6c4b6b06ad554ce58d3db968b232757cc1fdb8f8
aweebs <8809837+aweebs@users.noreply.github.com>
2023-02-20 22:55:47 -0800

    chore: Convert devices.vue to Composition api
    * Removes the old data table implementation, using a normal table now.
    * Add delete confirmation to devices page
    * Remove selected device info component and only use the table for removing devices

--- frontend/src/pages/settings/devices.vue
+++ frontend/src/pages/settings/devices.vue
@@ -2,181 +2,182 @@
   <settings-page page-title="settings.devices.devices">
     <template #actions>
       <v-btn
-        v-if="devices"
+        v-if="devices.length > 0"
         color="error"
         variant="elevated"
         class="ml-a"
+        :loading="loading"
         @click="deleteAllDevices">
-        {{ $t('settings.devices.deleteAll') }}
+        {{ t('settings.devices.deleteAll') }}
       </v-btn>
       <v-btn
         rel="noreferrer noopener"
         variant="elevated"
         href="https://jellyfin.org/docs/general/server/devices.html"
         target="_blank">
-        {{ $t('settings.help') }}
+        {{ t('settings.help') }}
       </v-btn>
     </template>
     <template #content>
       <v-col>
-        <!-- TODO: Wait for Vuetify implementation (https://github.com/vuetifyjs/vuetify/issues/13479) -->
-        <!-- <v-data-table
-          :headers="headers"
-          :items="devices"
-          @click:row="setSelectedDevice"
-        >
-          <template #item.DateLastActivity="{ item }">
-            <p class="text-capitalize-first-letter mb-0">
-              {{
-                $dateFns.formatRelative(
-                  $dateFns.parseJSON(item.DateLastActivity),
-                  new Date(),
-                  {
-                    locale: $i18n.locale
-                  }
-                )
-              }}
-            </p>
-          </template>
-        </v-data-table> -->
+        <v-table>
+          <thead>
+            <tr>
+              <th v-for="{ text, value } in headers" :key="value">
+                {{ text }}
+              </th>
+              <th><!--for delete button--></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="device in devices" :key="device.Id ?? undefined">
+              <td v-for="{ value } in headers" :key="value">
+                {{
+                  value !== 'DateLastActivity'
+                    ? device[value]
+                    : useDateFns(
+                        formatRelative,
+                        parseJSON(device[value] ?? 'unknown'),
+                        new Date()
+                      ).value
+                }}
+              </td>
+              <td>
+                <v-btn
+                  color="error"
+                  :disabled="loading"
+                  @click="confirmDelete = device.Id ?? undefined">
+                  {{ t('settings.devices.delete') }}
+                </v-btn>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
       </v-col>
-      <v-dialog v-model="deviceInfoDialog" width="fit-content">
-        <selected-device-info
-          v-if="selectedDevice.Name"
-          :selected-device="selectedDevice"
-          :is-dialog="true"
-          @close-dialog="closeDialog"
-          @delete-selected="deleteSelectedDevice" />
+      <v-dialog
+        width="auto"
+        :model-value="confirmDelete !== undefined"
+        @update:model-value="confirmDelete = undefined">
+        <v-card>
+          <v-card-text>
+            {{ t('settings.devices.deleteConfirm') }}
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" :loading="loading" @click="confirmDeletion">
+              {{ t('confirm') }}
+            </v-btn>
+            <v-btn :loading="loading" @click="confirmDelete = undefined">
+              {{ t('cancel') }}
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-dialog>
     </template>
   </settings-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<route lang="yaml">
+meta:
+  admin: true
+</route>
+
+<script setup lang="ts">
+import { parseJSON, formatRelative } from 'date-fns';
+import { computed, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
 import { DeviceInfo } from '@jellyfin/sdk/lib/generated-client';
 import { getDevicesApi } from '@jellyfin/sdk/lib/utils/api/devices-api';
-import { useRemote, useSnackbar } from '@/composables';
+import { useDateFns, useRemote, useSnackbar } from '@/composables';
 
-export default defineComponent({
-  async setup() {
-    const remote = useRemote();
+const { t } = useI18n();
+const remote = useRemote();
 
-    const devices = (await remote.sdk.newUserApi(getDevicesApi).getDevices())
-      .data.Items;
+const devices = ref(
+  (await remote.sdk.newUserApi(getDevicesApi).getDevices()).data.Items ?? []
+);
 
-    return {
-      devices,
-      useSnackbar
-    };
+const loading = ref(false);
+/** The device id to confirm being deleted (will be 'all' if all are being deleted) */
+const confirmDelete = ref<string>();
+
+const headers = computed<{ text: string; value: keyof DeviceInfo }[]>(() => [
+  {
+    text: t('settings.devices.userName'),
+    value: 'LastUserName'
   },
-  data() {
-    return {
-      selectedDevice: {} as DeviceInfo,
-      deviceInfoDialog: false
-    };
-  },
-  computed: {
-    headers(): { text: string; value: string }[] {
-      return [
-        {
-          text: this.$t('settings.devices.userName'),
-          value: 'LastUserName'
-        },
-        { text: this.$t('settings.devices.deviceName'), value: 'Name' },
-        { text: this.$t('settings.devices.appName'), value: 'AppName' },
-        { text: this.$t('settings.devices.appVersion'), value: 'AppVersion' },
-        {
-          text: this.$t('settings.devices.lastActive'),
-          value: 'DateLastActivity'
-        }
-      ];
-    }
-  },
-  methods: {
-    async deleteDevice(item: DeviceInfo): Promise<void> {
-      try {
-        await this.$remote.sdk.newUserApi(getDevicesApi).deleteDevice({
-          id: item.Id || ''
-        });
-
-        this.useSnackbar(
-          this.$t('settings.devices.deleteDeviceSuccess'),
-          'success'
-        );
-        this.devices =
-          (await this.$remote.sdk.newUserApi(getDevicesApi).getDevices()).data
-            .Items || [];
-      } catch (error) {
-        this.useSnackbar(
-          this.$t('settings.devices.deleteDeviceError'),
-          'error'
-        );
-
-        console.error(error);
-      }
-    },
-    async deleteAllDevices(): Promise<void> {
-      try {
-        for (const device of this.devices ?? []) {
-          if (this.$remote.sdk.deviceInfo.id === device.Id) {
-            return;
-          }
-
-          await this.$remote.sdk
-            .newUserApi(getDevicesApi)
-            .deleteDevice({ id: device.Id || '' });
-        }
-
-        this.useSnackbar(
-          this.$t('settings.devices.deleteAllDevicesSuccess'),
-          'success'
-        );
-
-        this.devices =
-          (await this.$remote.sdk.newUserApi(getDevicesApi).getDevices()).data
-            .Items || [];
-      } catch (error) {
-        this.useSnackbar(
-          this.$t('settings.devices.deleteAllDevicesError'),
-          'error'
-        );
-
-        console.error(error);
-      }
-    },
-    setSelectedDevice(device: DeviceInfo): void {
-      this.selectedDevice = device;
-      this.deviceInfoDialog = true;
-    },
-    closeDialog(): void {
-      this.deviceInfoDialog = false;
-      this.selectedDevice = {};
-    },
-    async deleteSelectedDevice(): Promise<void> {
-      try {
-        await this.$remote.sdk.newUserApi(getDevicesApi).deleteDevice({
-          id: this.selectedDevice.Id || ''
-        });
-
-        this.useSnackbar(
-          this.$t('settings.devices.deleteDeviceSuccess'),
-          'success'
-        );
-
-        this.selectedDevice = {};
-
-        this.devices =
-          (await this.$remote.sdk.newUserApi(getDevicesApi).getDevices()).data
-            .Items || [];
-      } catch (error) {
-        this.useSnackbar(this.$t('deleteDeviceError'), 'error');
-
-        console.error(error);
-      }
-
-      this.deviceInfoDialog = false;
-    }
+  { text: t('settings.devices.deviceName'), value: 'Name' },
+  { text: t('settings.devices.appName'), value: 'AppName' },
+  { text: t('settings.devices.appVersion'), value: 'AppVersion' },
+  {
+    text: t('settings.devices.lastActive'),
+    value: 'DateLastActivity'
   }
-});
+]);
+
+/**
+ * Deletes all remembered devices
+ */
+async function deleteAllDevices(): Promise<void> {
+  loading.value = true;
+
+  try {
+    for (const device of devices.value) {
+      if (!device.Id || remote.sdk.deviceInfo.id === device.Id) {
+        continue;
+      }
+
+      await remote.sdk
+        .newUserApi(getDevicesApi)
+        .deleteDevice({ id: device.Id });
+    }
+
+    useSnackbar(t('settings.devices.deleteAllDevicesSuccess'), 'success');
+
+    devices.value =
+      (await remote.sdk.newUserApi(getDevicesApi).getDevices()).data.Items ??
+      [];
+  } catch (error) {
+    useSnackbar(t('settings.devices.deleteAllDevicesError'), 'error');
+    console.error(error);
+  } finally {
+    loading.value = false;
+  }
+}
+
+/**
+ * Deletes the selected device
+ */
+async function deleteDevice(deviceId: string): Promise<void> {
+  loading.value = true;
+
+  try {
+    await remote.sdk.newUserApi(getDevicesApi).deleteDevice({ id: deviceId });
+
+    useSnackbar(t('settings.devices.deleteDeviceSuccess'), 'success');
+
+    devices.value =
+      (await remote.sdk.newUserApi(getDevicesApi).getDevices()).data.Items ??
+      [];
+  } catch (error) {
+    useSnackbar(t('deleteDeviceError'), 'error');
+    console.error(error);
+  } finally {
+    loading.value = false;
+  }
+}
+
+/**
+ * Confirms deleteion of a single device or all
+ */
+async function confirmDeletion(): Promise<void> {
+  if (!confirmDelete.value) {
+    return;
+  }
+
+  await (confirmDelete.value === 'all'
+    ? deleteAllDevices()
+    : deleteDevice(confirmDelete.value));
+
+  confirmDelete.value = undefined;
+}
 </script>
