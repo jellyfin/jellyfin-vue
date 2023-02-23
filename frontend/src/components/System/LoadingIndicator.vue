fd4ea0700232e731bb3ccfd12361bcb7446c64a2
Fernando Fernández <ferferga@hotmail.com>
2023-02-23 14:09:01 +0100

    feat(vue3): create global loading state
    Show a v-progress-linear when an Axios request is happening or suspense
    is loading content.
    
    In a similar fashion to GitHub's and YouTube loading indicators

---
+++ frontend/src/components/System/LoadingIndicator.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-progress-linear
+    :active="isLoading"
+    indeterminate
+    absolute
+    location="top"
+    color="primary" />
+</template>
+
+<script setup lang="ts">
+import { useLoading } from '@/composables';
+
+const { isLoading } = useLoading();
+</script>
