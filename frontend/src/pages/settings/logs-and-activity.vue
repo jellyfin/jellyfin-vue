e396fc66c6887dd15fa4b734dcd62e942c164ae5
aweebs <8809837+aweebs@users.noreply.github.com>
2023-02-26 06:52:27 -0800

    chore: convert logs and activities to setup composition api

--- frontend/src/pages/settings/logs-and-activity.vue
+++ frontend/src/pages/settings/logs-and-activity.vue
@@ -1,60 +1,54 @@
 <template>
-  <settings-page page-title="settingsSections.logsAndActivity.logsAndActivity">
+  <settings-page page-title="settingsSections.logs.name">
     <template #content>
       <v-col md="6" class="pt-0 pb-4">
         <v-fade-transition group>
           <h2 key="logs-title" class="text-h6 mb-2">
-            {{ $t('settings.logsAndActivity.logs') }}
+            {{ t('settings.logsAndActivity.logs') }}
           </h2>
           <v-list
-            v-if="logFiles && logFiles.length > 0"
+            v-if="logs.status === 'loaded' && logs.data.length > 0"
             key="log-list"
             lines="two"
             class="mb-2">
-            <v-list-group>
-              <v-list-item
-                v-for="file in logFiles"
-                :key="file.Name"
-                :href="getLogFileLink(file.Name)"
-                target="_blank"
-                rel="noopener">
-                <v-avatar>
-                  <v-icon>
-                    <i-mdi-file />
-                  </v-icon>
-                </v-avatar>
-                <v-list-item-title>
-                  {{ file.Name }}
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                  {{ getFormattedLogDate(file.DateModified) }}
-                </v-list-item-subtitle>
-                <v-list-item-action>
-                  <v-icon>
-                    <i-mdi-open-in-new />
-                  </v-icon>
-                </v-list-item-action>
-              </v-list-item>
-            </v-list-group>
+            <v-list-item
+              v-for="file in logs.data"
+              :key="file.Name ?? undefined"
+              :href="getLogFileLink(file.Name ?? '')"
+              target="_blank"
+              rel="noopener">
+              <v-avatar>
+                <v-icon>
+                  <i-mdi-file />
+                </v-icon>
+              </v-avatar>
+              <v-list-item-title>
+                {{ file.Name }}
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ getFormattedLogDate(file.DateModified) }}
+              </v-list-item-subtitle>
+              <v-list-item-action>
+                <v-icon>
+                  <i-mdi-open-in-new />
+                </v-icon>
+              </v-list-item-action>
+            </v-list-item>
           </v-list>
-          <v-card
-            v-else-if="loadingLogsStatus.status === 'loaded'"
-            key="no-log-card">
+          <v-card v-else-if="logs.status === 'loaded'" key="no-log-card">
             <v-card-title>
-              {{ $t('settings.logsAndActivity.noLogsFound') }}
+              {{ t('settings.logsAndActivity.noLogsFound') }}
             </v-card-title>
           </v-card>
-          <v-card
-            v-else-if="loadingLogsStatus.status === 'error'"
-            key="error-log-card">
+          <v-card v-else-if="logs.status === 'error'" key="error-log-card">
             <v-card-title>
               <v-icon color="error" class="pr-2">
                 <i-mdi-alert-circle />
               </v-icon>
-              {{ $t('settings.logsAndActivity.failedGetLogs') }}
+              {{ t('settings.logsAndActivity.failedGetLogs') }}
             </v-card-title>
-            <v-card-text v-if="loadingLogsStatus.errorMessage">
-              {{ loadingLogsStatus.errorMessage }}
+            <v-card-text v-if="logs.message">
+              {{ logs.message }}
             </v-card-text>
           </v-card>
         </v-fade-transition>
@@ -62,51 +56,53 @@
       <v-col md="6" class="pt-0 pb-4">
         <v-fade-transition group>
           <h2 key="activity-title" class="text-h6 mb-2">
-            {{ $t('settings.logsAndActivity.activity') }}
+            {{ t('settings.logsAndActivity.activity') }}
           </h2>
           <v-list
-            v-if="activityList && activityList.length > 0"
+            v-if="
+              activityList.status === 'loaded' && activityList.data.length > 0
+            "
             key="activity-list"
             lines="two"
             class="mb-2"
             disabled>
-            <v-list-group>
-              <v-list-item v-for="activity in activityList" :key="activity.Id">
-                <v-avatar :color="getColorFromSeverity(activity.Severity)">
-                  <v-icon :icon="getIconFromType(activity.Type)" />
-                </v-avatar>
-                <v-list-item-title>
-                  {{ activity.Name }}
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                  {{ activity.ShortOverview }}
+            <v-list-item
+              v-for="activity in activityList.data"
+              :key="activity.Id">
+              <v-avatar :color="getColorFromSeverity(activity.Severity)">
+                <v-icon :icon="getIconFromActivityType(activity.Type)" />
+              </v-avatar>
+              <v-list-item-title>
+                {{ activity.Name }}
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ activity.ShortOverview }}
+              </v-list-item-subtitle>
+              <v-list-item-action>
+                <v-list-item-subtitle class="text-capitalize-first-letter">
+                  {{ getFormattedActivityDate(activity.Date) }}
                 </v-list-item-subtitle>
-                <v-list-item-action>
-                  <v-list-item-subtitle class="text-capitalize-first-letter">
-                    {{ getFormattedActivityDate(activity.Date) }}
-                  </v-list-item-subtitle>
-                </v-list-item-action>
-              </v-list-item>
-            </v-list-group>
+              </v-list-item-action>
+            </v-list-item>
           </v-list>
           <v-card
-            v-else-if="loadingActivityStatus.status === 'loaded'"
+            v-else-if="activityList.status === 'loaded'"
             key="no-activity-card">
             <v-card-title>
-              {{ $t('settings.logsAndActivity.noActivityFound') }}
+              {{ t('settings.logsAndActivity.noActivityFound') }}
             </v-card-title>
           </v-card>
           <v-card
-            v-else-if="loadingActivityStatus.status === 'error'"
+            v-else-if="activityList.status === 'error'"
             key="error-activity-card">
             <v-card-title>
               <v-icon color="error" class="pr-2">
                 <i-mdi-alert-circle />
               </v-icon>
-              {{ $t('settings.logsAndActivity.failedGetActivity') }}
+              {{ t('settings.logsAndActivity.failedGetActivity') }}
             </v-card-title>
-            <v-card-text>
-              {{ loadingActivityStatus.errorMessage }}
+            <v-card-text v-if="activityList.message">
+              {{ activityList.message }}
             </v-card-text>
           </v-card>
         </v-fade-transition>
@@ -120,9 +116,14 @@ meta:
   admin: true
 </route>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { LogLevel } from '@jellyfin/sdk/lib/generated-client';
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useTheme } from 'vuetify';
+import {
+  ActivityLogEntry,
+  LogFile,
+  LogLevel
+} from '@jellyfin/sdk/lib/generated-client';
 import { getActivityLogApi } from '@jellyfin/sdk/lib/utils/api/activity-log-api';
 import { getSystemApi } from '@jellyfin/sdk/lib/utils/api/system-api';
 import { format, formatRelative, parseJSON } from 'date-fns';
@@ -136,98 +137,142 @@ import IMdiStop from 'virtual:icons/mdi/stop';
 import IMdiHelp from 'virtual:icons/mdi/help';
 import { useDateFns, useRemote } from '@/composables';
 
-interface LoadingStatus {
-  status: 'loading' | 'loaded' | 'error';
-  errorMessage?: string;
+type LoadingStatus<T> =
+  | { status: 'loading' }
+  | { status: 'loaded'; data: T }
+  | { status: 'error'; message?: string };
+
+const { t } = useI18n();
+const route = useRoute();
+const remote = useRemote();
+const theme = useTheme();
+
+route.meta.title = t('settingsSections.logs.name');
+
+const logs = ref<LoadingStatus<LogFile[]>>({ status: 'loading' });
+const activityList = ref<LoadingStatus<ActivityLogEntry[]>>({
+  status: 'loading'
+});
+
+/**
+ * Return a UI colour given log severity
+ */
+function getColorFromSeverity(severity: LogLevel | undefined): string {
+  switch (severity) {
+    case LogLevel.Trace: {
+      return theme.current.value.colors.success;
+    }
+    case LogLevel.Debug: {
+      return theme.current.value.colors.accent;
+    }
+    case LogLevel.Information: {
+      return theme.current.value.colors.info;
+    }
+    case LogLevel.Warning: {
+      return theme.current.value.colors.warning;
+    }
+    case LogLevel.Error: {
+      return theme.current.value.colors.error;
+    }
+    case LogLevel.Critical: {
+      return theme.current.value.colors.secondary;
+    }
+    default: {
+      return theme.current.value.colors.primary;
+    }
+  }
 }
 
-export default defineComponent({
-  async setup() {
-    const { t } = useI18n();
-    const route = useRoute();
-    const remote = useRemote();
+/**
+ * Gets an icon given an activity type
+ */
+function getIconFromActivityType(
+  type: string | undefined | null
+): typeof IMdiLogin {
+  switch (type) {
+    case 'SessionStarted': {
+      return IMdiLogin;
+    }
+    case 'SessionEnded': {
+      return IMdiLogout;
+    }
+    case 'UserPasswordChanged': {
+      return IMdiLock;
+    }
+    case 'VideoPlayback': {
+      return IMdiPlay;
+    }
+    case 'VideoPlaybackStopped': {
+      return IMdiStop;
+    }
+    default: {
+      return IMdiHelp;
+    }
+  }
+}
 
-    route.meta.title = t('settingsSections.logs.name');
+/**
+ * Format activitydates
+ */
+function getFormattedActivityDate(date: string | undefined): string {
+  return date
+    ? useDateFns(formatRelative, parseJSON(date), new Date()).value
+    : '';
+}
 
-    const minDate = new Date();
+/**
+ * Format log dates
+ */
+function getFormattedLogDate(date: string | undefined): string {
+  return date ? useDateFns(format, parseJSON(date), 'Ppp').value : '';
+}
 
-    minDate.setDate(minDate.getDate() - 7);
+/**
+ * Creates a link to the given type of log file
+ */
+function getLogFileLink(name: string): string {
+  return `${remote.sdk.api?.basePath}/System/Logs/Log?name=${name}&api_key=${remote.auth.currentUserToken}`;
+}
 
-    const activityList = (
-      await remote.sdk
-        .newUserApi(getActivityLogApi)
-        .getLogEntries({ minDate: minDate.toISOString() })
-    ).data.Items;
+/**
+ * Fetches logs
+ */
+async function fetchLogs(): Promise<void> {
+  logs.value = { status: 'loading' };
+
+  try {
     const logFiles = (await remote.sdk.newUserApi(getSystemApi).getServerLogs())
       .data;
 
-    return { activityList, logFiles };
-  },
-  data() {
-    return {
-      loadingLogsStatus: { status: 'error' } as LoadingStatus,
-      loadingActivityStatus: { status: 'error' } as LoadingStatus
-    };
-  },
-  methods: {
-    getColorFromSeverity(severity: LogLevel | undefined): string {
-      switch (severity) {
-        case LogLevel.Trace: {
-          return this.$vuetify.theme.current.colors.success;
-        }
-        case LogLevel.Debug: {
-          return this.$vuetify.theme.current.colors.accent;
-        }
-        case LogLevel.Information: {
-          return this.$vuetify.theme.current.colors.info;
-        }
-        case LogLevel.Warning: {
-          return this.$vuetify.theme.current.colors.warning;
-        }
-        case LogLevel.Error: {
-          return this.$vuetify.theme.current.colors.error;
-        }
-        case LogLevel.Critical: {
-          return this.$vuetify.theme.current.colors.secondary;
-        }
-        default: {
-          return this.$vuetify.theme.current.colors.primary;
-        }
-      }
-    },
-    getIconFromType(type: string | undefined | null): typeof IMdiLogin {
-      switch (type) {
-        case 'SessionStarted': {
-          return IMdiLogin;
-        }
-        case 'SessionEnded': {
-          return IMdiLogout;
-        }
-        case 'UserPasswordChanged': {
-          return IMdiLock;
-        }
-        case 'VideoPlayback': {
-          return IMdiPlay;
-        }
-        case 'VideoPlaybackStopped': {
-          return IMdiStop;
-        }
-        default: {
-          return IMdiHelp;
-        }
-      }
-    },
-    getFormattedActivityDate(date: string | undefined): string {
-      return date
-        ? useDateFns(formatRelative, parseJSON(date), new Date()).value
-        : '';
-    },
-    getFormattedLogDate(date: string | undefined): string {
-      return date ? useDateFns(format, parseJSON(date), 'Ppp').value : '';
-    },
-    getLogFileLink(name: string): string {
-      return `${this.$remote.sdk.api?.basePath}/System/Logs/Log?name=${name}&api_key=${this.$remote.auth.currentUserToken}`;
-    }
+    logs.value = { status: 'loaded', data: logFiles };
+  } catch {
+    logs.value = { status: 'error' };
   }
-});
+}
+
+/**
+ * Fetches activities
+ */
+async function fetchActivity(): Promise<void> {
+  const minDate = new Date();
+
+  minDate.setDate(minDate.getDate() - 7);
+  activityList.value = { status: 'loading' };
+
+  try {
+    const activityListEntries =
+      (
+        await remote.sdk
+          .newUserApi(getActivityLogApi)
+          .getLogEntries({ minDate: minDate.toISOString() })
+      ).data.Items ?? [];
+
+    activityList.value = { status: 'loaded', data: activityListEntries };
+  } catch {
+    activityList.value = { status: 'error' };
+  }
+}
+
+await fetchLogs();
+await fetchActivity();
 </script>
