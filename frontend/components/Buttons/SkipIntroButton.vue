b5e524a25cd95ab7a43e83d7eb5617b01a60bf22
Gibah Joseph <gibahjoe@gmail.com>
2023-03-04 18:52:03 +0100

    chore: Cleaned up implementation. Fixed hover staying forever and also made the fill animation more accurate

--- frontend/components/Buttons/SkipIntroButton.vue
+++ frontend/components/Buttons/SkipIntroButton.vue
@@ -41,7 +41,6 @@ export default Vue.extend({
   },
   data() {
     return {
-      skipIntroClicked: false,
       visibleDuration:
         (this.intro?.HideSkipPromptAt || 0) -
         (this.intro?.ShowSkipPromptAt || 0),
@@ -51,22 +50,23 @@ export default Vue.extend({
   computed: {
     ...mapStores(playbackManagerStore),
     show(): boolean {
-      if (this.skipIntroClicked) {
-        return false;
-      }
-
       if (!this.intro) {
         return false;
       }
 
       if (this.hover) {
-        return this.intro.IntroStart <= this.intro.IntroEnd;
+        return (this.playbackManager?.currentTime || 0) < this.intro.IntroEnd;
       }
 
       return (
         (this.playbackManager?.currentTime || 0) >=
           this.intro.ShowSkipPromptAt &&
         (this.playbackManager?.currentTime || 0) <= this.intro.HideSkipPromptAt
+      );
+    },
+    visibleDurationLeft() {
+      return (
+        this.intro.HideSkipPromptAt - (this.playbackManager?.currentTime || 0)
       );
     }
   },
@@ -81,7 +81,7 @@ export default Vue.extend({
     'playbackManager.status'(): void {
       switch (this.playbackManager.status) {
         case PlaybackStatus.Playing:
-          if (this.show) {
+          if (this.show && !this.hover) {
             this.animate();
           }
 
@@ -94,18 +94,23 @@ export default Vue.extend({
   },
   methods: {
     skipIntro() {
-      this.skipIntroClicked = true;
       this.playbackManager.changeCurrentTime(this.intro.IntroEnd);
     },
     animate() {
       setTimeout(() => {
-        const button = (this.$refs.skipIntro as Vue).$el;
+        const button = (this.$refs.skipIntro as Vue)?.$el as HTMLElement;
+
+        button?.style?.setProperty(
+          '--transition-duration',
+          this.visibleDurationLeft + 's'
+        );
         button?.classList?.remove('deanimate');
         button?.classList?.add('animate');
       }, 500);
     },
     unanimate() {
       const button = (this.$refs.skipIntro as Vue).$el;
+
       button?.classList?.remove('animate');
       button?.classList?.add('deanimate');
     },
@@ -148,7 +153,7 @@ export default Vue.extend({
 }
 
 .skip-intro.deanimate::before {
-  width: 0%;
+  width: 0;
   transition-duration: var(--fade-duration);
 }
 
