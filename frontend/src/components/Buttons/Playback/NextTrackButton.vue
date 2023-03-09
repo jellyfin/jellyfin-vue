a4128050850d182797104be36ed751417bf8ae36
Fernando Fernández <ferferga@hotmail.com>
2023-03-09 20:33:00 +0100

    refactor: factorize playback buttons into components

---
+++ frontend/src/components/Buttons/Playback/NextTrackButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-btn
+    icon
+    :size="props.size"
+    :disabled="!playbackManager.nextItem"
+    @click="playbackManager.setNextTrack">
+    <v-icon :size="size">
+      <i-mdi-skip-next />
+    </v-icon>
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import { playbackManagerStore } from '@/store';
+
+const props = defineProps<{ size?: string }>();
+const playbackManager = playbackManagerStore();
+</script>
