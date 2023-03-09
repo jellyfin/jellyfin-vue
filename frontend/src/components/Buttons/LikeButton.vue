d46e547e454598639daee4bb90af2d38b5af5a62
Fernando Fernández <ferferga@hotmail.com>
2023-03-09 11:16:37 +0100

    refactor: make LikeButton computed
    TODO: This will only work properly with the playback elements, since the items store must be completely reactive for this

--- frontend/src/components/Buttons/LikeButton.vue
+++ frontend/src/components/Buttons/LikeButton.vue
@@ -2,60 +2,48 @@
   <v-btn
     :icon="isFavorite ? IMdiHeart : IMdiHeartOutline"
     size="small"
-    @click.stop.prevent="toggleFavorite" />
+    :loading="loading"
+    @click.stop.prevent="isFavorite = !isFavorite" />
 </template>
 
 <script lang="ts" setup>
-import { ref, watch } from 'vue';
+import { computed, ref } from 'vue';
 import { BaseItemDto } from '@jellyfin/sdk/lib/generated-client';
 import { getUserLibraryApi } from '@jellyfin/sdk/lib/utils/api/user-library-api';
 import IMdiHeart from 'virtual:icons/mdi/heart';
 import IMdiHeartOutline from 'virtual:icons/mdi/heart-outline';
-import { useI18n } from 'vue-i18n';
-import { useRemote, useSnackbar } from '@/composables';
+import { useRemote } from '@/composables';
 
 const props = defineProps<{ item: BaseItemDto }>();
-
-const isFavorite = ref(false);
 const remote = useRemote();
-const { t } = useI18n();
+const loading = ref(false);
 
-watch(
-  () => props.item.UserData?.IsFavorite,
-  () => {
-    isFavorite.value = props.item.UserData?.IsFavorite || false;
+const isFavorite = computed({
+  get() {
+    return props.item.UserData?.IsFavorite ?? false;
   },
-  { immediate: true }
-);
+  async set(newValue) {
+    try {
+      if (!props.item.Id) {
+        throw new Error('Item has no Id');
+      }
 
-/**
- * Toggles the favorite on the server
- */
-async function toggleFavorite(): Promise<void> {
-  try {
-    if (!props.item.Id) {
-      throw new Error('Item has no Id');
+      loading.value = true;
+
+      await (newValue
+        ? remote.sdk.newUserApi(getUserLibraryApi).markFavoriteItem({
+            userId: remote.auth.currentUserId || '',
+            itemId: props.item.Id
+          })
+        : remote.sdk.newUserApi(getUserLibraryApi).unmarkFavoriteItem({
+            userId: remote.auth.currentUserId || '',
+            itemId: props.item.Id
+          }));
+      loading.value = false;
+    } catch {
+    } finally {
+      loading.value = false;
     }
-
-    if (!isFavorite.value) {
-      isFavorite.value = true;
-
-      await remote.sdk.newUserApi(getUserLibraryApi).markFavoriteItem({
-        userId: remote.auth.currentUserId || '',
-        itemId: props.item.Id
-      });
-    } else {
-      isFavorite.value = false;
-
-      await remote.sdk.newUserApi(getUserLibraryApi).unmarkFavoriteItem({
-        userId: remote.auth.currentUserId || '',
-        itemId: props.item.Id
-      });
-    }
-  } catch {
-    useSnackbar(t('unableToToggleLike'), 'error');
-
-    isFavorite.value = !isFavorite.value;
   }
-}
+});
 </script>
