a7a86bba939d5015875325f309dc86c2be061acf
Fernando Fernández <ferferga@hotmail.com>
2023-03-09 20:33:00 +0100

    refactor: factorize playback buttons into components

---
+++ frontend/src/components/Buttons/Playback/RepeatButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn
+    icon
+    :size="size"
+    :color="playbackManager.isRepeating ? 'primary' : undefined"
+    @click="playbackManager.toggleRepeatMode">
+    <v-icon :size="size" :icon="repeatIcon" />
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import IMdiRepeatOnce from 'virtual:icons/mdi/repeat-once';
+import IMdiRepeat from 'virtual:icons/mdi/repeat';
+import { playbackManagerStore } from '@/store';
+import { RepeatMode } from '@/store/playbackManager';
+
+defineProps<{ size?: string }>();
+
+const playbackManager = playbackManagerStore();
+
+const repeatIcon = computed(() =>
+  playbackManager.repeatMode === RepeatMode.RepeatOne
+    ? IMdiRepeatOnce
+    : IMdiRepeat
+);
+</script>
---
+++ frontend/src/components/Buttons/Playback/ShuffleButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn
+    icon
+    :size="size"
+    :color="playbackManager.isShuffling ? 'primary' : undefined"
+    @click="playbackManager.toggleShuffle">
+    <v-icon :size="size">
+      <i-mdi-shuffle />
+    </v-icon>
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import { playbackManagerStore } from '@/store';
+
+defineProps<{ size?: string }>();
+
+const playbackManager = playbackManagerStore();
+</script>
