23b98c2d6b1e6bfbc016f7af4c3e4ddcbc49413b
aweebs <8809837+aweebs@users.noreply.github.com>
2023-03-19 18:53:16 -0700

    chore: Added type assert for external config

--- frontend/src/utils/external-config.ts
+++ frontend/src/utils/external-config.ts
@@ -1,12 +1,45 @@
-import destr from 'destr';
 import { isNil } from 'lodash-es';
 
 interface ExternalJSONConfig {
-  defaultServerURLs: Array<string>;
+  defaultServerURLs: string[];
   routerMode: 'hash' | 'history';
 }
 
 let externalConfig: ExternalJSONConfig | undefined;
+
+/**
+ * Asserts that the config parameter is a valid configuration shape
+ */
+function validateJsonConfig(
+  config: unknown
+): asserts config is ExternalJSONConfig {
+  if (typeof config !== 'object' || !config) {
+    throw new Error('Expected not null or defined config');
+  }
+
+  if (
+    !('defaultServerURLs' in config) ||
+    !Array.isArray(config.defaultServerURLs)
+  ) {
+    throw new Error('Expected defaultServerURLS array');
+  }
+
+  if (
+    config.defaultServerURLs.some(
+      (defaultServerURL) => typeof defaultServerURL !== 'string'
+    )
+  ) {
+    throw new Error('Expected defaultServerURLs to be a list of strings');
+  }
+
+  if (
+    !('routerMode' in config) ||
+    typeof config.routerMode !== 'string' ||
+    !['hash', 'history'].includes(config.routerMode)
+  ) {
+    throw new Error('Expected router mode to be either hash or history');
+  }
+}
 
 /**
  * Fetch configuration at runtime from the config.json file
@@ -14,9 +47,11 @@ let externalConfig: ExternalJSONConfig | undefined;
  */
 export async function getJSONConfig(): Promise<ExternalJSONConfig> {
   if (isNil(externalConfig)) {
-    externalConfig = destr(
-      JSON.stringify(await (await fetch('/config.json')).json())
-    ) as ExternalJSONConfig;
+    const loadedConfig = await (await fetch('/config.json')).json();
+
+    validateJsonConfig(loadedConfig);
+
+    externalConfig = loadedConfig;
   }
 
   return externalConfig;
