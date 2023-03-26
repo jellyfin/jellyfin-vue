ff778ceeedbf5db41d7f45c199e9e23c6bbdb17c
aweebs <8809837+aweebs@users.noreply.github.com>
2023-03-26 12:24:22 -0700

    chore: support arbitrary data- attributes on vue components

--- frontend/types/global/attributes.d.ts
+++ frontend/types/global/attributes.d.ts
@@ -3,7 +3,12 @@ import '@vue/runtime-dom';
 
 declare module '@vue/runtime-dom' {
   export interface HTMLAttributes {
-    // allow any data-* attr
+    // allow any data-* attr on HTML elements
+    [key: `data${string}`]: string;
+  }
+
+  export interface ComponentCustomProps {
+    // allow any data-* attr on Vue components
     [key: `data${string}`]: string;
   }
 }
