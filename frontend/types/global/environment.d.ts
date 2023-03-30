6c98080381616c77fbc2ea9522eaa7ffc5751d81
Fernando Fernández <ferferga@hotmail.com>
2023-03-30 22:17:25 +0200

    chore: remove unnecessary Options API type declarations

--- frontend/types/global/environment.d.ts
+++ frontend/types/global/environment.d.ts
@@ -2,12 +2,5 @@
 /// <reference types="vite-plugin-pages/client" />
 /// <reference types="vite-plugin-vue-layouts/client" />
 
-declare module '*.vue' {
-  import { DefineComponent } from 'vue';
-
-  const component: DefineComponent<never, never, never>;
-  export default component;
-}
-
 declare const __COMMIT_HASH__: string;
 declare const __HISTORY_ROUTER_MODE__: boolean;
