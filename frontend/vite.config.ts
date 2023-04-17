b23bad68982ffa0ea54da55bd8f2aa0fec5be165
endrl <119058008+endrl@users.noreply.github.com>
2023-04-18 01:05:37 +0200

    fix: copy paste

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -175,7 +175,7 @@ export default defineConfig(({ mode }): UserConfig => {
               type: 'image/png'
             },
             {
-              src: 'android-chrome-192x192.png',
+              src: 'android-chrome-512x512.png',
               sizes: '512x512',
               type: 'image/png'
             },
