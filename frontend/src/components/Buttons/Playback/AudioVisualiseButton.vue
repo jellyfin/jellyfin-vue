be39e3156c54b0d1647a38f2d799118a7434ad83
Harvey Lelliott <42912136+flip-dots@users.noreply.github.com>
2023-05-16 18:25:56 +0000

    feat: add music visualiser button

---
+++ frontend/src/components/Buttons/Playback/AudioVisualiseButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn
+    icon
+    :size="size"
+    :color="playerElement.isVisualising ? 'primary' : undefined"
+    @click="playerElement.toggleVisualiser">
+    <v-icon :size="size">
+      <i-mdi-chart-bar />
+    </v-icon>
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import { playerElementStore } from '@/store';
+
+defineProps<{ size?: string }>();
+
+const playerElement = playerElementStore();
+</script>
