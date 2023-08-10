9bc330787d362777e1aab4460879dceb56b4f1e6
Fernando Fernández <ferferga@hotmail.com>
2023-08-10 10:50:00 +0200

    chore(types): create type definitions for virtual modules

---
+++ frontend/types/modules/virtual.d.ts
@@ -0,0 +1,17 @@
+declare module 'virtual:locales/date-fns' {
+  import * as locales from 'date-fns/esm/locale';
+
+  export = locales;
+}
+
+declare module 'virtual:locales/vuetify' {
+  import * as locales from 'vuetify/locale';
+
+  const typeWithoutRtl: Omit<typeof locales, 'defaultRtl'>;
+
+  export = typeWithoutRtl;
+}
+
+declare module 'virtual:locales/vuetify/rtl' {
+  export { defaultRtl } from 'vuetify/locale';
+}
