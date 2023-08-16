dreroc/Sluthub-vue

refactor: adjust how media info looked
- Also generalize the modal dialog for both media info and identify.
- Removed copy code for mediainfo
- Use single content component for all media stream type

Right now, it's using normal Yes/No for boolean with i18n translation key since it would be easier to copy the text.

frontend/src/components/Dialogs/GenericDialog.vue
@@ -1,0 +1,62 @@
+<template>
+  <v-dialog
+    :model-value="model"
+    :fullscreen="$vuetify.display.mobile"
+    :height="$vuetify.display.mobile ? undefined : 'auto'"
+    :width="$vuetify.display.mobile ? undefined : '70vw'"
+    @after-leave="emit('close')">
+    <v-card :loading="isLoading" height="100%" class="d-flex flex-column">
+      <slot name="loader" />
+      <v-toolbar color="transparent">
+        <template v-if="slots.toolbarPrepend" #prepend>
+          <slot name="toolbarPrepend" />
+        </template>
+        <template #append>
+          <v-btn icon @click="model = false">
+            <i-mdi-close />
+          </v-btn>
+        </template>
+        <v-toolbar-title>
+          {{ title }}
+        </v-toolbar-title>
+      </v-toolbar>
+
+      <v-card-subtitle v-if="subtitle" class="pb-3">
+        {{ subtitle }}
+      </v-card-subtitle>
+
+      <v-divider />
+
+      <slot />
+
+      <v-divider v-if="slots.actions" />
+      <v-card-actions
+        v-if="slots.actions"
+        class="d-flex align-center pa-3"
+        :class="{
+          'justify-end': !$vuetify.display.mobile,
+          'justify-center': $vuetify.display.mobile
+        }">
+        <slot name="actions" />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref, useSlots } from 'vue';
+
+const props = defineProps<{
+  modelValue: boolean;
+  title: string;
+  subtitle?: string;
+  isLoading?: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: 'close'): void;
+}>();
+
+const slots = useSlots();
+const model = ref(props.modelValue);
+</script>

jellyfin/jellyfin-vue

frontend/src/components/Dialogs/GenericDialog.vue
@@ -1,0 +1,62 @@
+<template>
+  <v-dialog
+    :model-value="model"
+    :fullscreen="$vuetify.display.mobile"
+    :height="$vuetify.display.mobile ? undefined : 'auto'"
+    :width="$vuetify.display.mobile ? undefined : '70vw'"
+    @after-leave="emit('close')">
+    <v-card :loading="loading" height="100%" class="d-flex flex-column">
+      <slot name="loader" />
+      <v-toolbar color="transparent">
+        <template v-if="slots.toolbarPrepend" #prepend>
+          <slot name="toolbarPrepend" />
+        </template>
+        <template #append>
+          <v-btn icon @click="model = false">
+            <i-mdi-close />
+          </v-btn>
+        </template>
+        <v-toolbar-title>
+          {{ title }}
+        </v-toolbar-title>
+      </v-toolbar>
+
+      <v-card-subtitle v-if="subtitle" class="pb-3">
+        {{ subtitle }}
+      </v-card-subtitle>
+
+      <v-divider />
+
+      <slot />
+
+      <v-divider v-if="slots.actions" />
+      <v-card-actions
+        v-if="slots.actions"
+        class="d-flex align-center pa-3"
+        :class="{
+          'justify-end': !$vuetify.display.mobile,
+          'justify-center': $vuetify.display.mobile
+        }">
+        <slot name="actions" />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref, useSlots } from 'vue';
+
+const props = defineProps<{
+  modelValue: boolean;
+  title: string;
+  subtitle?: string;
+  loading?: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: 'close'): void;
+}>();
+
+const slots = useSlots();
+const model = ref(props.modelValue);
+</script>