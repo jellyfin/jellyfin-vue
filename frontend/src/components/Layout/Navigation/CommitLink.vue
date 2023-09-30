9c7d8c8ab4255c383a03f91d8439d03f865a3ffe
Fernando Fernández <ferferga@hotmail.com>
2023-09-30 19:56:46 +0000

    fix: versioning in settings pages

--- frontend/src/components/Layout/Navigation/CommitLink.vue
+++ frontend/src/components/Layout/Navigation/CommitLink.vue
@@ -3,18 +3,14 @@
     v-if="commit"
     :title="'#' + commit.slice(0, 7)"
     :prepend-icon="IMdiGithub"
-    :href="url"
+    :href="sanitizeHtml(`https://github.com/jellyfin/jellyfin-vue/commit/${commit}`)"
     target="_blank"
     rel="noopener noreferrer" />
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
 import IMdiGithub from 'virtual:icons/mdi/github';
+import { sanitizeHtml } from '@/utils/html';
 
 const commit = __COMMIT_HASH__;
-
-const url = computed(() => {
-  return `https://github.com/jellyfin/jellyfin-vue/commit/${commit}`;
-});
 </script>
