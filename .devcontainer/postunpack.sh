0809739fa7ad2599c28622eb8ce803d847d385c7
Fernando Fernández <ferferga@hotmail.com>
2023-10-26 17:09:29 +0000

    ci(devcontainer): improve boot time
    Run postunpack.sh in postCreateCommand hook,
    which can continue running in the background and in parallel
    to `npm ci --no-audit` when VSCode
    is already connected to the devcontainer.
    
    The time it takes for the container to be fully ready it's the same
    but the time to start coding will be much lower.

--- .devcontainer/postunpack.sh
+++ .devcontainer/postunpack.sh
@@ -3,7 +3,7 @@ unset YARN_VERSION && rm -rf /opt/yarn*
 rm -rf /bin/sh && ln -s /bin/bash /bin/sh
 git config --global core.editor 'code --wait'
 
-## Install dependencies
-apt update -qq
-apt install -y --no-install-recommends jq $(cat packaging/tauri/apt_packages)
+## Install Tauri dependencies
+apt install -y --no-install-recommends $(cat packaging/tauri/apt_packages)
 rm -rf /var/lib/apt/lists /var/cache/apt/archives
+
