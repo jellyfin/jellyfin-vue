286ebb14b26e93999fbb79e785eb376ea70dc241
Fernando Fernández <ferferga@hotmail.com>
2023-11-09 16:05:37 +0100

    refactor: remove vite-plugin-vue-layouts
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- frontend/types/global/environment.d.ts
+++ frontend/types/global/environment.d.ts
@@ -1,5 +1,4 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client" />
-/// <reference types="vite-plugin-vue-layouts/client" />
 
 declare const __COMMIT_HASH__: string | undefined;
