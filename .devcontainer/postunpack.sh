b7f2714d97a34290499239e330bea57acc9080bc
Fernando Fernández <ferferga@hotmail.com>
2023-12-13 00:51:45 +0000

    perf(devcontainer): simplify initialization
    Signed-off-by: GitHub <noreply@github.com>

--- .devcontainer/postunpack.sh
+++ .devcontainer/postunpack.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-unset YARN_VERSION && rm -rf /opt/yarn*
-rm -rf /bin/sh && ln -s /bin/bash /bin/sh
-git config --global core.editor 'code --wait'
 
 ## Install Tauri dependencies
+apt update
 apt install -y --no-install-recommends $(cat packaging/tauri/apt_packages)
 rm -rf /var/lib/apt/lists /var/cache/apt/archives
-
