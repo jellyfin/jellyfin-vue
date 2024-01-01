6ee285e48f07225077f4c61c40c0ac8ec38b4808
Fernando Fernández <ferferga@hotmail.com>
2024-01-01 01:26:56 +0100

    fix: windows builds
    Workaround sourced from https://github.com/vitejs/vite/issues/15153
    
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- frontend/scripts/paths.ts
+++ frontend/scripts/paths.ts
@@ -2,3 +2,8 @@ import { resolve } from 'node:path';
 
 export const localeFilesFolder = resolve('locales/**');
 export const srcRoot = `${resolve('src')}/`;
+export const entrypoints = {
+  index: `${resolve('index.html')}`,
+  main: `${srcRoot}/main.ts`,
+  splashscreen: `${srcRoot}/splashscreen.ts`
+};
