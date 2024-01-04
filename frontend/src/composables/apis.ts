8a5cad083743dbc91c3c28dbed44c2acead8e9fd
Fernando Fernández <ferferga@hotmail.com>
2024-01-04 10:06:01 +0100

    feat: finish api composables and create apistore
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- frontend/src/composables/apis.ts
+++ frontend/src/composables/apis.ts
@@ -1,25 +1,35 @@
-
 import { remote } from '@/plugins/remote';
-import { items } from '@/store/items';
+import { apiStore } from '@/store/api';
 import type { Api } from '@jellyfin/sdk';
-import { ItemFields, type BaseItemDto } from '@jellyfin/sdk/lib/generated-client';
+import type { BaseItemDto, BaseItemDtoQueryResult } from '@jellyfin/sdk/lib/generated-client';
 import type { AxiosResponse } from 'axios';
-import { computed, getCurrentScope, ref, toValue, unref, watch, type ComputedRef, type MaybeRef, type Ref } from 'vue';
+import { computed, getCurrentScope, isRef, ref, toValue, unref, watch, type ComputedRef, type MaybeRef, type Ref } from 'vue';
 
-const allFields = Object.freeze(Object.values(ItemFields));
-
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any, @typescript-eslint/ban-ts-comment, @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return */
 type ParametersAsGetters<T extends (...args: any) => any> = T extends (...args: infer P) => any ? { [K in keyof P]: () => P[K] } : never;
-
 type ExtractResponseDataType<T> = Awaited<T> extends AxiosResponse<infer U, any> ? U : undefined;
 /**
  * If response.data is BaseItemDto or BaseItemDto[], returns it. Otherwise, returns undefined.
  */
-type ExtractBaseItemDtoResponse<T> = (ExtractResponseDataType<T> extends BaseItemDto ? ExtractResponseDataType<T> : (ExtractResponseDataType<T> extends BaseItemDto[] ? ExtractResponseDataType<T> : undefined));
+type ExtractBaseItemDtoResponse<T> =
+  (ExtractResponseDataType<T> extends BaseItemDto ? BaseItemDto :
+  (ExtractResponseDataType<T> extends BaseItemDtoQueryResult ? BaseItemDto[] :
+  ExtractResponseDataType<T> extends BaseItemDto[] ? BaseItemDto[] : undefined));
 /**
  * If response.data is BaseItemDto or BaseItemDto[], returns undefined. Otherwise, returns the data type.
  */
-type ExtractResponseType<T> = (ExtractResponseDataType<T> extends BaseItemDto ? undefined : (ExtractResponseDataType<T> extends BaseItemDto[] ? undefined : ExtractResponseDataType<T>));
+type ExtractResponseType<T> =
+  (ExtractResponseDataType<T> extends BaseItemDto ? undefined :
+  (ExtractResponseDataType<T> extends BaseItemDtoQueryResult ? undefined :
+  ExtractResponseDataType<T>));
+
+type ReturnData<T extends Record<K, (...args: any[]) => any>, K extends keyof T, J extends boolean> =
+  J extends true ? ExtractBaseItemDtoResponse<ReturnType<T[K]>> : ExtractResponseType<ReturnType<T[K]>>;
+
+interface ReturnPayload<T extends Record<K, (...args: any[]) => any>, K extends keyof T, J extends boolean> {
+  loading: Ref<boolean>,
+  data: ComputedRef<ReturnData<T, K, J>>;
+}
 
 /**
  * Ensures the function is used in the given
@@ -36,16 +46,14 @@ function ensureCorrectUsage(func: any): void {
  * Perfoms the given request
  * @param api - Relevant API
  * @param methodName - Method to execute
- * @param isBaseItem - Whether the request is BaseItemDto based or not
- * @param requestData - Ref to hold the request data
+ * @param ofBaseItem - Whether the request is BaseItemDto based or not
  * @param loading - Ref to hold the loading state
  * @param args - Func args
  */
 async function resolveAndAdd<T extends Record<K, (...args: any[]) => any>, K extends keyof T>(
   api: (api: Api) => T,
   methodName: K,
-  isBaseItem: boolean,
-  requestData: Ref<Awaited<ReturnType<T[K]>['data']> | undefined>,
+  ofBaseItem: boolean,
   loading: Ref<boolean>,
   ...args: ParametersAsGetters<T[K]>): Promise<void> {
   /**
@@ -53,23 +61,32 @@ async function resolveAndAdd<T extends Record<K, (...args: any[]) => any>, K ext
    * toValue normalizes the getters.
    */
   const extendedParams = [
-    { ...toValue(args[0]), fields: allFields },
+    {
+      ...toValue(args[0]),
+      fields: apiStore.apiEnums.fields,
+      enabledImageTypes: apiStore.apiEnums.images,
+      enableImages: true,
+      enableTotalRecordCount: false
+    },
     ...args.slice(1).map((a) => toValue(a))
   ] as Parameters<T[K]>;
 
   try {
     loading.value = true;
 
+    const stringifiedArgs = JSON.stringify(args.map((a) => toValue(a)));
+    const funcName = `${api.name}.${String(methodName)}`;
     const response = await remote.sdk.newUserApi(api)[methodName](...extendedParams) as Awaited<ReturnType<T[K]>>;
 
     if (response.data) {
-      requestData.value = response.data;
+      const requestData = response.data as Awaited<ReturnType<T[K]>['data']>;
+      const result = 'Items' in requestData && Array.isArray(requestData.Items) ? requestData.Items : requestData;
 
-      if (isBaseItem) {
-        items.rawAdd(response.data as BaseItemDto | BaseItemDto[]);
+      if (ofBaseItem) {
+        apiStore.baseItemAdd(result as BaseItemDto | BaseItemDto[]);
       }
-    } else {
-      requestData.value = undefined;
+
+      apiStore.requestAdd(funcName, stringifiedArgs, ofBaseItem, result);
     }
   } catch {} finally {
     loading.value = false;
@@ -77,17 +94,78 @@ async function resolveAndAdd<T extends Record<K, (...args: any[]) => any>, K ext
 }
 
 /**
+ * This is the internal logic of the composables
+ */
+function _sharedInternalLogic<T extends Record<K, (...args: any[]) => any>, K extends keyof T>(
+  ofBaseItem: boolean,
+  api: MaybeRef<(api: Api) => T>,
+  methodName: MaybeRef<K>
+): (this: any, ...args: ParametersAsGetters<T[K]>) => Promise<ReturnPayload<T, K, typeof ofBaseItem>> {
+  ensureCorrectUsage(remote.sdk.newUserApi(unref(api))[unref(methodName)]);
+
+  const loading = ref(true);
+  const argsRef = ref<Parameters<T[K]>>();
+  const stringArgs = computed(() => {
+    return JSON.stringify(argsRef.value);
+  });
+
+  const data = computed<ReturnData<T, K, typeof ofBaseItem>>(() =>
+    apiStore.getRequest(`${unref(api).name}.${String(unref(methodName))}`, stringArgs.value) as ReturnData<T, K, typeof ofBaseItem>
+  );
+
+  return async function (this: any, ...args: ParametersAsGetters<T[K]>) {
+    const setArgs = (): void => {
+      argsRef.value = args.map((a) => toValue(a)) as Parameters<T[K]>;
+    };
+
+    setArgs();
+
+    const run = async (args: ParametersAsGetters<T[K]>): Promise<void> => {
+      try {
+        await resolveAndAdd(unref(api), unref(methodName), ofBaseItem, loading, ...args);
+      } catch {}
+    };
+    const isCached = Boolean(data.value);
+
+    if (getCurrentScope() !== undefined) {
+      watch(args, async () => {
+        setArgs();
+        await run(args);
+      });
+      isRef(api) && watch(api, async () => await run(args));
+      isRef(methodName) && watch(methodName, async () => await run(args));
+
+      /**
+       * If there's available data before component mount, we return the cached data rightaway (see below how
+       * we skip the promise) to get the component mounted as soon as possible.
+       * However, we queue a request to the server to update the data after the component is
+       * mounted
+       */
+      isCached && window.setTimeout(async () => {
+        await run(args);
+      });
+    }
+
+    !isCached && await run(args);
+
+    return { loading, data };
+  };
+}
+
+/**
  * Reactively performs item requests to the API:
  *
  * - When the parameters of the request changes, the request is performed again and the ComputedRef returns updated data
- * - If there's already cached data in the offline store for the given parameters, a request to the
- * server it's still performed, but the ComputedRef will return the cached data first and then the server data.
+ * - Caches the request response in the apiStore
+ * - If there's already cached data in the store for the given parameters, a request to the
+ * server it's still performed to refresh, but the Promise will be resolved
+ * immediately and the ComputedRef will return the cached data first.
  *
  * This composable also returns a promise, so it prevents rendering the component with Suspense until the initial request is done
  * (ensuring this way the data is always available before mount).
  * See [Vue docs](https://vuejs.org/guide/built-ins/suspense.html#suspense) for more information.
  *
- * Here's an usage example:
+ * Here's an usage example. **Request parameters must be passed as getters (like props on watchers)**:
  *
  * ```ts
  * const { loading, data: item } = await useBaseItem(getUserLibraryApi, 'getItem')(() => {
@@ -96,11 +174,12 @@ async function resolveAndAdd<T extends Record<K, (...args: any[]) => any>, K ext
  * ```
  *
  * Caveats:
- * - If not used inside a component's <script setup> area (orn any Vue's effctScope), changing parameters will not be reactive.
+ * - If not used inside a component's <script setup> area (or in any Vue's effectScope), changing parameters will not be reactive.
  * This is done in order to avoid memory leaks.
- * - **Request parameters must be passed as getters (like props on watchers)**.
- * - It only works with requests that return BaseItemDto or BaseItemDto[] responses. If you need to use another type, you **must** use the `useApi` composable.
- * TypeScript will tell you that `data` is always undefined when you can't use the composable with an specific API method.
+ * - It only works with requests that return BaseItemDto or BaseItemDtoQueryResult responses. If you need to use another type, you **must**
+ * use the `useApi` composable.
+ *
+ * Don't worry, TypeScript will tell you that `data` is always undefined when you can't use the composable with an specific API method.
  *
  * @param api - The API's endpoint to use.
  * @param methodname- - The operation to execute.
@@ -108,127 +187,51 @@ async function resolveAndAdd<T extends Record<K, (...args: any[]) => any>, K ext
  * @returns loading - A boolean ref that indicates if the request is in progress.
  */
 export function useBaseItem<T extends Record<K, (...args: any[]) => any>, K extends keyof T>(
-  api: (api: Api) => T,
-  methodName: K
-): (this: any, ...args: ParametersAsGetters<T[K]>) => Promise<{ loading: Ref<boolean>, data: ComputedRef<ExtractBaseItemDtoResponse<ReturnType<T[K]>>> }> {
-  ensureCorrectUsage(remote.sdk.newUserApi(unref(api))[unref(methodName)]);
-
-  /**
-   * For some reason, the watcher also fires on startup, so we need to keep track of that to avoid double requests.
-   */
-  let initialFetchDone = false;
-  const loading = ref(true);
-  const requestData = ref<Awaited<ReturnType<T[K]>['data']>>();
-  const calledFunctions = computed(() => `${unref(api).name}.${unref(methodName).toString()}`);
-
-  /**
-   * Returns a proxy ref from the store
-   */
-  // @ts-expect-error - Typings get too complex at this point
-  const data = computed<ExtractBaseItemDtoResponse<ReturnType<T[K]>>>(() => {
-    if (typeof requestData.value === 'object') {
-      // @ts-expect-error - We check both capitalizations just in case
-      const itemArray: BaseItemDto[] | undefined = requestData.value.items ?? requestData.value.Items;
-
-      if (Array.isArray(itemArray)) {
-        const ids = itemArray.map((i) => i.Id).filter((id): id is string => typeof id === 'string');
-
-        return items.getItemsById(ids).filter((item): item is BaseItemDto => typeof item === 'object');
-      } else {
-        return items.getItemById((requestData.value as BaseItemDto).Id);
-      }
-    }
-  });
-
-  return async function (this: any, ...args: ParametersAsGetters<T[K]>) {
-    const run = async (args: ParametersAsGetters<T[K]>): Promise<void> => {
-      try {
-        await resolveAndAdd(unref(api), unref(methodName), true, requestData, loading, ...args);
-        initialFetchDone = true;
-      } catch {}
-    };
-
-    if (getCurrentScope() !== undefined) {
-      const cbk = async (): Promise<void> => {
-        if (initialFetchDone) {
-          await run(args);
-        }
-      };
-
-      watch(args, cbk);
-      watch(calledFunctions, cbk);
-    }
-
-    await run(args);
-
-    return { loading, data };
-  };
+  api: MaybeRef<(api: Api) => T>,
+  methodName: MaybeRef<K>
+): (this: any, ...args: ParametersAsGetters<T[K]>) => Promise<ReturnPayload<T, K, true>> {
+  return _sharedInternalLogic<T, K>(true, api, methodName);
 }
 
 /**
- * Initial JSDoc
+ * Reactively performs requests to the API:
  *
- * @param api
- * @param methodName
- * @returns
+ * - When the parameters of the request changes, the request is performed again and the ComputedRef returns updated data.
+ * - Caches the request response in the apiStore
+ * - If there's already cached data in the store for the given parameters, a request to the
+ * server it's still performed to refresh, but the Promise will be resolved
+ * immediately and the ComputedRef will return the cached data first.
+ *
+ * This composable also returns a promise, so it prevents rendering the component with Suspense until the initial request is done
+ * (ensuring this way the data is always available before mount).
+ * See [Vue docs](https://vuejs.org/guide/built-ins/suspense.html#suspense) for more information.
+ *
+ * Here's an usage example. **Request parameters must be passed as getters (like props on watchers)**.:
+ *
+ * ```ts
+ * const { loading, data: item } = await useApi(getItemUpdateApi, 'updateItemContentType')(() => {
+ *   ...the request parameters
+ * });
+ * ```
+ *
+ * Caveats:
+ * - If not used inside a component's <script setup> area (or in any Vue's effectScope), changing parameters will not be reactive.
+ * This is done in order to avoid memory leaks.
+ * - It only works with requests that doesn't return BaseItemDto or BaseItemDtoQueryResult responses. If the return result
+ * of your request is any of those types, you **must** use the `useBaseItem` composable.
+ *
+ * Don't worry, TypeScript will tell you that `data` is always undefined when you can't use the composable with an specific API method.
+ *
+ * @param api - The API's endpoint to use.
+ * @param methodname- - The operation to execute.
+ * @returns data  - The request data.
+ * @returns loading - A boolean ref that indicates if the request is in progress.
  */
 export function useApi<T extends Record<K, (...args: any[]) => any>, K extends keyof T>(
   api: MaybeRef<(api: Api) => T>,
   methodName: MaybeRef<K>
-): (this: any, ...args: ParametersAsGetters<T[K]>) => Promise<{ loading: Ref<boolean>, data: ComputedRef<ExtractResponseType<ReturnType<T[K]>>> }> {
-  ensureCorrectUsage(remote.sdk.newUserApi(unref(api))[unref(methodName)]);
-
-  /**
-   * For some reason, the watcher also fires on startup, so we need to keep track of that to avoid double requests.
-   */
-  let initialFetchDone = false;
-  const loading = ref(true);
-  const requestData = ref<Awaited<ReturnType<T[K]>['data']>>();
-  const calledFunctions = computed(() => `${unref(api).name}.${unref(methodName).toString()}`);
-
-  /**
-   * Returns a proxy ref from the store
-   */
-  // @ts-expect-error - Typings get too complex at this point
-  const data = computed<ExtractResponseType<ReturnType<T[K]>>>(() => {
-    if (typeof requestData.value === 'object') {
-      // @ts-expect-error - We check both capitalizations just in case
-      const itemArray: BaseItemDto[] | undefined = requestData.value.items ?? requestData.value.Items;
-
-      if (Array.isArray(itemArray)) {
-        const ids = itemArray.map((i) => i.Id).filter((id): id is string => typeof id === 'string');
-
-        return items.getItemsById(ids).filter((item): item is BaseItemDto => typeof item === 'object');
-      } else {
-        return items.getItemById((requestData.value as BaseItemDto).Id);
-      }
-    }
-  });
-
-  return async function (this: any, ...args: ParametersAsGetters<T[K]>) {
-    const run = async (args: ParametersAsGetters<T[K]>): Promise<void> => {
-      try {
-        await resolveAndAdd(unref(api), unref(methodName), false, requestData, loading, ...args);
-        initialFetchDone = true;
-      } catch {}
-    };
-
-    if (getCurrentScope() !== undefined) {
-      // eslint-disable-next-line sonarjs/no-identical-functions
-      const cbk = async (): Promise<void> => {
-        if (initialFetchDone) {
-          await run(args);
-        }
-      };
-
-      watch(args, cbk);
-      watch(calledFunctions, cbk);
-    }
-
-    await run(args);
-
-    return { loading, data };
-  };
+): (this: any, ...args: ParametersAsGetters<T[K]>) => Promise<ReturnPayload<T, K, false>> {
+  return _sharedInternalLogic<T, K>(false, api, methodName);
 }
 
-/* eslint-enable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any, @typescript-eslint/ban-ts-comment, @typescript-eslint/no-unsafe-return */
+/* eslint-enable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return */
