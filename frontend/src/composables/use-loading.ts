27044a1f082852d416cb02405fe962a0f107b7bd
Fernando Fernández <ferferga@hotmail.com>
2024-01-09 22:00:40 +0100

    feat: show wait cursor while loading
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- frontend/src/composables/use-loading.ts
+++ frontend/src/composables/use-loading.ts
@@ -1,7 +1,14 @@
-import { computed, ref, type ComputedRef } from 'vue';
+import { computed, ref, watch, type ComputedRef } from 'vue';
 
 const requests = ref(0);
 const isLoading = computed(() => requests.value > 0);
+const cssVarKey = '--j-client-cursor';
+
+watch(isLoading, () => {
+  isLoading.value ?
+    window.document.documentElement.style.setProperty(cssVarKey, 'wait') :
+    window.document.documentElement.style.removeProperty(cssVarKey);
+});
 
 /**
  * Composable for triggering the linear progress that appears at the top of the page
