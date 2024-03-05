02e7040119a27d7f4379c6566a40226bec13c777
Fernando Fernández <ferferga@hotmail.com>
2024-03-05 12:46:29 +0000

    fix(packaging): rootless Docker not working in some systems
    Signed-off-by: GitHub <noreply@github.com>

--- packaging/docker/contents/postunpack.sh
+++ packaging/docker/contents/postunpack.sh
@@ -6,14 +6,6 @@ NGINX_CONFIG_FILE=/etc/nginx/nginx.conf
 ## setup.sh dependencies
 apk add --no-cache jq
 
-# Trim image
-apk --purge del apk-tools
-rm -rf /docker-entrypoint.d /.dockerenv /usr/sbin/nginx-debug
-rm -rf /usr/share/zoneinfo
-rm -rf /sbin/apk /etc/apk /lib/apk /usr/share/apk /var/lib/apk
-rm -rf /usr/lib/libcrypto* /usr/lib/libintl* /usr/lib/libssl* \
-    /usr/lib/engines-3 /usr/lib/modules-load.d /usr/lib/nginx /usr/lib/ossl-modules
-
 # CONTAINER ROOTLESS SETUP
 ### Set correct permissions and make frontend config.json file editable for the runtime user
 mkdir -p /run/nginx
@@ -21,3 +13,15 @@ chown nginx:nginx -R /run/nginx /var/cache/nginx /usr/share/nginx/html/config.js
 sed -i 's|/var/run|/var/run/nginx|g' $NGINX_CONFIG_FILE
 ## The 'user' config option is useless when running rootless and gives a warning
 sed -i '/^user /d' $NGINX_CONFIG_FILE
+## Allow to open privileged ports
+apk add --no-cache libcap
+setcap CAP_NET_BIND_SERVICE=+eip /usr/sbin/nginx
+apk --purge del libcap
+
+# Trim image
+apk --purge del apk-tools
+rm -rf /docker-entrypoint.d /.dockerenv /usr/sbin/nginx-debug
+rm -rf /usr/share/zoneinfo
+rm -rf /sbin/apk /etc/apk /lib/apk /usr/share/apk /var/lib/apk
+rm -rf /usr/lib/libcrypto* /usr/lib/libintl* /usr/lib/libssl* \
+    /usr/lib/engines-3 /usr/lib/modules-load.d /usr/lib/nginx /usr/lib/ossl-modules
