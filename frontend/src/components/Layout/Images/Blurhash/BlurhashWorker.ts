85e6b733b859437234f6fce65d7fa866f741f9f4
Fernando Fernández <ferferga@hotmail.com>
2024-03-29 08:13:59 +0000

    perf: reduce default blurhash quality
    Signed-off-by: GitHub <noreply@github.com>

--- frontend/src/components/Layout/Images/Blurhash/BlurhashWorker.ts
+++ frontend/src/components/Layout/Images/Blurhash/BlurhashWorker.ts
@@ -2,11 +2,22 @@ import { decode } from 'blurhash';
 import { expose } from 'comlink';
 import { sealed } from '@/utils/validation';
 
+/**
+ * By default, 20x20 pixels with a punch of 1 is returned.
+ * Although the default values recommended by Blurhash developers is 32x32,
+ * a size of 20x20 seems to be the sweet spot for us, improving the performance
+ * and reducing the memory usage, while retaining almost full blur quality.
+ * Lower values had more visible pixelation
+ */
+export const DEFAULT_WIDTH = 20;
+export const DEFAULT_HEIGHT = 20;
+export const DEFAULT_PUNCH = 1;
+
 @sealed
 class BlurhashWorker {
   private readonly _cache = new Map<string, Uint8ClampedArray>();
   /**
-   * Decodes blurhash outside the main thread, in a web worker
+   * Decodes blurhash outside the main thread, in a web worker.
    *
    * @param hash - Hash to decode.
    * @param width - Width of the decoded pixel array
@@ -16,9 +27,9 @@ class BlurhashWorker {
    */
   public readonly getPixels = (
     hash: string,
-    width: number,
-    height: number,
-    punch: number
+    width: number = DEFAULT_WIDTH,
+    height: number = DEFAULT_HEIGHT,
+    punch: number = DEFAULT_PUNCH
   ): Uint8ClampedArray => {
     try {
       const params = [hash, width, height, punch].toString();
