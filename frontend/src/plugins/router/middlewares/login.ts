28ded0d5200e1ee06afe7bf01630f0151fcfabb0
Fernando Fernández <ferferga@hotmail.com>
2024-05-19 14:24:18 +0200

    refactor: use until composable from VueUse to simplify instead of effectScopes
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- frontend/src/plugins/router/middlewares/login.ts
+++ frontend/src/plugins/router/middlewares/login.ts
@@ -3,8 +3,7 @@ import type {
   RouteLocationPathRaw,
   RouteLocationRaw
 } from 'vue-router';
-import { effectScope } from 'vue';
-import { watchImmediate } from '@vueuse/core';
+import { until } from '@vueuse/core';
 import { remote } from '@/plugins/remote';
 import { isNil } from '@/utils/validation';
 import { getJSONConfig } from '@/utils/external-config';
@@ -13,25 +12,6 @@ const serverAddUrl = '/server/add';
 const serverSelectUrl = '/server/select';
 const serverLoginUrl = '/server/login';
 const routes = new Set([serverAddUrl, serverSelectUrl, serverLoginUrl]);
-
-/**
- * Waits for a server connection to be established for cases where there are default servers
- * defined and the server selection screen is disallowed.
- */
-async function ensureServer(): Promise<void> {
-  const scope = effectScope();
-
-  await new Promise<void>((resolve) => {
-    scope.run(() => {
-      watchImmediate(() => remote.auth.currentServer, () => {
-        if (remote.auth.currentServer) {
-          resolve();
-        }
-      });
-    });
-  });
-  scope.stop();
-}
 
 /**
  * Redirects to login page if there's no user logged in.
@@ -53,7 +33,7 @@ export async function loginGuard(
       if (jsonConfig.allowServerSelection) {
         destinationRoute = { path: serverSelectUrl, replace: true };
       } else {
-        await ensureServer();
+        await until(() => remote.auth.currentServer).toBeTruthy();
 
         return loginGuard(to);
       }
