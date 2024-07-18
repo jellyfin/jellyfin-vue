ac424696e8aefe09a707fc594b080bc2480ed5f6
Sean McBroom <sean.mcbroom@outlook.com>
2024-07-18 15:33:19 -0500

    feat(FontSelector): refactor font selector logic and move to component

--- frontend/src/pages/settings/subtitles.vue
+++ frontend/src/pages/settings/subtitles.vue
@@ -1,80 +1,52 @@
 <template>
-    <SettingsPage page-title="subtitles">
-        <template #content>
-            <VCol
-              md="6"
-              class="pt-0 pb-4">
-                <VSelect
-                  v-model="clientSettings.subtitleAppearance.fontFamily"
-                  :label="$t('subtitleFont')"
-                  :items="availableSubtitleFonts"/>
+  <SettingsPage>
+    <template #title>
+      {{ t('subtitles') }}
+    </template>
 
-                <VSlider
-                  v-model="clientSettings.subtitleAppearance.fontSize"
-                  :label="$t('fontSize')"
-                  :min="1"
-                  :max="4.5"
-                  :step="0.1"/>
+    <template #content>
+      <VCol
+        md="6"
+        class="pt-0 pb-4">
+        <FontSelector
+          v-model="clientSettings.subtitleAppearance.fontFamily"
+          :label="$t('subtitleFont')" />
 
-                <VSlider
-                  v-model="clientSettings.subtitleAppearance.positionFromBottom"
-                  :label="$t('positionFromBottom')"
-                  :min="0"
-                  :max="30"
-                  :step="1"/>
+        <VSlider
+          v-model="clientSettings.subtitleAppearance.fontSize"
+          :label="$t('fontSize')"
+          :min="1"
+          :max="4.5"
+          :step="0.1" />
 
-                <VCheckbox
-                  v-model="clientSettings.subtitleAppearance.backdrop"
-                  :label="$t('backdrop')"/>
+        <VSlider
+          v-model="clientSettings.subtitleAppearance.positionFromBottom"
+          :label="$t('positionFromBottom')"
+          :min="0"
+          :max="30"
+          :step="1" />
 
-                <VCheckbox
-                  v-model="clientSettings.subtitleAppearance.stroke"
-                  :label="$t('stroke')"/>
+        <VCheckbox
+          v-model="clientSettings.subtitleAppearance.backdrop"
+          :label="$t('backdrop')" />
 
-                <SubtitleTrack :previewText="$t('subtitlePreviewText')"/>
-            </VCol>
-        </template>
-      </SettingsPage>
+        <VCheckbox
+          v-model="clientSettings.subtitleAppearance.stroke"
+          :label="$t('stroke')" />
+
+        <SubtitleTrack :preview-text="$t('subtitlePreviewText')" />
+      </VCol>
+    </template>
+  </SettingsPage>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRoute } from 'vue-router/auto';
 import { clientSettings } from '@/store/client-settings';
-import { SUBTITLE_FONT_FAMILIES } from '@/utils/subtitles';
 
 const { t } = useI18n();
 const route = useRoute();
 
 route.meta.title = t('subtitles');
-
-const availableSubtitleFonts = ref<string[]>([]);
-
-// Load subtitle fonts available to the client
-const loadAvailableFonts = async () => {
-  const checkFontAvailability = async (font: string) => {
-    // Default font
-    if (font == SUBTITLE_FONT_FAMILIES[0]) {
-      return font;
-    }
-
-    try {
-      const fontFace = new FontFace(font, `local(${font})`);
-      const loaded = await fontFace.load();
-
-      if (loaded.status !== 'error') {
-        return font;
-      }
-    } catch {}
-  };
-
-  const fontChecks = SUBTITLE_FONT_FAMILIES.map(checkFontAvailability);
-  const fonts = await Promise.all(fontChecks);
-  const validFonts = fonts.filter(font => font !== undefined);
-
-  availableSubtitleFonts.value = validFonts;
-};
-
-await loadAvailableFonts();
 </script>
