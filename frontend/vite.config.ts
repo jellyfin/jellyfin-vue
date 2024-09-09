6e9b90c880a250fa83a61365e26868075c96d84c
Fernando Fernández <ferferga@hotmail.com>
2024-09-09 19:45:00 +0200

    perf: code-split the application
    Code-split the application (but without lazy loading, aka dynamic imports aka async modules, so all the assets are loaded on app load, not when requested:
    this is needed for ensuring an smooth experience over slow networks)
    
    Lighthouse performance score went from 66 to 77
    
    There's still a lot of work to do, but this is a good start
    
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,6 +18,7 @@ import { defineConfig } from 'vite';
 import { entrypoints, localeFilesFolder, srcRoot } from './scripts/paths';
 import virtualModules from './scripts/virtual-modules';
 import { JellyfinVueAnalysis } from './scripts/plugins/analysis';
+import { JellyfinVueChunking } from './scripts/plugins/chunking';
 
 export default defineConfig({
   appType: 'spa',
@@ -25,6 +26,7 @@ export default defineConfig({
   cacheDir: '../node_modules/.cache/vite',
   plugins: [
     JellyfinVueAnalysis(),
+    JellyfinVueChunking(),
     Virtual(virtualModules),
     VueRouter({
       dts: './types/global/routes.d.ts',
@@ -64,7 +66,8 @@ export default defineConfig({
       compositionOnly: true,
       fullInstall: false,
       forceStringify: true,
-      include: localeFilesFolder
+      include: localeFilesFolder,
+      dropMessageCompiler: true
     }),
     UnoCSS(),
     VueDevTools()
@@ -77,7 +80,6 @@ export default defineConfig({
     /**
      * Disable chunk size warnings
      */
-    chunkSizeWarningLimit: Number.NaN,
     cssCodeSplit: true,
     cssMinify: 'lightningcss',
     modulePreload: false,
@@ -89,28 +91,7 @@ export default defineConfig({
         index: entrypoints.index
       },
       output: {
-        chunkFileNames: (chunkInfo) => {
-          /**
-           * This is the default value: https://rollupjs.org/configuration-options/#output-chunkfilenames
-           */
-          return chunkInfo.name === 'validation' ? 'assets/common-[hash].js' : '[name]-[hash].js';
-        },
-        validate: true,
-        /**
-         * This is the first thing that should be debugged when there are issues
-         * withe the bundle. Check these issues:
-         * - https://github.com/vitejs/vite/issues/5142
-         * - https://github.com/evanw/esbuild/issues/399
-         * - https://github.com/rollup/rollup/issues/3888
-         */
-        manualChunks(id) {
-          if (
-            id.includes('virtual:locales')
-            || id.includes('@intlify/unplugin-vue-i18n/messages')
-          ) {
-            return 'assets/locales';
-          }
-        }
+        validate: true
       }
     }
   },
