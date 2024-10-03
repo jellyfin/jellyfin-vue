3fbf550a15912e0963b521f523c823a95e629329
Fernando Fernández <ferferga@hotmail.com>
2024-10-03 21:17:17 +0000

    ci(devcontainer): use official debian base image

--- .devcontainer/postunpack.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-## Install Tauri dependencies
-apt update
-apt install -y --no-install-recommends $(cat packaging/tauri/apt_packages)
-rm -rf /var/lib/apt/lists /var/cache/apt/archives
