1f8aed8c0cb5b3237715c6935b71bbab05bf038f
Fernando Fernández <ferferga@hotmail.com>
2024-11-06 23:35:18 +0100

    chore(docker): trim more the image, always run setup (#2491)
    This allows to simplify the custom entrypoints command a little bit

--- packaging/docker/contents/setup.sh
+++ packaging/docker/contents/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CONFIG_FILE_PATH="/usr/share/nginx/html/config.json"
+CONFIG_FILE_PATH="$ASSETS/config.json"
 echo "Writing data to $CONFIG_FILE_PATH..."
 
 if [[ "$HISTORY_ROUTER_MODE" == "0" ]]; then
@@ -15,7 +15,12 @@ else
     ALLOW_SERVER_SELECTION=true
 fi
 
-echo "DEFAULT_SERVERS value: $DEFAULT_SERVERS"
+if [ -n "$DEFAULT_SERVERS" ]; then
+    echo "DEFAULT_SERVERS value: $DEFAULT_SERVERS"
+else
+    echo "No DEFAULT_SERVERS value specified"
+fi
+
 echo "ALLOW_SERVER_SELECTION value: $ALLOW_SERVER_SELECTION"
 echo "ROUTER_MODE value: $ROUTER_MODE"
 
