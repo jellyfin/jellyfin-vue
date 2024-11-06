6ed33cdaa525bdb08ed7d229910b1ec850038e38
Fernando Fernández <ferferga@hotmail.com>
2024-11-06 16:17:48 +0000

    chore(docker): trim more the image, always run setup
    This allows to simplify the custom entrypoints command a little bit

--- packaging/docker/contents/postunpack.sh
+++ packaging/docker/contents/postunpack.sh
@@ -9,8 +9,10 @@ apk add --no-cache jq
 # CONTAINER ROOTLESS SETUP
 ### Set correct permissions and make frontend config.json file editable for the runtime user
 mkdir -p /run/nginx
-chown nginx:nginx -R /run/nginx /var/cache/nginx /usr/share/nginx/html/config.json
+chown nginx:nginx -R /run/nginx /var/cache/nginx "$ASSETS"/config.json
 sed -i 's|/var/run|/var/run/nginx|g' $NGINX_CONFIG_FILE
+## Replaces the 'root' directive with the value of $ASSETS
+sed -i "s|\${ASSETS}|${ASSETS}|g" "$NGINX_DEFAULT_SITE_CONFIG"
 ## The 'user' config option is useless when running rootless and gives a warning
 sed -i '/^user /d' $NGINX_CONFIG_FILE
 ## Allow to open privileged ports
@@ -21,7 +23,10 @@ apk --purge del libcap
 # Trim image
 apk --purge del apk-tools
 rm -rf /docker-entrypoint.d /.dockerenv /usr/sbin/nginx-debug
-rm -rf /usr/share/zoneinfo
-rm -rf /sbin/apk /etc/apk /lib/apk /usr/share/apk /var/lib/apk
-rm -rf /usr/lib/libcrypto* /usr/lib/libintl* /usr/lib/libssl* \
-    /usr/lib/engines-3 /usr/lib/modules-load.d /usr/lib/nginx /usr/lib/ossl-modules
+rm -rf /usr/share/zoneinfo /usr/share/man
+rm -rf /var/cache/apk
+rm -rf /etc/nginx/fast* /etc/nginx/*_params /etc/nginx/modules
+rm -rf /sbin/apk /etc/apk /lib/apk /usr/share/apk /var/lib/apk /lib/libapk*
+rm -rf /usr/lib/libintl* /lib/libintl*
+rm -rf /usr/lib/engines-3 /usr/lib/modules-load.d /usr/lib/nginx /usr/lib/ossl-modules
+rm -rf /etc/ssl
