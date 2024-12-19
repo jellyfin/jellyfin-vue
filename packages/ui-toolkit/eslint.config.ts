0be0f3063244cf1c4b92e233219d51a7d19ac476
Fernando Fernández <ferferga@hotmail.com>
2024-12-19 19:50:57 +0100

    chore: extend monorepo structure, better docs
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- packages/ui-toolkit/eslint.config.ts
+++ packages/ui-toolkit/eslint.config.ts
@@ -1,9 +1,8 @@
 import type { Linter } from 'eslint';
-import { getBaseConfig, getTSVueConfig, getNodeFiles, tsFiles } from './src/lint';
+import { getBaseConfig, getTSVueConfig } from '@jellyfin-vue/configs/lint';
 import pkg from './package.json' with { type: 'json' };
 
 export default [
   ...getBaseConfig(pkg.name),
-  ...getTSVueConfig(false, import.meta.dirname),
-  ...getNodeFiles(tsFiles)
+  ...getTSVueConfig(true, import.meta.dirname)
 ] satisfies Linter.Config[];
