33fa3d7f827ace9e139cf760ca0ec917a9cfb4f5
Fernando Fernández <ferferga@hotmail.com>
2025-01-04 12:03:39 +0100

    refactor(App): attribute removal
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,16 +15,12 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-
 /**
  * When app is mounted, the classes and styles we initialized in the pre-Vue splashscreen in body
  * are now useless and can break the page if not removed.
  *
  * We set all the styles and vars in JApp, so we want a body with 0 styling attributes.
  */
-onMounted(() => {
-  document.body.removeAttribute('class');
-  document.body.removeAttribute('style');
-});
+document.body.removeAttribute('class');
+document.body.removeAttribute('style');
 </script>
