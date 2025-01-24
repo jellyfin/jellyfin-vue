242bf6fb35216318f8f50339c3efb4a62d106214
Fernando Fernández <ferferga@hotmail.com>
2025-01-24 23:46:42 +0100

    chore: add storybook
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

---
+++ frontend/storybook/main.ts
@@ -0,0 +1,1 @@
+export { default } from '@jellyfin-vue/configs/storybook';
---
+++ packages/ui-toolkit/storybook/stories/JProgressCircular.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import { JProgressCircular } from '#/components';
+
+const meta: Meta<typeof JProgressCircular> = {
+  component: JProgressCircular
+};
+
+export default meta;
+
+type Story = StoryObj<typeof JProgressCircular>;
+
+export const Default: Story = {
+  args: {
+    indeterminate: false
+  }
+};
+
+export const Indeterminate: Story = {
+  args: {
+    indeterminate: true
+  }
+};
