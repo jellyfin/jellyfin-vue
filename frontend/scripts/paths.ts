769c2a3cef3f946daad9814179bc530930f8294b
Fernando Fernández <ferferga@hotmail.com>
2025-04-28 12:31:26 +0200

    refactor: use getPackagePath helper
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- frontend/scripts/paths.ts
+++ frontend/scripts/paths.ts
@@ -1,3 +1,4 @@
 import { resolve } from 'node:path';
+import { getPackagePath } from '@jellyfin-vue/shared/utils';
 
-export const localeFilesFolder = resolve(import.meta.dirname, '../locales/**');
+export const localeFilesFolder = resolve(getPackagePath('@jellyfin-vue/i18n'), 'strings/**');
