2fd30889b25d1b6bc5116e97239cd4c1ecc6d473
Fernando Fernández <ferferga@hotmail.com>
2025-04-28 08:58:31 +0000

    refactor(packaging): switch base nginx image to nginx-unprivileged

--- packaging/docker/contents/postunpack.sh
+++ packaging/docker/contents/postunpack.sh
@@ -3,22 +3,15 @@ echo "Configuring environment for nginx..."
 
 NGINX_CONFIG_FILE=/etc/nginx/nginx.conf
 
-## setup.sh dependencies
+# setup.sh dependencies
 apk add --no-cache jq
+
+# Replaces the 'root' directive with the value of $ASSETS
+sed -i "s|\${ASSETS}|${ASSETS}|g" "$NGINX_DEFAULT_SITE_CONFIG"
 
 # CONTAINER ROOTLESS SETUP
 ### Set correct permissions and make frontend config.json file editable for the runtime user
-mkdir -p /run/nginx
-chown nginx:nginx -R /run/nginx /var/cache/nginx "$ASSETS"/config.json
-sed -i 's|/var/run|/var/run/nginx|g' $NGINX_CONFIG_FILE
-## Replaces the 'root' directive with the value of $ASSETS
-sed -i "s|\${ASSETS}|${ASSETS}|g" "$NGINX_DEFAULT_SITE_CONFIG"
-## The 'user' config option is useless when running rootless and gives a warning
-sed -i '/^user /d' $NGINX_CONFIG_FILE
-## Allow to open privileged ports
-apk add --no-cache libcap
-setcap CAP_NET_BIND_SERVICE=+eip /usr/sbin/nginx
-apk --purge del libcap
+chown nginx:nginx "$ASSETS"/config.json
 
 # Trim image
 apk --purge del apk-tools
