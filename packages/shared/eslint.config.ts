81a60d2050a1fd81d070bbf3df04d82fcbf6aa38
Fernando Fernández <ferferga@hotmail.com>
2025-04-30 15:01:31 +0000

    refactor(shared): extract node utils to their own namespace

--- packages/shared/eslint.config.ts
+++ packages/shared/eslint.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'eslint/config';
-import { getBaseConfig, getTSVueConfig } from '@jellyfin-vue/configs/lint';
+import { getBaseConfig, getTSVueConfig, getNodeFiles } from '@jellyfin-vue/configs/lint';
 import pkg from './package.json' with { type: 'json' };
 
 export default defineConfig([
   ...getBaseConfig(pkg.name),
-  ...getTSVueConfig(false, import.meta.dirname)
+  ...getTSVueConfig(false, import.meta.dirname),
+  ...getNodeFiles(['./src/node/**/*.ts'])
 ]);
