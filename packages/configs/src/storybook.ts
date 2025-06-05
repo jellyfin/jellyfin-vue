456f93b008631d0ed051aca01210d72bceb543fc
Fernando Fernández <ferferga@hotmail.com>
2025-06-05 21:23:17 +0000

    chore(deps): update storybook to v9

--- packages/configs/src/storybook.ts
+++ packages/configs/src/storybook.ts
@@ -3,9 +3,7 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 export default {
   stories: ['./stories/**/*.stories.ts'],
   addons: [
-    '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
     '@storybook/addon-a11y'
   ],
   framework: {
