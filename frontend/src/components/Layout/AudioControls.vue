585bdf904036a55ba63276ef562b8af22a190fde
mtallalazam <m.tallalazam@gmail.com>
2026-03-28 02:54:32 +0400

    refactor: migrate css from vuetify to unocss for AudioControls

--- frontend/src/components/Layout/AudioControls.vue
+++ frontend/src/components/Layout/AudioControls.vue
@@ -10,24 +10,24 @@
       "
       class="uno-select-none uno-bg-surface">
       <VContainer fluid>
-        <VRow class="ma-0">
+        <VRow class="uno-m-0">
           <VCol
             cols="9"
             md="3"
-            class="pa-0 d-flex flex-row">
+            class="uno-flex uno-flex-row uno-p-0">
             <RouterLink :to="'/playback/music'">
               <div class="img uno-h-20 uno-w-20">
                 <BlurhashImage :item="playbackManager.currentItem.value" />
               </div>
             </RouterLink>
-            <VCol class="d-flex flex-column justify-center ml-4">
-              <VRow class="align-end">
+            <VCol class="uno-ml-4 uno-flex uno-flex-col uno-justify-center">
+              <VRow class="uno-items-end">
                 <RouterLink
                   v-slot="{ navigate }"
                   :to="getItemDetailsLink(playbackManager.currentItem.value)"
                   custom>
                   <span
-                    class="text-truncate link uno-h-fit"
+                    class="uno-h-fit uno-cursor-pointer uno-truncate uno-color-inherit uno-decoration-none"
                     @click="navigate">
                     {{ playbackManager.currentItem.value?.Name }}
                   </span>
@@ -35,17 +35,17 @@
               </VRow>
               <VRow
                 v-if="playbackManager.currentItem.value?.ArtistItems"
-                class="align-start">
+                class="uno-items-start">
                 <span
                   v-for="artist in playbackManager.currentItem.value?.ArtistItems"
                   :key="`artist-${artist.Id}`">
-                  <p class="mr-2 mb-0">
+                  <p class="uno-mb-0 uno-mr-2">
                     <RouterLink
                       v-slot="{ navigate }"
                       :to="getItemDetailsLink(artist, 'MusicArtist')"
                       custom>
                       <span
-                        class="text-truncate link text-caption"
+                        class="uno-transform-none uno-cursor-pointer uno-truncate uno-text-xs uno-color-inherit uno-font-normal uno-leading-7 uno-tracking-wide uno-decoration-none"
                         @click="navigate">
                         {{ artist.Name }}
                       </span>
@@ -57,24 +57,24 @@
           </VCol>
           <VCol
             cols="6"
-            class="pa-0 d-none d-md-inline">
-            <div class="d-flex flex-column justify-center">
-              <div class="d-flex align-center justify-center">
-                <ShuffleButton class="mx-1" />
-                <PreviousTrackButton class="mx-1" />
-                <PlayPauseButton class="mx-1" />
-                <NextTrackButton class="mx-1" />
-                <RepeatButton class="mx-1" />
+            class="uno-hidden uno-p-0 uno-min-[960px]:inline">
+            <div class="uno-flex uno-flex-col uno-justify-center">
+              <div class="uno-flex uno-items-center uno-justify-center">
+                <ShuffleButton class="uno-mx-1" />
+                <PreviousTrackButton class="uno-mx-1" />
+                <PlayPauseButton class="uno-mx-1" />
+                <NextTrackButton class="uno-mx-1" />
+                <RepeatButton class="uno-mx-1" />
               </div>
               <TimeSlider />
             </div>
           </VCol>
           <VCol
             cols="3"
-            class="d-none align-center d-md-flex justify-end">
+            class="uno-hidden uno-items-center uno-justify-end uno-min-[960px]:flex">
             <LikeButton :item="playbackManager.currentItem.value" />
             <QueueButton />
-            <div class="hidden-lg-and-down">
+            <div class="uno-hidden uno-min-[1920px]:block">
               <VolumeSlider />
             </div>
             <ItemMenu
@@ -89,15 +89,15 @@
           </VCol>
           <VCol
             cols="3"
-            class="d-flex pa-0 align-center justify-end d-md-none">
-            <PlayPauseButton class="mx-1" />
-            <NextTrackButton class="mx-1" />
+            class="uno-flex uno-items-center uno-justify-end uno-p-0 uno-min-[960px]:hidden">
+            <PlayPauseButton class="uno-mx-1" />
+            <NextTrackButton class="uno-mx-1" />
             <RepeatButton
               v-if="!$vuetify.display.xs"
-              class="mx-1" />
+              class="uno-mx-1" />
             <ShuffleButton
               v-if="!$vuetify.display.xs"
-              class="mx-1" />
+              class="uno-mx-1" />
           </VCol>
         </VRow>
       </VContainer>
