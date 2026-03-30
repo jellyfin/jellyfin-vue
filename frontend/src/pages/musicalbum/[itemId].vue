8271d708c3e815aa0dd12b222d2e4d5dfea1547c
mtallalazam <m.tallalazam@gmail.com>
2026-03-30 23:43:06 +0400

    refactor: migrate css from vuetify to unocss for musicalbum/[itemId]

--- frontend/src/pages/musicalbum/[itemId].vue
+++ frontend/src/pages/musicalbum/[itemId].vue
@@ -14,13 +14,13 @@
           md="9">
           <h1
             class="text-h4"
-            :class="{ 'text-center': !$vuetify.display.mdAndUp }">
+            :class="{ 'uno-text-center': !$vuetify.display.mdAndUp }">
             {{ item.Name }}
           </h1>
           <h2
             v-if="item.AlbumArtist && item?.AlbumArtists?.[0]"
-            class="text-subtitle-1 text-truncate mt-2"
-            :class="{ 'text-center': !$vuetify.display.mdAndUp }">
+            class="text-subtitle-1 uno-mt-2 uno-truncate"
+            :class="{ 'uno-text-center': !$vuetify.display.mdAndUp }">
             <RouterLink
               class="link"
               :to="getItemDetailsLink(item.AlbumArtists[0], 'MusicArtist')">
@@ -28,8 +28,8 @@
             </RouterLink>
           </h2>
           <div
-            class="text-caption text-h4 font-weight-medium mt-2"
-            :class="{ 'text-center': !$vuetify.display.mdAndUp }">
+            class="text-caption text-h4 uno-mt-2 uno-font-medium"
+            :class="{ 'uno-text-center': !$vuetify.display.mdAndUp }">
             <MediaInfo
               :item="item"
               year
@@ -38,18 +38,18 @@
               ends-at />
           </div>
           <VRow
-            class="my-4 align-center"
+            class="uno-my-4 uno-items-center"
             :class="{
-              'justify-center': !$vuetify.display.mdAndUp,
-              'ml-0': $vuetify.display.mdAndUp
+              'uno-justify-center': !$vuetify.display.mdAndUp,
+              'uno-ml-0': $vuetify.display.mdAndUp
             }">
             <PlayButton :item="item" />
             <LikeButton
               :item="item"
-              class="mr-2" />
+              class="uno-mr-2" />
             <MarkPlayedButton
               :item="item"
-              class="mr-2" />
+              class="uno-mr-2" />
             <ItemMenu :item="item" />
           </VRow>
           <VCol
@@ -61,11 +61,11 @@
               <VCol
                 :cols="12"
                 :sm="2"
-                class="text-truncate px-0">
+                class="uno-truncate uno-px-0">
                 <label class="text--secondary">{{ $t('genres') }}</label>
               </VCol>
               <VCol
-                class="px-0"
+                class="uno-px-0"
                 :cols="12"
                 :sm="10">
                 <VSlideGroup>
@@ -75,7 +75,7 @@
                     <VChip
                       size="small"
                       link
-                      :class="{ 'ml-2': index > 0 }"
+                      :class="{ 'uno-ml-2': index > 0 }"
                       :to="`/genre/${genre.Id}?type=${item.Type}`">
                       {{ genre.Name }}
                     </VChip>
