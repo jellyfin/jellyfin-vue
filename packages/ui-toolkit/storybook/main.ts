c209ba3fc31a05e7ecf6afa4020aeaa28962064b
Fernando Fernández <ferferga@hotmail.com>
2026-04-07 12:40:50 +0200

    chore(monorepo): move frontend under packages folder
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- packages/ui-toolkit/storybook/main.ts
+++ packages/ui-toolkit/storybook/main.ts
@@ -1,6 +1,11 @@
 import storybookConfig from '@jellyfin-vue/configs/storybook';
+import { getPackagePath } from '@jellyfin-vue/shared/node/utils';
+import { join } from 'node:path';
 
-storybookConfig.core.builder.options.viteConfigPath = '../../frontend/vite.config.ts';
+storybookConfig.core.builder.options.viteConfigPath = join(
+  getPackagePath('@jellyfin-vue/frontend')!,
+  'vite.config.ts'
+);
 
 // eslint-disable-next-line unicorn/prefer-export-from
 export default storybookConfig;
