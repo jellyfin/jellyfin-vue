de14c404e68790d4a94e0262344f1f4b8812b3bb
Fernando Fernández <ferferga@hotmail.com>
2026-04-07 14:21:44 +0200

    fix(monorepo): cyclic workspace dependency between shared and configs
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- packages/frontend/src/splashscreen.ts
+++ packages/frontend/src/splashscreen.ts
@@ -7,7 +7,7 @@
  */
 import { destr } from 'destr';
 import type { PartialDeep } from 'type-fest';
-import { darkColors, lightColors } from '@jellyfin-vue/shared/colors';
+import { darkColors, lightColors } from '@jellyfin-vue/configs/uno/colors';
 import type { ThemeSettingsState } from '#/store/settings/theme';
 import '#/assets/styles/splashscreen.css';
 
